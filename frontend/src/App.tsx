9d37082732ca76a73d10557eb3125c2fa6a672b6
MarxZhou <marxzhou@aliyun.com>
2020-10-15 01:59:00 +0800

    refactor: 重新设计样式，修复swiper-bar初始化显示异常的bug
    增加了一定的屏幕尺寸兼容性样式

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,37 @@
 import React from "react";
 import "./App.css";
 
-import Carousel from './components/Carousel';
+import airpods from './assets/airpods.png';
+import iphone from './assets/iphone.png';
+import tablet from './assets/tablet.png';
 
+import Carousel,{DataSourceType} from './components/Carousel';
 
+const carouselDataSource:DataSourceType=[
+  {
+    title: ['xPhone'],
+    desc: ['Lots to love.Less to spend.', 'Starting at $399.'],
+    color: '#fff',
+    bgc: '#111111',
+    bgi: iphone,
+  },
+  {
+    title: ['Tablet'],
+    desc: ['Just the right amount of everything'],
+    bgc: '#FAFAFA',
+    bgi: tablet,
+  },
+  {
+    title: ['Buy a Tablet or xPhone for college.', 'Get arPods.'],
+    bgc: '#F1F1F3',
+    bgi: airpods,
+  },
+]
 
 function App() {
   return (
     <div className="App">
-      <Carousel />
+      <Carousel dataSource={carouselDataSource} duration={3000} />
     </div>
   );
 }
--- frontend/src/components/Carousel/index.tsx
+++ frontend/src/components/Carousel/index.tsx
@@ -1,49 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
-import airpods from '../../assets/airpods.png';
-import iphone from '../../assets/iphone.png';
-import tablet from '../../assets/tablet.png';
+import './css/index.css';
 
-import './index.css';
-
-interface Sliders {
+export interface SliderType {
   title: string[],
-  desc: string[],
+  desc?: string[],
   color?: string,
   bgc?: string,
   bgi?: any;
 }
 
-const sliders: Sliders[] = [
-  {
-    title: ['xPhone'],
-    desc: ['Lots to love.Less to spend.', 'Starting at $399.'],
-    color: '#fff',
-    bgc: '#111111',
-    bgi: iphone,
-  },
-  {
-    title: ['Tablet'],
-    desc: ['Just the right amount of everything'],
-    bgc: '#FAFAFA',
-    bgi: tablet,
-  },
-  {
-    title: ['Buy a Tablet or xPhone for college.', 'Get arPods.'],
-    desc: [],
-    bgc: '#F1F1F3',
-    bgi: airpods,
-  },
-];
+export type DataSourceType = SliderType[];
 
-const duration = 3000;
+export interface CarouselType {
+  dataSource: DataSourceType,
+  duration: number
+}
 
-function Carousel() {
+function Carousel(props: CarouselType) {
+  const { duration, dataSource } = props;
+  const { length } = dataSource;
+
   const [slideIndex, setSlideIndex] = useState(0);
+  const [sliding, setSliding] = useState(false);
 
   useEffect(() => {
+    setSliding(true);
+
     let timer: any = setInterval(() => {
-      setSlideIndex((slideIndex + 1) % sliders.length);
+      setSlideIndex((slideIndex + 1) % length);
     }, duration);
 
     return () => {
@@ -52,124 +37,96 @@ function Carousel() {
         timer = null;
       }
     };
-  }, [slideIndex]);
+  }, [slideIndex, length, duration, sliding]);
 
   return (
-    <div className='swiper'>
+    <div className='swiper-container'>
       <div
         className='swiper-wrapper'
         style={{
-          width: sliders.length * 100 + 'vw',
+          width: length * 100 + 'vw',
           transform: `translateX(${-100 * slideIndex}vw)`,
         }}
       >
-        {
-          sliders.map((value, index) => {
-            return (
-              <div
-                key={'slider' + index}
-                className='swiper-slider'
-                style={{
-                  backgroundColor: value.bgc,
-                  color: value.color,
-                }}
-              >
-                <div className='text'>
-                  {
-                    value.title.map((v, i) => {
-                      if (value.title.length === 1 && i === 0) {
-                        return (
-                          <div
-                            key={'title' + i}
-                            className='title'
-                            style={{
-                              paddingBottom: 20,
-                            }}
-                          >
-                            {v}
-                          </div>
-                        );
-                      }
+        {dataSource.map((value, index) => {
+          return (
+            <div
+              key={'slider' + index}
+              className='swiper-slider'
+              style={{
+                backgroundColor: value.bgc,
+                color: value.color,
+              }}
+            >
+              <div className='text-wrapper'>
+                {
+                  value.title.map((v, i) => {
+                    if (value.title.length === 1 && i === 0) {
                       return (
                         <div
-                          className='title'
-                          key={'title' + i}
+                          key={'text-title' + i}
+                          className='title pb20'
                         >
                           {v}
                         </div>
                       );
-                    })
-                  }
-                  {
-                    value.desc.map((v, i) => (
+                    }
+                    return (
                       <div
-                        className='desc'
-                        key={'desc' + i}
+                        className='text-title'
+                        key={'title' + i}
                       >
                         {v}
                       </div>
-                    ))
-                  }
-                </div>
-                <div className='bgi'>
-                  {
-                    value.bgi &&
-                    <img
-                      src={value.bgi}
-                      alt=''
-                    />
-                  }
-                </div>
+                    );
+                  })
+                }
+                {
+                  value.desc && value.desc.map((v, i) => (
+                    <div
+                      className='text-desc'
+                      key={'desc' + i}
+                    >
+                      {v}
+                    </div>
+                  ))
+                }
               </div>
-            );
-          })
-        }
+              <div className='swiper-img-wrapper'>
+                {
+                  value.bgi &&
+                  <img
+                    src={value.bgi}
+                    alt=''
+                  />
+                }
+              </div>
+            </div>
+          );
+        })}
       </div>
       <div
         className='swiper-bar'
-        style={{
-          backgroundColor: 'rgba(0,0,0,0)',
-          color: sliders[slideIndex].color ? sliders[slideIndex].color : '#000',
-        }}
       >
-        <div className='swiper-bar-wrapper'>
-          {sliders.map((value, index, array): any => {
-            /*
-            * 页面在初次渲染时，存在问题
-            * 首次渲染时，第一个进度条无法自动读条，必须要等第二轮才能自动读条
-            * */
+        {dataSource.map((value, index): any => {
+          let swiperBarItemSlotClassname = 'swiper-bar-item-slot';
 
-            const swiperBarItemSlotStyles: any = {
-              width: '0%',
-            };
+          if (slideIndex === index && sliding) {
+            swiperBarItemSlotClassname += ' swiper-bar-item-slot-transition';
+          }
 
-            if (slideIndex === index) {
-              swiperBarItemSlotStyles.transition = `all ${duration}ms linear`;
-              swiperBarItemSlotStyles.width = '100%';
-            }
-
-            return (
+          return (
+            <div
+              key={'swiper-bar' + index}
+              className='swiper-bar-item'
+            >
               <div
-                key={'swiper-bar' + index}
-                className='swiper-bar-item'
-                style={{
-                  backgroundColor: '#ccc',
-                }}
+                className={swiperBarItemSlotClassname}
               >
-                <div
-                  className='swiper-bar-item-slot'
-                  style={{
-                    backgroundColor: '#888',
-                    width:'0%',
-                    ...swiperBarItemSlotStyles,
-                  }}
-                >
-
-                </div>
               </div>
-            );
-          })}
-        </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
