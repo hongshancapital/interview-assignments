5ec303c23b03a27bac74f0fa7b04c47dfac7e72d
nathan.huang <nathan.huang@pwc.com>
2020-11-19 19:16:15 +0800

    add vpc no public config

--- dev-ops/Q2/index.ts
+++ dev-ops/Q2/index.ts
@@ -2,14 +2,22 @@ import ec2 = require('@aws-cdk/aws-ec2');
 import ecs = require('@aws-cdk/aws-ecs');
 import ecs_patterns = require('@aws-cdk/aws-ecs-patterns');
 import cdk = require('@aws-cdk/core');
+import waf = require('@aws-cdk/aws-waf');
 
 class MyTestFargate extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Create VPC and Fargate Cluster
-    // NOTE: Limit AZs to avoid reaching resource quotas
-    const vpc = new ec2.Vpc(this, 'MyTestVpc', { maxAzs: 2 });
+    const vpc = new ec2.Vpc(this, 'MyTestVpc', {
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          name: 'PrivateService',
+          subnetType: ec2.SubnetType.PRIVATE,
+        }
+      ]
+    });
     const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
 
     // Instantiate Fargate Service with just cluster and image
