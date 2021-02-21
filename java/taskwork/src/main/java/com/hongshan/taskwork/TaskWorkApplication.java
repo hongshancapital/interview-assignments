3fea0e3f3891db35a88681bde005c435360f1974
daikai <13621151569@yeah.net>
2021-02-21 22:59:52 +0800

    rename

--- java/taskwork/src/main/java/com/hongshan/taskwork/TaskWorkApplication.java
+++ java/taskwork/src/main/java/com/hongshan/taskwork/TaskWorkApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TaskWorkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TaskWorkApplication.class, args);
 	}
 
 }
