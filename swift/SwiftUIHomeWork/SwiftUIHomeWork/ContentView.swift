a7c6e7b72b9df47f67bacf77d78f9bfe9ba2b6a6
RemiliaScarlet <49673934+a1342966673@users.noreply.github.com>
2021-03-17 13:48:25 +0800

    删除了无用代码

---
+++ swift/SwiftUIHomeWork/SwiftUIHomeWork/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  SwiftUIHomeWork
+//
+//  Created by RemiliaScarlet on 2021/3/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LoginView()
+            .environmentObject(MockUserInfo())
+    }
+    
+}
+
+struct UnderLine:View {
+    var body: some View{
+        Rectangle()
+            .frame(height: 2.0, alignment: .bottom)
+            .foregroundColor(Color.gray)
+    }
+}//定义了一个下划线
+
+struct ConfirmButton:View {
+    var body: some View{
+        Rectangle()
+            .frame(width: UIScreen.main.bounds.width/1.3, height: UIScreen.main.bounds.height/15)
+            .foregroundColor(.green)
+            .cornerRadius(10)
+    }
+}//定义了按钮样式
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift/SwiftUIHomeWork/SwiftUIHomeWork/SwiftUIHomeWorkApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIHomeWorkApp.swift
+//  SwiftUIHomeWork
+//
+//  Created by RemiliaScarlet on 2021/3/15.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct SwiftUIHomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
