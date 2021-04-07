79c1637b4008151d643f84605537d7a104739648
jade75 <yu.chen723@gmail.com>
2021-04-07 19:56:38 +0800

    v0.9 pic of system

--- java/interview/src/main/java/com/d00216118/demo/DemoApiApplication.java
+++ java/interview/src/main/java/com/d00216118/demo/DemoApiApplication.java
@@ -14,7 +14,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableCaching
 @EnableJpaAuditing
-//@EnableSwagger2
 public class DemoApiApplication {
 
     public static void main(String[] args) {
