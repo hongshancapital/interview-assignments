2ab8cc73dcacc1535a5248aea0a74506c735d371
jade75 <yu.chen723@gmail.com>
2021-04-08 14:46:00 +0800

    ignore

--- java/interview/src/main/java/com/d00216118/demo/config/RepoConfig.java
+++
@@ -1,8 +0,0 @@
-package com.d00216118.demo.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-public interface RepoConfig {
-}
--- java/interview/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
---  create table user (
---        id bigint not null,
---         password varchar(255),
---         username varchar(255),
---         primary key (id)
---     );
-
-insert into USER (password, username, id) values ('test1', 'test1' ,1);
-insert into USER (password, username, id) values ('test2', 'test2' ,2);
