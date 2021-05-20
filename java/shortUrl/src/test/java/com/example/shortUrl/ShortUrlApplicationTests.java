73d6b73a13e3826c6657d7deeddd8e5334986bf7
shiqingshan <sqskg@163.com>
2021-05-20 15:42:16 +0800

    单元测试并集成jacoco

--- java/shortUrl/src/test/java/com/example/shortUrl/ShortUrlApplicationTests.java
+++ java/shortUrl/src/test/java/com/example/shortUrl/ShortUrlApplicationTests.java
@@ -3,20 +3,66 @@ package com.example.shortUrl;
 import com.example.shortUrl.utils.ShortUrlGenerator;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @Slf4j
-@SpringBootTest
-class ShortUrlApplicationTests {
+@SpringBootTest(classes = ShortUrlApplication.class)
+public class ShortUrlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private MockMvc mvc;
 
-	@Test
-	void testGenerator(){
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void testGenerator() {
+        String longUrl = "https://github.com/sqskg/interview-assignments";
+        log.info("short url = {}", ShortUrlGenerator.shortCodeGenerate(longUrl));
+        //short url = VjzMBFIj
+    }
+
+    @Test
+    public void generate() throws Exception {
+        mvc = MockMvcBuilders.webAppContextSetup(wac).build(); //初始化MockMvc对象
 		String longUrl = "https://github.com/sqskg/interview-assignments";
-		log.info("short url = {}",ShortUrlGenerator.shortCodeGenerate(longUrl));
-		//short url = VjzMBFIj
-	}
+		String uri = "/shortUrl/generate";
+        ResultActions actions = mvc
+                .perform(MockMvcRequestBuilders.post(uri).characterEncoding("UTF-8")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(longUrl));
+		actions.andReturn().getResponse().setCharacterEncoding("UTF-8");
+		actions.andExpect(
+				MockMvcResultMatchers.status().isOk())
+				.andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void resolve() throws Exception {
+        mvc = MockMvcBuilders.webAppContextSetup(wac).build(); //初始化MockMvc对象
+        String uri = "/shortUrl/resolve";
+        String shortUrl = "https://tk.vip/M72VRb6Z";
+        ResultActions actions = mvc
+                .perform(MockMvcRequestBuilders.get(uri).characterEncoding("UTF-8")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .param("shortUrl",shortUrl));
+        actions.andReturn().getResponse().setCharacterEncoding("UTF-8");
+        actions.andExpect(
+                MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
+    }
 }
