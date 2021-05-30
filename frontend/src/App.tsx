89b77e531f5cecdf9f56242058464a49e1b92755
zhangyx <aa94600180@163.com>
2021-05-30 23:59:43 +0800

    feat: 张宇行 web前端开发工程师

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,103 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
-function App() {
-  return <div className="App">{/* write your component here */}</div>;
+const IMAGELIST = [
+  { text: '图片1', color: '#fcf', id: 1 },
+  { text: '图片2', color: '#cfc', id: 2 },
+  { text: '图片3', color: '#fcc', id: 3 }
+];
+const minWidth = 500;
+
+const Carousel = () => {
+  const [index, setIndex] = useState(0);
+  const [posLeft, setPosLeft] = useState(0);
+  const [timmer, setTimmer] = useState(null);
+
+  const getPointList = () => {
+    const pointArr = [];
+
+    for (let pointIndedx = 0; pointIndedx < IMAGELIST.length; pointIndedx++) {
+      let pointDom = (
+        <span
+          className={`point ${pointIndedx === index ? 'active' : ''}`}
+          key={Math.random()}
+        >
+        </span>
+      );
+      pointArr.push(pointDom);
+    }
+    return pointArr;
+  }
+
+  const start = () => {
+    clearInterval(timmer);
+    let _timmer = setInterval(() => {
+      turn('left');
+    }, 1000);
+    setTimmer(_timmer);
+  }
+
+  const stop = () => {
+    clearInterval(timmer)
+    setTimmer(null);
+  }
+
+  const turn = (direction: string) => {
+    let _index = index;
+    if (direction === 'left') {
+      _index = _index + 1;
+    } else if (direction === 'right') {
+      _index = index - 1;
+    }
+
+    if (_index > IMAGELIST.length - 1) {
+      _index = 0;
+    }
+
+    if (_index < 0) {
+      _index = IMAGELIST.length - 1;
+    }
+
+    setIndex(_index);
+    setPosLeft(-minWidth * _index)
+  }
+
+  useEffect(() => {
+    start();
+  }, [index]);
+
+  return (
+    <div className="App"
+      onMouseOver={() => stop()}
+      onMouseOut={() => start()}
+    >
+      <div
+        className="banner"
+        style={{
+          width: minWidth * IMAGELIST.length,
+          left: posLeft
+        }}
+      >
+        {IMAGELIST.map(item => (
+          <div
+            className="banner-image"
+            key={item.id}
+            style={{
+              width: minWidth,
+              background: item.color,
+            }}
+          >
+            {item.text}
+          </div>
+        ))}
+      </div>
+      <div className="left" onClick={() => turn('right')}>{'<'}</div>
+      <div className="right" onClick={() => turn('left')}>{'>'}</div>
+      <div className="point-list">
+        {getPointList()}
+      </div>
+    </div>
+  );
 }
 
-export default App;
+export default Carousel;
