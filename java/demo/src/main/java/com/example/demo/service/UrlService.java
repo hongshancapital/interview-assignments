766cc7eb19b6c2166de727da805c11754a540e21
zhangzhi <9873199@qq.com>
2021-07-06 10:43:11 +0800

    8.1 将HashMap改成ConcurrentHashMap 8.2 在获取短链接之前, 先看map中是否已存在 8.3 修改设计开发.md

--- java/demo/src/main/java/com/example/demo/service/UrlService.java
+++ java/demo/src/main/java/com/example/demo/service/UrlService.java
@@ -1,8 +1,8 @@
 package com.example.demo.service;
 
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateFormatUtils;
@@ -16,7 +16,7 @@ import com.example.demo.dto.Repository;
 @Service
 public class UrlService {
 
-	private static Map<String, Repository> repositoryMap = new HashMap<>();
+	private static Map<String, Repository> repositoryMap = new ConcurrentHashMap<>();
 
 	public String getNormalUrl(String shortUrl) {
 		CommonUtil.validateShortUrlFormat(shortUrl);
@@ -30,6 +30,10 @@ public class UrlService {
 
 	private String createShortUrl(String normalUrl) {
 		String key = CommonUtil.createKey(normalUrl);
+		Repository repository = repositoryMap.get(key);
+		if (repository != null && normalUrl.equals(repository.getNormalUrl())) {
+			return repository.getShortUrl();
+		}
 		String shortUrl = StringUtils.join(Constants.BASE_DOMAIN, key);
 		putRepository(key, normalUrl, shortUrl);
 		return shortUrl;
