237e396f80456b4b19b885ac537ea7412fdcec1e
maruie <maruie@dcits.com>
2021-07-08 18:43:55 +0800

    短域名存储接口：接受长域名信息，返回短域名信息 短域名读取接口：接受短域名信息，返回长域名信息。

---
+++ java/smart-url/src/test/java/com/changeurl/UrlApplicationTests.java
@@ -0,0 +1,27 @@
+package com.changeurl;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class UrlApplicationTests {
+    @Autowired
+
+
+    @Test
+    void contextLoads() {
+
+    }
+
+    @Test
+    void urlTest() {
+
+    }
+
+}
