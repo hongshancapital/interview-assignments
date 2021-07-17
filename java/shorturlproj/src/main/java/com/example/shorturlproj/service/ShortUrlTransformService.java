72a4ec90ee65f010ae5386f055102adcfdbc837b
piscescapo <capolsy@163.com>
2021-07-18 02:18:22 +0800

    test1

--- java/shorturlproj/src/main/java/com/example/shorturlproj/service/ShortUrlTransformService.java
+++
@@ -1,9 +0,0 @@
-package com.example.shorturlproj.service;
-
-
-import java.util.concurrent.ExecutionException;
-
-public interface ShortUrlTransformService{
-    public String getShortUrlFromLongUrl(String longUrl) throws ExecutionException;
-    public String getLongUrlFromShortUrl(String shortUrl) throws ExecutionException;
-}
