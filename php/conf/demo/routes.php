bcae1662950cd1fb89c49dea27a17d8f98915a1a
Zbin <694309555@qq.com>
2021-07-17 12:46:24 +0800

    拉取测试

---
+++ php/conf/demo/routes.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * 路由配置
+ *
+ * @version 1.0
+ * @author ZBin
+ */
+declare(strict_types=1);
+
+$routes = [];
+
+//$routes['/{controller}/{action}'] = [];
+
+//$routes['/']['controller'] = 'index';
+//$routes['/']['action'] = 'index';
+
+//$routes['/rpc']['namespace'] = 'Project\Controller\Rpc';
+//$routes['/rpc']['controller'] = 'index';
+//$routes['/rpc']['action'] = 'index';
+
+//$routes['/test']['namespace'] = 'Myspace';
+//$routes['/test']['controller'] = 'test';
+//$routes['/test']['action'] = 'index';
+
+return $routes;
+
+?>
---
+++ php/conf/demo/services.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * 应用服务
+ *
+ * @version 1.0
+ * @author ZBin
+ */
+declare(strict_types=1);
+
+$router = new Phalcon\Mvc\Router();
+$router->removeExtraSlashes(true);
+$router->setDefaults([
+    'controller' => 'index',
+    'action' => 'index'
+]);
+if ($routes = include('routes.php')) {
+    foreach ($routes as $route => $definition) {
+        $router->add($route, $definition);
+    }
+}
+$di->setShared('router', $router);
+
+//$serviceClass = 'Project\Controllers\MyController';
+//$di->setShared('Project\Controllers\TestController', new $serviceClass());
+
+
+//可变参数服务（一般为动态实例化服务）（其他尽量共享化）
+$services = [];
+
+?>
