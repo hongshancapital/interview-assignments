e0dbedb6174c757214019d20770a6fa7aa8e0770
Soyn <w604408482@163.com>
2021-09-15 23:06:39 +0800

    fix(remove tailwind): direct import css assets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
+import { Slides } from './slides';
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return <div className="App w-full h-full p-8 flex justify-center">
+    <Slides />
+  </div>;
 }
 
 export default App;
