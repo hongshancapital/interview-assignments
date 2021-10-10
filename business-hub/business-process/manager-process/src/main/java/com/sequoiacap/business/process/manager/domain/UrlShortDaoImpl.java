323d025c420b7208fa5bacfa2d351c1051ca4e99
xulong.wang <123abcd!>
2021-10-10 17:50:40 +0800

    [java]:xulong.wang‘s interview assignments 长域名转成短域名的时候先检查是否已经存在

--- business-hub/business-process/manager-process/src/main/java/com/sequoiacap/business/process/manager/domain/UrlShortDaoImpl.java
+++ business-hub/business-process/manager-process/src/main/java/com/sequoiacap/business/process/manager/domain/UrlShortDaoImpl.java
@@ -2,15 +2,11 @@ package com.sequoiacap.business.process.manager.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  *@ClassName: UrlShortDaoImpl
