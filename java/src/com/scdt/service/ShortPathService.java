7b8ef8fcb87bd15746545958aea26d28ad223760
wangxiaofeng <wangxiaofeng@ajmide.com>
2021-10-10 10:48:10 +0800

    java homework 王晓峰 2021-10-9
    13917000708
    sppnew111@hotmail.com

--- java/src/com/scdt/service/ShortPathService.java
+++ java/src/com/scdt/service/ShortPathService.java
@@ -1,6 +1,9 @@
 package com.scdt.service;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
+@Api(value = "短路径服务")
 public interface ShortPathService {
     /**
      * 短域名存储接口：接受长域名信息，返回短域名信息
@@ -10,6 +13,7 @@ public interface ShortPathService {
      *                 YWFhL2JiYi9jY2MlM0ZhYmMlM0QxMjM=
      * @return 短路径
      */
+    @ApiOperation(value = "接受长域名信息，返回短域名信息", notes = "有保存作用, 长路径需要加密（base64）后传入")
     String getShortPath(String longPath);
 
     /**
@@ -17,6 +21,7 @@ public interface ShortPathService {
      * @param shortPath 短路径
      * @return 长路径， 使用时需要解密
      */
+    @ApiOperation(value = "接受短域名信息，返回长域名信息", notes = "如果没有对应长路径就返回空, 长路径需要解密（base64）后使用")
     String getLongPath(String shortPath);
 }
 
--- java/src/com/scdt/service/ShortPathServiceImpl.java
+++ java/src/com/scdt/service/ShortPathServiceImpl.java
@@ -1,18 +1,12 @@
 package com.scdt.service;
 
 import com.scdt.util.Tools;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.URL;
-import java.net.URLDecoder;
 import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
-@Api(value = "短路径服务")
 public class ShortPathServiceImpl implements ShortPathService {
     /**
      * 内部存储1
@@ -28,7 +22,6 @@ public class ShortPathServiceImpl implements ShortPathService {
 
     private final static int SHORT_PATH_CHAR_LENGTH = 8;
 
-    @ApiOperation(value = "接受长域名信息，返回短域名信息", notes = "有保存作用")
     @GetMapping(value="/get_short_path/{long_path}")
     @Override
     public String getShortPath(@PathVariable("long_path") String longPath) {
@@ -51,7 +44,6 @@ public class ShortPathServiceImpl implements ShortPathService {
         return shortPath;
     }
 
-    @ApiOperation(value = "接受短域名信息，返回长域名信息", notes = "如果没有对应长路径就返回空")
     @Override
     @GetMapping(value="/get_long_path/{short_path}")
     public String getLongPath(@PathVariable("short_path")String shortPath) {
