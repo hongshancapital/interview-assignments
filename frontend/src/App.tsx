0046b38f6e8b766fc6aa41bab86f600d439e6620
jackchen <690399198@qq.com>
2021-10-11 23:30:44 +0800

    init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
-
+import Carouse from "./component/Carousel"
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return <div className="App"><Carouse >
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  </Carouse></div>;
 }
 
 export default App;
