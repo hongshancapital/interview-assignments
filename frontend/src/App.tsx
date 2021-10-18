9d6ef9c89c3c1d2fb0c04c2c6d7c4c945a4cadb7
lypcoding <344438059@qq.com>
2021-10-18 20:28:18 +0800

    mod:性能优化

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import "./App.css";
 import Carousel from "./components/carousel/Carousel";
-import img1 from './assets/airpods.png'
-import img2 from './assets/iphone.png'
-import img3 from './assets/tablet.png'
-console.log(1)
+import img3 from './assets/airpods.png'
+import img1 from './assets/iphone.png'
+import img2 from './assets/tablet.png'
 function App() {
-  return <div className="App" style={{height:'600px'}}>
+  return <div className="App" style={{ height: '100%' }}>
     <Carousel>
-      <img src={img1} alt="" />
-      <img src={img2} alt="" />
-      <img src={img3} alt="" />
-    </Carousel>  
+    <div style={{ backgroundColor: '#111111', color: '#fff' }}>
+        <p className="pt200 font36">xPhone</p>
+        <p className="pt30 font18">lots to love.Less to spend.</p>
+        <p className="font18">Starting at $399.</p>
+        <img src={img1} alt="" width="50%" />
+      </div>
+      <div style={{ backgroundColor: '#fafafa', color: '#111111' }}>
+        <p className="pt200 font36">Tablet</p>
+        <p className="pt30 font18">Just the right amount of everything.</p>
+        <img src={img2} alt="" width="100%" />
+      </div>
+      <div style={{ backgroundColor: '#f1f1f3', color: '#111111' }}>
+        <p className="pt200 font36">Buy a Tablet or xPhone for college</p>
+        <p className="font36">Get arPods</p>
+        <img src={img3} alt="" width="100%" />
+      </div>
+    </Carousel>
   </div>;
 }
 
