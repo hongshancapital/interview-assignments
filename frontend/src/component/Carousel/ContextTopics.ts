42e59485b199bc72b9b9893543344d0b10bb0ea7
ramroll <ramroll@gmail.com>
2021-10-27 10:32:36 +0800

    修改

---
+++ frontend/src/component/Carousel/ContextTopics.ts
@@ -0,0 +1,5 @@
+export enum ContextTopics {
+  Preapre,
+  Enter,
+  Finish
+}
--- frontend/src/component/Scroller.tsx
+++ frontend/src/component/Scroller.tsx
@@ -1,6 +1,9 @@
 import React, {CSSProperties} from 'react'
 import classes from './scroller.module.scss'
 
+/**
+ * 用它实现Carousel但是和Carousel无关，可以复用
+ */
 type ScrollerProps = {
   style?: CSSProperties
   children: JSX.Element | JSX.Element[]
