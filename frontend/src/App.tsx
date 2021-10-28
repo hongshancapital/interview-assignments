e46d4e8f2af04d31632f33f88dddf8d9fc3669f8
= <ramroll@gmail.com>
2021-10-28 20:00:19 +0800

    数据化原有程序

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,69 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import "./App.css";
 import { Carousel } from "./component/Carousel/Carousel";
 import { MyBullets } from "./component/MyBullets";
 
-const Info = ({children} : {children : string}) => {
-  return <h3 style={{
-    color : 'blue'
-  }}>{children}</h3>
+type SliderData = {
+  lines: { type: string, content?: string, src?: string }[],
+  style ? : CSSProperties
 }
+const sliders : SliderData[] = [
+  {
+    style : {
+      backgroundColor: "black",
+      color: "white", 
+    },
+    lines: [
+      {type : 'h1', content : "XPhone"},
+      {type : 'h2', content : "Lots to love. ,Less to spend."},
+      {type : 'h2', content : "Starting at $399. "}
+    ]
+  },
+  {
+    lines: [
+      {
+        type: 'h1',
+        content: "Tablet"
+      }, {
+        type: 'h2',
+        content: "just the right amount of everything"
+      }]
+  },
+  {
+    lines : [
+      {
+        type : 'h1',
+        content : 'Buy a Tablet or XPhone for college.'
+      },
+      {
+        type : 'h1',
+        content : 'Get arPods'
+      }
+    ]
+  }
+]
+
+const Slider = ( props : SliderData & {className? : string} ) => {
+  return <div style={props.style} className={props.className}>
+    {props.lines.map( ({type, content, src}, i) => {
+      switch(type) {
+        case 'h1':
+          return <h1 key={i}>{content}</h1>
+        case 'h2':
+          return <h2 key={i}>{content}</h2>
+        case 'img':
+          return <img alt="" src={src} key={i} />
+        default :
+          throw new Error(`type ${type} not supported.`)
+      }
+    })}
+  </div>
+}
+
+
+
+
+
 function App() {
   return (
     <div className="App">
@@ -25,32 +81,9 @@ function App() {
         mode="one-side"
         addons = {<MyBullets />}
       >
-        <div
-          style={{
-            backgroundColor: "black",
-            color: "white",
-          }}
-        >
-          <h1>XPhone</h1>
-          <h2>Lots to love, less to spend</h2>
-          <h2>Starting at $399</h2>
-        </div>
-        <div>
-          <h1>Tablet</h1>
-          <h2>Just the right amount of everything.</h2>
-        </div>
-        <div
-          style={{
-            backgroundColor: "palevioletred",
-          }}
-        >
-          <h1>WonderFul</h1>
-          <h2>This is a Test Page</h2>
-        </div>
-        <div>
-          <h1>Buy a Tablet or xPhone for college.</h1>
-          <h1>Get arPods</h1>
-        </div>
+        {sliders.map((slider, i) => {
+          return <Slider {...slider} key={i} />
+        })}
       </Carousel>
 
       <Info>
@@ -67,32 +100,9 @@ function App() {
         mode="one-side"
         addons = {<MyBullets />}
       >
-        <div
-          style={{
-            backgroundColor: "black",
-            color: "white",
-          }}
-        >
-          <h1>XPhone</h1>
-          <h2>Lots to love, less to spend</h2>
-          <h2>Starting at $399</h2>
-        </div>
-        <div>
-          <h1>Tablet</h1>
-          <h2>Just the right amount of everything.</h2>
-        </div>
-        <div
-          style={{
-            backgroundColor: "palevioletred",
-          }}
-        >
-          <h1>WonderFul</h1>
-          <h2>This is a Test Page</h2>
-        </div>
-        <div>
-          <h1>Buy a Tablet or xPhone for college.</h1>
-          <h1>Get arPods</h1>
-        </div>
+        {sliders.map((slider, i) => {
+          return <Slider {...slider} key={i} />
+        })}
       </Carousel>
 
       <Info>单向无限循环（的轮播图）</Info>
@@ -105,24 +115,9 @@ function App() {
         }}
         addons = {<MyBullets />}
       >
-        <div
-          style={{
-            backgroundColor: "black",
-            color: "white",
-          }}
-        >
-          <h1>XPhone</h1>
-          <h2>Lots to love, less to spend</h2>
-          <h2>Starting at $399</h2>
-        </div>
-        <div>
-          <h1>Tablet</h1>
-          <h2>Just the right amount of everything.</h2>
-        </div>
-        <div>
-          <h1>Buy a Tablet or xPhone for college.</h1>
-          <h1>Get arPods</h1>
-        </div>
+        {sliders.map((slider, i) => {
+          return <Slider {...slider} key={i} />
+        })}
       </Carousel> 
 
 
@@ -137,27 +132,17 @@ function App() {
         dir="right"
         addons = {<MyBullets />}
       >
-        <div
-          style={{
-            backgroundColor: "black",
-            color: "white",
-          }}
-        >
-          <h1>XPhone</h1>
-          <h2>Lots to love, less to spend</h2>
-          <h2>Starting at $399</h2>
-        </div>
-        <div>
-          <h1>Tablet</h1>
-          <h2>Just the right amount of everything.</h2>
-        </div>
-        <div>
-          <h1>Buy a Tablet or xPhone for college.</h1>
-          <h1>Get arPods</h1>
-        </div>
+        {sliders.map((slider, i) => {
+          return <Slider {...slider} key={i} />
+        })}
       </Carousel> 
     </div>
   )
+}
+const Info = ({children} : {children : string}) => {
+  return <h3 style={{
+    color : 'blue'
+  }}>{children}</h3>
 }
 
 
