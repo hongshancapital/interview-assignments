e2e93a4a968a4ae9d0a887f13436f676318f0154
tataazy <489546574@qq.com>
2021-11-02 23:50:33 +0800

    1、简化短网址生成算法，减少不必要的操作； 2、内存溢出考虑；

--- java/src/main/java/com/tataazy/work/domainmanage/service/ShortUrlProcessServiceImpl.java
+++ java/src/main/java/com/tataazy/work/domainmanage/service/ShortUrlProcessServiceImpl.java
@@ -1,12 +1,13 @@
 package com.tataazy.work.domainmanage.service;
 
-import com.tataazy.work.domainmanage.utils.Md5Utils;
+import com.google.common.hash.Hashing;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -14,57 +15,43 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ShortUrlProcessServiceImpl {
     public static final Log log = LogFactory.getLog(ShortUrlProcessServiceImpl.class);
 
-    private volatile Map<String, String> urlMap, recordMap;
+    private volatile Map<String, String> recordMap;
 
     @PostConstruct
     public void init() {
-        urlMap = new ConcurrentHashMap<>();
         recordMap = new ConcurrentHashMap<>();
     }
 
-    private static final char[] CHARACTERS = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
-            'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z','0', '1', '2', '3', '4', '5', '6', '7', '8',
-            '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    private void mapReInit() {
+        if (null != recordMap && !recordMap.isEmpty()) {
+            recordMap.clear();
+        }
+    }
 
     public String generateShorUrl(String normalUrl) {
         if (StringUtils.isBlank(normalUrl)) {
             return null;
         }
-        try {
-            String sMD5Result = Md5Utils.encodeByMd5(normalUrl);
-            String shortUrl = recordMap.get(sMD5Result);
-            if (StringUtils.isNotBlank(shortUrl)) {
-                return shortUrl;
-            }
-
-            StringBuilder resultUrl = new StringBuilder();
-            for (int i = 0; i < 4; i++) {
-                String subString = sMD5Result.substring(i*8, i*8 + 8);
-                long hexLong = 0x3FFFFFFF & Long.parseLong(subString,16);
-                String outChars = "";
-                for (int j = 0; j < 2; j++) {
-                    long index = 0x0000003D & hexLong;
-                    outChars += CHARACTERS[(int)index];
-                    hexLong = hexLong >> 15;
-                }
-                resultUrl.append(outChars);
-            }
-            shortUrl = resultUrl.toString();
-            urlMap.put(shortUrl, normalUrl);
-            recordMap.put(sMD5Result, shortUrl);
-            String preFix = normalUrl.substring(0, normalUrl.indexOf(":"));
-            return preFix + "://" + shortUrl;
-        } catch (Exception e) {
-            log.error("generateShorUrl exception : ", e);
+        String hashStr = Hashing.murmur3_32().hashString(normalUrl, StandardCharsets.UTF_8).toString();
+        if (StringUtils.isNotBlank(recordMap.get(hashStr))) {
+            return new StringBuilder(16).append(normalUrl,0,normalUrl.indexOf(":"))
+                    .append("://").append(hashStr).toString();
         }
-        return null;
+        try {
+            recordMap.put(hashStr,normalUrl);
+        } catch (Throwable e) {
+            log.error("generateShorUrl exception : ", e);
+            if (e instanceof OutOfMemoryError) {
+                mapReInit();
+            }
+        }
+        return new StringBuilder(16).append(normalUrl,0,normalUrl.indexOf(":"))
+                .append("://").append(hashStr).toString();
     }
 
     public String getNormalUrl(String shortUrl) {
         if (StringUtils.isNotBlank(shortUrl)) {
-            return urlMap.get(shortUrl.substring(shortUrl.lastIndexOf("/") + 1));
+            return recordMap.get(shortUrl.substring(shortUrl.lastIndexOf("/") + 1));
         }
         return null;
     }
