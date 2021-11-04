655f20002de4fc2b830d33ae56062c04788f9ca5
gewenmao <gewenmao@gmail.com>
2021-11-04 14:36:03 +0800

    feat: app carousel page

---
+++ frontend/src/App.data.ts
@@ -0,0 +1,25 @@
+import iphoneImage from './assets/iphone.png';
+import tabletImage from './assets/tablet.png';
+import airpodsImage from './assets/airpods.png';
+
+const data = [{
+  id: 'xPhone',
+  title: ['xPhone'],
+  text: ['Lots to love.Less to spend.', 'Starting at $399.'],
+  image: iphoneImage,
+  theme: 'dark',
+}, {
+  id: 'Tablet',
+  title: ['Tablet'],
+  text: ['Just the right amount of everything.'],
+  image: tabletImage,
+  theme: 'light',
+}, {
+  id: 'arpods',
+  title: ['Buy a Tablet or xPhone for collage.', 'Get arPods.'],
+  text: [],
+  image: airpodsImage,
+  theme: 'light',
+}]
+
+export default data;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,36 @@
 import React from "react";
+import Carousel from "./Carousel";
+import cardsData from './App.data';
+
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return (
+    <div className="App">
+      <Carousel speed={5}>
+        {cardsData.map(item => (
+          <div
+            key={item.id}
+            className={`product-card ${item.theme}`}
+            style={{
+              backgroundImage: `url(${item.image})`
+            }}
+          >
+            <div className="product-title">
+              {item.title.map(
+                (title, index) => (<div key={index}>{title}</div>)
+              )}
+            </div>
+            <div className="product-text">
+              {item.text.map(
+                (text, index) => (<div key={index}>{text}</div>)
+              )}
+            </div>
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
 }
 
 export default App;
