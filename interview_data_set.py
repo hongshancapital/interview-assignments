ef1fa49ff520e2b64b8d9e840a1e5265f36efa41
LaoGuy <93748118+LaoGuy@users.noreply.github.com>
2021-11-05 09:24:14 +0800

    Update interview_data_set.py

--- interview_data_set.py
+++ interview_data_set.py
@@ -7,8 +7,6 @@ def main(file,url):
     new_list=quchong(json_dict)   #去重
     new_list=count(json_dict,new_list)  #计数
     send_json(new_list,url)   #post
-    # print(new_list)
-    # print(json_dict)
 
 def read_file(file):   #读取文件
     json_dict=[]
@@ -29,7 +27,7 @@ def read_file(file):   #读取文件
             text=pre_text+text         #特殊行合并
         json_dict.append(analysis(text))      #将每行数据发给模块做分析
         pre_text=text
-        # print(pre_text)
+        
     return json_dict
 
 def quchong(list):     #去重操作
@@ -40,12 +38,11 @@ def quchong(list):     #去重操作
     return new_list
 
 def count(list,new_list):  #统计，利用list count来做统计
-    # print(new_list)
+ 
         for it in new_list:
             total=list.count(it)
             it['numberOfOccurrence']=total
-            # if total>1:
-            #     print(it)
+           
         return new_list
 
 def send_json(list,url):     #将json数据post
@@ -53,15 +50,13 @@ def send_json(list,url):     #将json数据post
         try:
             response = requests.post(url=url, headers=headers, data=json.dumps(list),verify=False)
             if response.status_code == 200:
-                # return response.json()
-                print(response)
+                print('Post Done')
             else:
                 print(response)
         except requests.ConnectionError as e:
             print('Error', e.args)
 
 def analysis(dump):  #提取文件中的关键信息，生成字典
-    # print(dump)
     data={}
     data['deviceName']=dump.split()[3]
     data['processName']=re.match('^[a-zA-Z\.]*',dump.split()[4]).group()
@@ -79,5 +74,4 @@ def analysis(dump):  #提取文件中的关键信息，生成字典
 if __name__=='__main__':
     file='C:/Users/josh.wang/PycharmProjects/Net Automation/interview_data_set'
     url='https://foo.com/bar'
-    # url="http://coolaf.com"
     main(file,url)
