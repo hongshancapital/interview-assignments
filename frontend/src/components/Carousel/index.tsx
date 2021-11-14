a6926f21d6e02aa4758ca52c2532bd12a639500f
赵永宁 <zhaoyongning.zhaoyn@bytedance.com>
2021-11-14 16:36:10 +0800

    feat: 完成组件开发

--- frontend/src/components/Carousel/index.tsx
+++ frontend/src/components/Carousel/index.tsx
@@ -2,29 +2,35 @@ import React, { useState, useEffect, useRef, useMemo, ReactNode } from 'react';
 import { setInterval } from 'timers';
 import './style.css';
 
+let timer: ReturnType<typeof setInterval>;
+const INITIAL_STEP = 0;
+
 export type CarouselDataItem = {
  key: string | number;
- content: ReactNode | string;
- render?: (current: CarouselDataItem['key']) => ReactNode | string;
+ content:
+  | ReactNode
+  | string
+  | ((current: CarouselDataItem['key']) => ReactNode | string);
 };
 
 export interface CarouselProps {
+ wrapClassName?: string | string[];
  options: CarouselDataItem[];
  during?: number;
  easing?: 'linear' | 'ease' | 'ease-in' | 'ease-out' | 'ease-in-out';
 }
-let timer: ReturnType<typeof setInterval>;
+
 export default function Carousel({
+ wrapClassName,
  options,
  during = 3000,
- easing = 'linear',
+ easing = 'ease-in',
 }: CarouselProps) {
- const [current, setCurrent] = useState(0);
  const timerCallback = useRef<() => void>();
+ const [current, setCurrent] = useState(INITIAL_STEP);
 
  timerCallback.current = () => {
-  console.log(current === options.length - 1 ? 0 : current + 1);
-  setCurrent(current === options.length - 1 ? 0 : current + 1);
+  setCurrent(current === options.length - 1 ? INITIAL_STEP : current + 1);
  };
 
  useEffect(() => {
@@ -39,24 +45,51 @@ export default function Carousel({
  }, [during]);
 
  const transitionStyle = useMemo(() => {
-  return `transform 1s ${current === options.length - 1 ? 'unset' : easing}`;
- }, [options, easing, current]);
+  return `transform 0.4s ${easing}`;
+ }, [easing]);
 
  const transformStyle = useMemo(() => {
   return options.length > 0 ? `translate3d(-${current * 100}vw,0,0)` : '';
  }, [current, options]);
 
+ const progressAnimation = useMemo(() => {
+  return `progressAct ${during / 1000}s linear 0s none`;
+ }, [during]);
+
  return (
-  <div className='carousel-wrap'>
-   <div className='carousel-options-wrap' style={{ transition: transitionStyle, transform: transformStyle }}>
-    {options.map(({ key, content, render }, index) => {
+  <div
+   className={`carousel-wrap ${
+    wrapClassName
+     ? typeof wrapClassName === 'string'
+       ? wrapClassName
+       : wrapClassName.join(' ')
+     : ''
+   }`}
+  >
+   <div
+    className='carousel-options-wrap'
+    style={{ transition: transitionStyle, transform: transformStyle }}
+   >
+    {options.map(({ key, content }, index) => {
      return (
       <div
        className='carousel-option'
        key={key}
        style={{ left: `${index * 100}vw` }}
       >
-       {render ? render(key) : content}
+       {typeof content === 'function' ? content(key) : content}
+      </div>
+     );
+    })}
+   </div>
+   <div className='carousel-progress-wrap'>
+    {options.map(({ key }, index) => {
+     return (
+      <div className='carousel-progress-outer' key={key}>
+       <div
+        className='carousel-progress-inner'
+        style={current === index ? { animation: progressAnimation } : {}}
+       />
       </div>
      );
     })}
