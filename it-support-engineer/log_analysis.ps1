6413f85bbb396a6470cf44a2f751ff06053f0145
Lionel <congen_406@163.com>
2021-11-16 21:55:48 +0800

    IT Support Engineer - Lionel Cong 2nd push

--- it-support-engineer/log_analysis.ps1
+++ it-support-engineer/log_analysis.ps1
@@ -1,13 +1,23 @@
-"""
-Author:Lionel
+<#
+Author:从恩Lionel
 Tel: +86 13821818560
-"""
-"""
+
 思路：
-1. 按行读取log文件，用空格分割每一行字符串
-2. 
-"""
+1. 按行读取log文件,查找符合要求的error日志
+2. 每一行重新组合成两个字符串，分别装进两个数组，event_list和log_arr
+    deviceName____processName____description____timeWindow
+    deviceName____processName____description____timeWindow____processId
+3. 用event_list中的每一项去比对log_arr中的每一项，前四个键完全一致的，
+   把processId装进一个数组，并统计processId的个数numberOfOccurrence。
+4. 每一次内层循环结束，按字典格式整理数据，放进result数组里。
+5. 把result数组转换成json格式，上传到服务器。
+#>
 Function format_time($time)
+<#
+时间处理函数，把形如“hh:mm:ss”的时间转换成“XX00-XX00”的格式
+1. 补全两位：如果时间在1-9点，就前面补个0；如果是10-12，就不做操作
+2. 时间后面再补2个0，与时间+1的值组成“XX00-XX00”的格式
+#>
 {
     $split_time=$time.split(':')
     $hour1=[int]$split_time[0]
@@ -29,7 +39,7 @@ Function format_time($time)
     {
         $hour2=$hour2.ToString()+'00'
     }
-    
+
     $time_str=$hour1+'-'+$hour2
 
     return $time_str
@@ -38,15 +48,16 @@ Function format_time($time)
 $log_file='.\interview_data_set'
 $log=Get-Content $log_file
 
-# 创建数组，准备按行接收日志数据
+# 创建数组，准备按行接收日志字符串
 $log_arr=New-Object -TypeName System.Collections.ArrayList
-
+# 创建数组，准备按行接收事件字符串
 $event_list=New-Object -TypeName System.Collections.ArrayList
+#按行匹配，找到有形如“(xxx[xxx]):xxx”的字符串的行，组合成两个字符串，分别放进数组
 ForEach($line in $log)
 {
     $line_split=$line.split(" ",7)
-    $line_split[5] -match '\((.*)\[(\d.*)\]\)\:'
-    if($matches)
+    $check=$line_split[5] -match '\((.*)\[(\d.*)\]\)\:'
+    if($check)
     {
 	$deviceName = $line_split[3]
 	$time = $line_split[2]
@@ -55,41 +66,48 @@ ForEach($line in $log)
 	$processName = $matches[1]
 	$description = $line_split[6]
 
-        $event=$deviceName+'_@'+$processName+'_@'+$description+'_@'+$timeWindow
+        $event=$deviceName+'____'+$processName+'____'+$description+'____'+$timeWindow
         if ($event_list -notcontains $event){$event_list.Add($event)}
-	$log_all=$event+'_@'+$processId
+	$log_all=$event+'____'+$processId
 	$log_arr.Add($log_all)
-        
+
     }
 }
-
-
+# 创建数组，准备接收统计好的事件字典
 $result=New-Object -TypeName System.Collections.ArrayList
-
+#外层循环遍历事件，即不包含processId和发生次数的其他信息。
 
 foreach($i in $event_list)
 {
    $processId_list=New-Object -TypeName System.Collections.ArrayList
+   [System.Collections.Generic.Dictionary[String,Object]]$error=@{}
    $numberOfOccurrence=0
+   #外层遍历每次创建新的数组接收所有processId，创建新的字典，numberOfOccurrence归零，防止重复统计。
+   $i_split=$i.Split("____",[System.StringSplitOptions]::RemoveEmptyEntries)
+   #日志内容末尾有换行符，分隔完字符串要去掉空格，不然拿索引取不到正确的值。
    foreach($j in $log_arr)
     {
-        if (($i.Split('_@')[0] -eq $j.Split('_@')[0]) -and ($i.Split('_@')[1] -eq $j.Split('_@')[1]) -and ($i.Split('_@')[2] -eq $j.Split('_@')[2]) -and ($i.Split('_@')[3] -eq $j.Split('_@')[3]))
+        #内层循环每次遍历完log_arr数组，做好统计再把总的结果放进result数组
+        $j_split=$j.Split("____",[System.StringSplitOptions]::RemoveEmptyEntries)
+        if (($i_split[0] -eq $j_split[0]) -and ($i_split[1] -eq $j_split[1]) -and ($i_split[2] -eq $j_split[2]) -and ($i_split[3] -eq $j_split[3]))
         {
-           $processId_list.Add($j.Split('_@')[4])
-           $numberOfOccurrence +=1           
+           $processId_list.Add($j_split[4])
+           $numberOfOccurrence +=1
         }
     }
-    $error = New-Object -TypeName PSObject -Property $properties @{
-	'deviceName' = $i.Split('_@')[0]
-    	'processId' = $processId_list
-    	'processName' = $i.Split('_@')[1]
-    	'description' = $i.Split('_@')[2]
-    	'timeWindow' = $i.Split('_@')[3]
-    	'numberOfOccurrence' = $numberOfOccurrence
-	
-	}
-    
+
+	    $error['deviceName'] = $i_split[0]
+    	$error['processId'] = $processId_list
+    	$error['processName'] = $i_split[1]
+    	$error['description'] = $i_split[2]
+    	$error['timeWindow'] = $i_split[3]
+    	$error['numberOfOccurrence'] = $numberOfOccurrence
+
+
     $result.Add($error)
 }
-$output = $result | Convertto-Json | Out-File .\json_output.txt
+#处理完毕，转换成json格式，并保存txt文件在脚本目录下。
+$output = $result | Convertto-Json | Out-File .\ps_output.txt
+#处理结果上传服务器
 Invoke-WebRequest http://coolaf.com -Method POST -ContentType "application/json" -Body $output
+Read-Host "处理完毕，按任意键退出……"
--- it-support-engineer/log_analysis.py
+++ it-support-engineer/log_analysis.py
@@ -22,7 +22,8 @@ for i in range(len(lines)):
         # 不符合特征的第二种情况：正则分割分不出来第三个元素，out of range。
         print(i, e)
 # 准备接收整理完格式日志的列表
-formatted_lines = []
+log_list = []
+event_list=[]
 # 遍历日志，整理成目标格式。这里都是单条记录，不计数，最后统一计算。
 for item in valid_lines:
     deviceName = item[3]
@@ -36,33 +37,38 @@ for item in valid_lines:
     """
     timeWindow = item[2].split(':')[0] + '00-' + str(int(item[2].split(':')[0]) + 1).zfill(2) + '00'
     # 构造json格式数据，暂时没有processID和计数的key，那两部分需要后续处理。
-    temp = [{'deviceName': deviceName, 'processName': processName, 'description': description,
+    temp1 = [{'deviceName': deviceName, 'processName': processName, 'description': description,
              'timeWindow': timeWindow}, processId]
-    formatted_lines.append(temp)
+    temp2= {'deviceName': deviceName, 'processName': processName, 'description': description,
+             'timeWindow': timeWindow}
+    log_list.append(temp1)
+    event_list.append(temp2)
 
 # 准备事件列表：对除了processID以外，完全一样的消息去重
-event_list = []
-for item in formatted_lines:
-    if item not in event_list:
-        event_list.append(item[0])
+event_list_distinct = []
+for item in event_list:
+    if item not in event_list_distinct:
+        event_list_distinct.append(item)
 # 准备计算列表：把同时段，相同事件的processID以列表形式添加到字典里，每个事件是一个列表元素。
-calculation_list = []
-for i in event_list:
+
+result = []
+for i in event_list_distinct:
     processId_list = []
-    for j in formatted_lines:
+    for j in log_list:
         if i == j[0]:
             processId_list.append(j[1])
     i['processID'] = processId_list
-    calculation_list.append(i)
+    result.append(i)
+print(len(result))
 # 准备jason数据列表
 json_list = []
-for item in calculation_list:
+for item in result:
     item['numberOfOccurrence'] = len(item['processID'])
     # processID列表的长度就是同时段出现相同事件的次数，把它也添加到json数据结构中。
     json_list.append(item)
 
 json_str = json.dumps(json_list, ensure_ascii=False)
-with open('output.txt','w') as f1:
+with open('py_output.txt','w') as f1:
     f1.write(json_str)
 """
 最后一步一直报如下证书错误，代码被注释掉了。输出结果请参考output.txt。
@@ -70,9 +76,9 @@ requests.exceptions.SSLError: HTTPSConnectionPool(host='foo.com', port=443):
 Max retries exceeded with url: /bar (Caused by SSLError(SSLCertVerificationError(1, 
 '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: unable to get local issuer certificate (_ssl.c:1125)')))
 """
-if json_str:
-    url = "https://foo.com/bar"
-    headers = {'Content-Type': 'application/json; charset=UTF-8'}
-    res = requests.request("post", url, json=json_str, headers=headers)
-    print(res.status_code)
-    print(res.text)
+# if json_str:
+#     url = "https://foo.com/bar"
+#     headers = {'Content-Type': 'application/json; charset=UTF-8'}
+#     res = requests.request("post", url, json=json_str, headers=headers)
+#     print(res.status_code)
+#     print(res.text)
