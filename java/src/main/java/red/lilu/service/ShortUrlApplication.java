b93b97361031bed33842ee6f2fc36f2e78af105b
李小明 <alx696@gmail.com>
2021-11-18 20:44:52 +0800

    提交作业

---
+++ java/src/main/java/red/lilu/service/ShortUrlApplication.java
@@ -0,0 +1,13 @@
+package red.lilu.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortUrlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShortUrlApplication.class, args);
+  }
+
+}
