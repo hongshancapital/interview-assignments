e354d471f212bedf991b57aa6ab93503449ed0b9
张亮 <48906088@qq.com>
2021-12-01 22:47:33 +0800

    -- feature：init project 1. 初始化项目

---
+++ java/ShortUrlService/src/main/java/com/zhangliang/suril/ShortUrlServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zhangliang.suril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortUrlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortUrlServiceApplication.class, args);
+    }
+
+}
