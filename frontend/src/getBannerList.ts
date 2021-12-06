04a8d8f899689046d451f1e9bce81114425803ae
eric <eric@xiaohongshu.com>
2021-12-07 01:14:55 +0800

    feat: del useless file

--- frontend/src/getBannerList.ts
+++
@@ -1,7 +0,0 @@
-import pic1 from "./assets/pic1.jpeg";
-import pic2 from "./assets/pic2.jpeg";
-import pic3 from "./assets/pic3.jpeg";
-
-export const getBannerList = () => {
-  return [pic1, pic2, pic3]
-};
--- frontend/src/getSlideDataList.ts
+++ frontend/src/getSlideDataList.ts
@@ -1,32 +1,34 @@
-import { Slide } from "./types/Slide"
-import iphoneImage from './assets/iphone.png'
-import airpodsImage from './assets/airpods.png'
-import tabletImage from './assets/tablet.png'
+import { Slide } from "./types/Slide";
+import iphoneImage from "./assets/iphone.png";
+import airpodsImage from "./assets/airpods.png";
+import tabletImage from "./assets/tablet.png";
 
-export const getSlideDataList=()=>{
-  const result:Slide[]=[{
-    id: 'iphone',
-    title: ['xPhone'],
-    content: ['Lots to love. Less to spend.', 'Starting at $399.'],
-    image: iphoneImage,
-    color: '#ffffff',
-    backgroundColor: '#111111'
-  },
-  {
-    id: 'tablet',
-    title: ['Tablet'],
-    content: ['Just the right amount of everything.'],
-    image: tabletImage,
-    color: '#000000',
-    backgroundColor: '#fafafa'
-  },
-  {
-    id: 'airpods',
-    title: ['Buy a Tablet or xPhone for college.', 'Get airPods'],
-    content:[],
-    image:  airpodsImage,
-    color: '#000',
-    backgroundColor: '#f1f1f3'
-  }]
-  return result
-}
+export const getSlideDataList = () => {
+  const result: Slide[] = [
+    {
+      id: "iphone",
+      title: ["xPhone"],
+      content: ["Lots to love. Less to spend.", "Starting at $399."],
+      image: iphoneImage,
+      color: "#ffffff",
+      backgroundColor: "#111111",
+    },
+    {
+      id: "tablet",
+      title: ["Tablet"],
+      content: ["Just the right amount of everything."],
+      image: tabletImage,
+      color: "#000000",
+      backgroundColor: "#fafafa",
+    },
+    {
+      id: "airpods",
+      title: ["Buy a Tablet or xPhone for college.", "Get airPods"],
+      content: [],
+      image: airpodsImage,
+      color: "#000",
+      backgroundColor: "#f1f1f3",
+    },
+  ];
+  return result;
+};
