aad8adeac79a5406f6b9f824f703e27be51708da
Patrick <patrick.wang@geekbang.org>
2021-12-12 13:51:53 +0800

    feat: add Carousel Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,41 @@
 import React from "react";
 import "./App.css";
+import Carousel from "./components/Carousel";
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return <div className="App">
+    <Carousel duration={300} delay={3000} showPagination height={window.innerHeight+'px'}>
+        <div className="wrapper phone">
+          <div className="title-wrapper">
+            <h1 className="title white">
+              xPhone
+            </h1>
+            <p className="text white mrt20">
+              Lots to love. Less to spend. <br />
+              Starting at $399.
+            </p>
+          </div>
+        </div>
+        <div className="wrapper tablet">
+          <div className="title-wrapper">
+            <h1 className="title black">
+              Tablet
+            </h1>
+            <p className="text black mrt20">
+              Just the right amount of everything
+            </p>
+          </div>
+        </div>
+        <div className="wrapper airpods">
+          <div className="title-wrapper">
+            <h1 className="title black">
+              Buy a Tablet or xPhone for college. <br />
+              Get arPods
+            </h1>
+          </div>
+        </div>
+    </Carousel>
+  </div>;
 }
 
 export default App;
---
+++ frontend/src/components/Carousel/Carousel.tsx
@@ -0,0 +1,54 @@
+import React, {useState, useEffect, FC, useRef, CSSProperties} from 'react'
+import { ICarouselProps } from './interface'
+import Pagination from './Pagination'
+import './Carousel.css'
+import useInterval from './useInterval'
+
+
+const Carousel:FC<ICarouselProps> = (props) => {
+  const {children, showPagination, delay, duration, height} = props
+  let count = React.Children.count(children)
+
+  const [active, setActive] = useState(0)
+  const [itemWidth, setItemWidth] = useState<number>(0)
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if(wrapperRef.current){
+      setItemWidth(wrapperRef.current.clientWidth)
+    }
+  },[])
+
+  useInterval(()=>{
+    goNext(active+1)
+  }, delay)
+
+  const goNext = (nextActive:number) => {
+    nextActive = nextActive >= count ? 0 : nextActive
+    setActive(nextActive)
+  }
+
+  const wrapperStyle:CSSProperties = {
+    width: 100 * count + '%',
+    transform: `translateX(${-itemWidth * active}px)`,
+    transitionDuration: `${duration}ms`
+  }
+
+  const renderPagination = () => {
+      return showPagination ? <Pagination count={count} delay={delay} active={active} duration={duration} onClick={goNext}/> : null
+  }
+
+  return (
+    <div className='container' ref={wrapperRef} style={{height: height}}>
+      <div className='carousel-wrapper' style={wrapperStyle}>
+        {React.Children.map(children, ((child,index) => {
+          return <div className='carousel-item'>
+            {child}
+          </div>
+        }))}
+      </div>
+      {renderPagination()}
+    </div>
+  )
+}
+export default Carousel
