3a8f983542e8565328e6e253db3f6558288e99c7
whucnhg <741956824@qq.com>
2021-12-12 18:24:45 +0800

    移动测试类

--- java/shortlink/src/test/java/com/scdt/shortlink/Test.java
+++ java/shortlink/src/test/java/com/scdt/shortlink/Test.java
@@ -1,9 +1,9 @@
+package com.scdt.shortlink;
+
 import org.apache.tomcat.util.buf.StringUtils;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Test {
     public static void main(String[] args) {
