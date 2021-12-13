dbbd70fe626afdf4efa974ab546ec25a1f3b4fc6
学生李令新 <stullx@qq.com>
2021-12-13 22:07:21 +0800

    feat(test): 查询接口单元测试

--- java/shorturl/src/test/java/com/homework/shorturl/service/ShortUrlServiceTest.java
+++ java/shorturl/src/test/java/com/homework/shorturl/service/ShortUrlServiceTest.java
@@ -4,10 +4,13 @@ import com.homework.shorturl.model.LongShortMapModel;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
 
 @SpringBootTest
 class ShortUrlServiceTest {
@@ -15,10 +18,21 @@ class ShortUrlServiceTest {
     @Autowired
     private ShortUrlService service;
 
+    private static final int INITIAL_CAPACITY = 10;
+    private static final ArrayList<String> longUrls = new ArrayList<>(INITIAL_CAPACITY);
+
+    @BeforeAll
+    static void initLongUrls() {
+        for (int i = 0; i < INITIAL_CAPACITY; i++) {
+            longUrls.add(randomLongUrl());
+        }
+    }
+
     @Test
     void create() {
-        for (int i = 0; i < 5; i++) {
-            ResponseEntity<LongShortMapModel> resp = createShortUrl(randomLongUrl());
+        for (int i = 0; i < longUrls.size(); i++) {
+            String longUrl = longUrls.get(i);
+            ResponseEntity<LongShortMapModel> resp = createShortUrl(longUrl);
             Assertions.assertNotNull(resp.getBody());
             Assertions.assertEquals(String.valueOf(i), resp.getBody().getShortUrl());
         }
@@ -32,9 +46,14 @@ class ShortUrlServiceTest {
 
     @Test
     void queryLongUrl() {
+        for (int i = 0; i < longUrls.size(); i++) {
+            ResponseEntity<LongShortMapModel> resp = service.queryLongUrl(String.valueOf(i));
+            Assertions.assertNotNull(resp.getBody());
+            Assertions.assertEquals(longUrls.get(i), resp.getBody().getLongUrl());
+        }
     }
 
-    private String randomLongUrl() {
+    private static String randomLongUrl() {
         StringBuilder sb = new StringBuilder();
         sb.append("http://");
         sb.append(RandomStringUtils.random(RandomUtils.nextInt(1, 128)));
