1685f1e381ca42e54b89bcad8fddc7e5bf446db8
学生李令新 <stullx@qq.com>
2021-12-14 22:14:34 +0800

    feat(test): 查询接口单元测试
    1.超出限额时，期望返回507
    2.创建已创建过的短链时，期望返回相同的短链

--- java/shorturl/src/test/java/com/homework/shorturl/service/ShortUrlServiceTest.java
+++ java/shorturl/src/test/java/com/homework/shorturl/service/ShortUrlServiceTest.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.RandomUtils;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
@@ -19,6 +20,8 @@ class ShortUrlServiceTest {
 
     private static final int INITIAL_CAPACITY = 10;
     private static final ArrayList<String> longUrls = new ArrayList<>(INITIAL_CAPACITY);
+    private static final int MAX_MAP_LIMIT = 1_000;
+
 
     @BeforeAll
     static void initLongUrls() {
@@ -48,6 +51,36 @@ class ShortUrlServiceTest {
             Assertions.assertNotNull(resp.getBody());
             Assertions.assertEquals(longUrls.get(i), resp.getBody().getLongUrl());
         }
+    }
+
+    @Test
+    @Order(3)
+    void create_expect_503_when_create_exceed_the_limit() {
+        int remainingCapacity = MAX_MAP_LIMIT - longUrls.size();
+        for (int i = 0; i < remainingCapacity; i++) {
+            LongShortMapModel req = new LongShortMapModel();
+            String longUrl = randomLongUrl();
+            req.longUrl(longUrl);
+            service.create(req);
+            longUrls.add(longUrl);
+        }
+        LongShortMapModel req = new LongShortMapModel();
+        req.longUrl(randomLongUrl());
+        ResponseEntity<LongShortMapModel> resp = service.create(req);
+        Assertions.assertNotNull(resp);
+        Assertions.assertEquals(HttpStatus.INSUFFICIENT_STORAGE.value(), resp.getStatusCode());
+    }
+
+    @Test
+    @Order(4)
+    void create_expect_the_same_short_url_when_repeat_creat() {
+        int longUrlIndex = RandomUtils.nextInt(0, longUrls.size());
+        String longUrl = longUrls.get(longUrlIndex);
+        LongShortMapModel req = new LongShortMapModel();
+        req.longUrl(longUrl);
+        ResponseEntity<LongShortMapModel> resp = service.create(req);
+        Assertions.assertNotNull(resp.getBody());
+        Assertions.assertEquals(String.valueOf(longUrlIndex), resp.getBody().getShortUrl());
     }
 
     private static String randomLongUrl() {
