1d93ee8ca0ab8bc5881f9fc9f07167fc3b396cdb
davisliudelong <96175775+davisliudelong@users.noreply.github.com>
2021-12-16 19:30:45 +0800

    燃果—刘德龙—高级java

---
+++ java/domainmapping/src/main/java/com/cn/scdt/config/RedisConfig.java
@@ -0,0 +1,56 @@
+package com.cn.scdt.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+
+@SpringBootConfiguration
+@EnableCaching
+@Slf4j
+public class RedisConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.redis.pool")
+    public JedisPoolConfig getRedisConfig(){
+        JedisPoolConfig config = new JedisPoolConfig();
+        return config;
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix="spring.redis")
+    public JedisConnectionFactory getConnectionFactory(){
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        JedisPoolConfig config = getRedisConfig();
+        factory.setPoolConfig(config);
+        log.info("JedisConnectionFactory bean init success.");
+        return factory;
+    }
+
+    @Bean
+    @Primary
+    public RedisTemplate redisTemplate() {
+        JedisConnectionFactory jedisConnectionFactory = getConnectionFactory();
+        StringRedisTemplate template = new StringRedisTemplate(jedisConnectionFactory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
---
+++ java/domainmapping/src/main/java/com/cn/scdt/utils/ShortUuidUtils.java
@@ -0,0 +1,22 @@
+package com.cn.scdt.utils;
+
+import java.util.UUID;
+
+/**
+ * 生成8位字符串工具类
+ */
+public class ShortUuidUtils {
+
+    public static String[] chars = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+    public static synchronized String generateShortUuid() {
+        StringBuffer shortBuffer = new StringBuffer();
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        for (int i = 0; i < 8; i++) {
+            String str = uuid.substring(i * 4, i * 4 + 4);
+            int x = Integer.parseInt(str, 16);
+            shortBuffer.append(chars[x % 0x3E]);
+        }
+        return shortBuffer.toString();
+    }
+}
