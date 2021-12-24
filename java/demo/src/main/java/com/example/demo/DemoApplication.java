584bac7edbaa51cdd408375f9351c4a5b5f2a465
tomcat.dai <tomcat.dai@shijigroup.com>
2021-12-24 15:49:23 +0800

    提交作业

--- java/demo/src/main/java/com/example/demo/DemoApplication.java
+++ java/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,27 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main( String[] args ) {
-        SpringApplication.run(DemoApplication.class, args);
+    public static void main( String[] args ) throws UnknownHostException {
+        ApplicationContext ctx=SpringApplication.run(DemoApplication.class, args);
+        Environment env=ctx.getEnvironment();
+        String ip=InetAddress.getLocalHost().getHostAddress();
+        String port=env.getProperty("server.port");
+        String path=env.getProperty("server.servlet.context-path");
+        System.out.print("\n----------------------------------------------------------\n\t" +
+                "Application 基础服务模块启动正常! \n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                "Swagger-UI: \t\thttp://" + ip + ":" + port + path + "/doc.html\n" +
+                "----------------------------------------------------------");
     }
 
 }
--- java/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ java/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,74 @@
 package com.example.demo;
 
+import com.example.demo.service.StorageService;
+import com.example.demo.service.UrlService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import javax.annotation.Resource;
+
+/**
+ * @Author: dsm
+ * @Description: 单元测试类
+ * @Date Create in 2021/12/23
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.DEFINED_PORT)
 class DemoApplicationTests {
+    @Resource
+    private UrlService urlService;
+    @Resource
+    private StorageService storageService;
+    @Resource
+    private TestRestTemplate restTemplate;
+    @Resource
+    private ObjectMapper objectMapper;
+    @Test
+    void shortUrl() {
+        String[] resUrl=urlService.shortUrl("https://zhidao.baidu.com/");
+        for (String url : resUrl) {
+            System.out.println(url);
+        }
+    }
 
     @Test
-    void contextLoads() {
+    void storageService() {
+        String resUrl=storageService.AreaNameStorage("https://zhidao.baidu.com/");
+        System.out.println(resUrl);
+        String url=storageService.AreaNameRead(resUrl);
+        System.out.println(url);
+    }
+
+    @Test
+    void areaNameStorag() {
+        HttpHeaders httpHeaders=new HttpHeaders();
+        String result1=this.restTemplate.postForObject("/areaNameController/areaNameStorag?areaName=https://zhidao.baidu.com/", new HttpEntity <String>(httpHeaders), String.class);
+        System.out.println(result1);
+    }
+
+    @Test
+    void AreaNameController() throws JsonProcessingException {
+        HttpHeaders httpHeaders=new HttpHeaders();
+        httpHeaders.add(HttpHeaders.AUTHORIZATION, "token");
+        String result1=this.restTemplate.postForObject("/areaNameController/areaNameStorag?areaName=https://zhidao.baidu.com/", new HttpEntity <String>(httpHeaders), String.class);
+        JsonNode jsonNode=objectMapper.readTree(result1);
+        int code=jsonNode.get("code").intValue();
+        if (code == 200) {
+            String url="/areaNameController/areaNameRead?areaName=" + jsonNode.get("data").asText();
+            String result2=this.restTemplate.postForObject(url, new HttpEntity <String>(httpHeaders), String.class);
+            System.out.println(result2);
+        } else {
+            System.out.println(result1);
+        }
+
     }
 
 }
