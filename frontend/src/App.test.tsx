ad6cb468d7d5a1f00469fc22c683f28dfe331f76
tanjing9406 <1157185306@qq.com>
2021-12-25 11:46:48 +0800

    Test: fixed App test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('render App successfully', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Just the right amount of everything./i);
   expect(linkElement).toBeInTheDocument();
 });
