d621e852a44431791fbf17bdca90a55771b19263
sequoia1225 <sequoia1225@126.com>
2021-12-26 00:04:56 +0800

    EnableSwagger2

--- src/main/java/com/sequoia/domain/ShortDomainServiceApplication.java
+++ src/main/java/com/sequoia/domain/ShortDomainServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = "com.sequoia.domain")
-//@EnableSwagger2
+@EnableSwagger2
 public class ShortDomainServiceApplication {
 
     public static void main(String[] args) {
