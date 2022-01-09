21bb77aca676fb0240c64c401033d02dc8a8c62e
liusong <liusong6@xiaomi.com>
2022-01-09 16:57:32 +0800

    接入Swagger

--- java/src/main/java/com/tizzy/business/controller/ShortUrlController.java
+++ java/src/main/java/com/tizzy/business/controller/ShortUrlController.java
@@ -4,16 +4,23 @@ import com.tizzy.business.dto.UrlLongRequest;
 import com.tizzy.business.exception.EntityNotFoundException;
 import com.tizzy.business.response.ResponseMessage;
 import com.tizzy.business.service.ShortUrlService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.net.URI;
 
+@Api(tags = "短域名服务")
 @RestController
-@RequestMapping("/shorturl/api/v1")
+@RequestMapping("/url/api/v1")
 public class ShortUrlController {
 
     private static final Logger logger = LoggerFactory.getLogger(ShortUrlController.class);
@@ -21,6 +28,7 @@ public class ShortUrlController {
     @Autowired
     private ShortUrlService urlService;
 
+    @ApiOperation(value = "获取一个新的短连接", notes = "将长链接转化为短链接")
     @PostMapping("/shorten")
     public ResponseMessage convertToShortUrl(@RequestBody @Valid UrlLongRequest request) {
         String longUrl = request.getLongUrl();
@@ -30,8 +38,10 @@ public class ShortUrlController {
         return new ResponseMessage(shortUrl, longUrl, true, "short url have got");
     }
 
+    @ApiOperation(value = "重定向到原链接", notes = "将短链接转化为原链接，并重定向")
+    @ApiImplicitParam(name = "shortUrl", value = "短链接", required = true)
     @GetMapping(value = "/parse/{shortUrl}")
-    public ResponseMessage getAndRedirect(@PathVariable String shortUrl) {
+    public Object getAndRedirect(@PathVariable String shortUrl) {
         if (StringUtils.isEmpty(shortUrl) || StringUtils.isEmpty(shortUrl.trim())) {
             return new ResponseMessage("", "", false, "param shortUrl is wrong.");
         }
@@ -43,6 +53,9 @@ public class ShortUrlController {
             logger.info("not fount original url.");
             return new ResponseMessage(shortUrl, "", false, "original url is not found.");
         }
-        return new ResponseMessage(shortUrl, url, true, "original url have got.");
+
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create(url))
+                .build();
     }
 }
