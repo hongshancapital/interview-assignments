0e13a16cf1ddb8e80f6bcee9cbbe96fcedd1196a
Zhang Gang <gzhang@thoughtworks.com>
2022-01-09 16:29:07 +0800

    init

---
+++ java/zhanggang/src/main/java/interview/assignments/zhanggang/ZhanggangApplication.java
@@ -0,0 +1,13 @@
+package interview.assignments.zhanggang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhanggangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhanggangApplication.class, args);
+	}
+
+}
---
+++ java/zhanggang/src/test/java/interview/assignments/zhanggang/ZhanggangApplicationTests.java
@@ -0,0 +1,13 @@
+package interview.assignments.zhanggang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhanggangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
