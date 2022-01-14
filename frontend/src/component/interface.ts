35b32c21342e7b3481c3baffd073ec6f12b77534
李康 <16084548+LeeDou@users.noreply.github.com>
2022-01-14 19:47:00 +0800

    Update interface.ts

--- frontend/src/component/interface.ts
+++ frontend/src/component/interface.ts
@@ -1,14 +1,3 @@
-export interface Image {
-  src?: string,
-  text?: string
-}
-
-export interface Props {
-  list: Array<CarouselItemProps>,
-  height?: number,
-  width: number
-}
-
 export interface CarouselItemProps {
   src?: string,
   title?: string,
