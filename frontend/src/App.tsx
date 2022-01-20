fdc3ce5ccb17b8a85100d7d02cbabe078615687d
hjin-me <hjin.me@gmail.com>
2022-01-20 17:55:35 +0800

    feat: 先把翻转的动画做了

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,49 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect, useState } from "react";
+import "./App.scss";
+import { getImages } from "./imageset";
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  let images = getImages();
+  let [current, setCurrent] = useState(0);
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setCurrent((current + 1) % images.length);
+    }, 3000);
+    return () => clearInterval(timer);
+  });
+
+  return <div className="App">
+    <div className="scroll" style={{
+      transform: `translateX(${-current * 100}vw)`
+    }}>
+      {
+        images.map((img, index) => {
+          return <div className="page bg" style={{
+            backgroundImage: `url(${img.url})`,
+            color: img.color,
+            transform: `translateX(${index * 100}vw)`
+          }} key={index}>
+            <div className="tip">
+              <div className="title">{img.title?.reduce((p, c) => {
+                if (p.length !== 0) {
+                  p.push(<br/>);
+                }
+                p.push(c);
+                return p;
+              }, [] as (JSX.Element | string)[])}</div>
+              <div className="text">{img.text?.reduce((p, c) => {
+                if (p.length !== 0) {
+                  p.push(<br/>);
+                }
+                p.push(c);
+                return p;
+              }, [] as (JSX.Element | string)[])}</div>
+            </div>
+          </div>;
+        })
+      }
+    </div>
+  </div>;
 }
 
 export default App;
---
+++ frontend/src/imageset.ts
@@ -0,0 +1,26 @@
+import img1 from "./assets/iphone.png";
+import img2 from "./assets/tablet.png";
+import img3 from "./assets/airpods.png";
+
+interface ImageSet {
+  url: string;
+  title?: string[];
+  text?: string[];
+  color?: string;
+}
+
+export function getImages(): ImageSet[] {
+  return [{
+    url: img1,
+    title: ["xPhone"],
+    text: ["Lots to love. Less to spend.", "Starting at $399."],
+    color: "white"
+  }, {
+    url: img2,
+    title: ["Tablet"],
+    text: ["Just the right amount of everything."]
+  }, {
+    url: img3,
+    title: ["Buy a Tablet or xPhone for college.", "Get arPods."]
+  }];
+}
