88fe18857e18846e84a85c2b4f9e3e0415fcf2d4
helondeng <helondeng@tencent.com>
2022-01-20 14:22:19 +0800

    feat: App test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from '../src/App';
+import App from './App';
 
 test('app basic render', () => {
   render(<App />);
+  const container = document.querySelector('.App');
+  const carouselElement = container.children[0]
+  expect(carouselElement.className).toBe('carousel');
+  expect(carouselElement.children.length).toBe(2);
+  const slider = carouselElement.children[0]
+  expect(slider.className).toBe('carousel-slider');
+  expect(slider.children.length).toBe(3);
   expect(screen.getByText('xPhone')).toBeInTheDocument();
 });
