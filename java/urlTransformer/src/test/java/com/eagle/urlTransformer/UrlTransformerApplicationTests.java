61a35068e37fc722e148a9bf90ca4340cfa7944b
xiaobugs <29818470@qq.com>
2022-01-20 16:25:55 +0800

    assignment

---
+++ java/urlTransformer/src/test/java/com/eagle/urlTransformer/UrlTransformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eagle.urlTransformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlTransformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
