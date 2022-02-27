0780c8f846109b1a8e2dfef31079c937f6db2475
荣恒 <rongheng.rh@gmail.com>
2022-02-27 21:19:22 +0800

    feat: AppList 功能需求完成

---
+++ swift/AppListDemo/AppListDemo/AppListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppListDemoApp.swift
+//  AppListDemo
+//
+//  Created by 荣恒 on 2022/2/27.
+//
+
+import SwiftUI
+
+@main
+struct AppListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppListView()
+        }
+    }
+}
