e5b88de1678fad37318e6f15c2ac8e000b681a8b
Guang Lei <575403441@qq.com>
2022-03-11 22:30:18 +0800

    init swift project, including modifying .gitignore file

---
+++ swift/AppCollections/AppCollections/AppCollectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCollectionsApp.swift
+//  AppCollections
+//
+//  Created by Guang Lei on 2022/3/11.
+//
+
+import SwiftUI
+
+@main
+struct AppCollectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift/AppCollections/AppCollections/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppCollections
+//
+//  Created by Guang Lei on 2022/3/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
