473af4149b143bec156ac286f6bd3bb0c086d0bf
Guang Lei <575403441@qq.com>
2022-03-12 10:00:37 +0800

    finish network and search api

---
+++ swift/AppCollections/AppCollections/Network/Network.swift
@@ -0,0 +1,43 @@
+//
+//  Network.swift
+//  AppCollections
+//
+//  Created by Guang Lei on 2022/3/12.
+//
+
+import Foundation
+import Alamofire
+
+class Network {
+    
+    static let shared = Network()
+    
+    private let host = "https://itunes.apple.com"
+    
+    private let decoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+    
+    typealias HTTPMethod = Alamofire.HTTPMethod
+    typealias Parameters = Alamofire.Parameters
+    
+    func request<DecodableType: Decodable>(
+        path: String,
+        method: HTTPMethod = .get,
+        parameters: Parameters? = nil,
+        decodableType: DecodableType.Type = DecodableType.self
+    ) async throws -> DecodableType {
+        guard let url = URL(string: "\(host)\(path)") else {
+            let message = "[Error] Network url is not correct, host: \(host), path: \(path) "
+            fatalError(message)
+        }
+        do {
+            return try await AF.request(url, method: method, parameters: parameters).serializingDecodable(DecodableType.self).value
+        } catch {
+            throw error
+        }
+        
+    }
+}
---
+++ swift/AppCollections/AppCollections/Network/SearchNetwork.swift
@@ -0,0 +1,28 @@
+//
+//  SearchNetwork.swift
+//  AppCollections
+//
+//  Created by Guang Lei on 2022/3/12.
+//
+
+import Foundation
+
+/**
+ itunes search api 接口文档：https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api
+ */
+
+class SearchNetwork {
+
+    func searchChatApp<DecodableType: Decodable>(
+        limit: Int,
+        decodableType: DecodableType.Type = DecodableType.self
+    ) async throws -> DecodableType {
+        let path = "/search"
+        let parameters: Network.Parameters = [
+            "entity": "software",
+            "term": "chat",
+            "limit": limit
+        ]
+        return try await Network.shared.request(path: path, parameters: parameters, decodableType: DecodableType.self)
+    }
+}
