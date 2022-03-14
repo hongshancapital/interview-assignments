26329137e0a4e0cb1414f4da0bb21801a15b23e6
张泽群 <zhangzequn@sensetime.com>
2022-03-14 11:27:01 +0800

    job for TypeScript Frontend Engineer Assignment

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders App main entry', () => {
+  const ComponentBody = render(<App />);
+  console.log('——————App component test start here——————')
+  const carousel1Element = ComponentBody.getByText('xPhone')
+  const carousel2Element = ComponentBody.getByText('Tablet')
+  expect(carousel1Element).toBeInTheDocument()
+  expect(carousel2Element).toBeInTheDocument()
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,48 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
+import Carousel from './components/Carousel'
+import airPods from './assets/airpods2.png'
+import iphone from './assets/iphone2.png'
+import tablet from './assets/tablet2.png'
+import './App.css'
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  const contents: React.ReactNode[] = []
+  contents.push(
+    <div className="component-cell component-cell-1">
+      <div>
+        <h1>xPhone</h1>
+        <div>Lots to love. Less to spend.</div>
+        <div>Starting at $399.</div>
+      </div>
+      <img className="imgs" src={iphone} width={76} />
+    </div>,
+  )
+  contents.push(
+    <div className="component-cell component-cell-2">
+      <div>
+        <h1>Tablet</h1>
+        <div>Just the right amount of everything.</div>
+      </div>
+      <img className="imgs" src={tablet} width={76} />
+    </div>,
+  )
+  contents.push(
+    <div className="component-cell component-cell-3">
+      <h1>
+        Buy a Tablet or xPhone for colleage.
+        <br />
+        Get airPods
+      </h1>
+      <img className="imgs" src={airPods} width={76} />
+    </div>,
+  )
+  return (
+    <div className="App">
+      <div className="component-container">
+        <Carousel contents={contents} />
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
