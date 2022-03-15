9ff55538b87ecb0a75851be8a7654cdf93e5dbb7
hl <1715185444@qq.com>
2022-03-15 16:54:28 +0800

    init

---
+++ swift/ListViewDemo/ListViewDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListViewDemo
+//
+//  Created by HL on 2022/3/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift/ListViewDemo/ListViewDemo/ListViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewDemoApp.swift
+//  ListViewDemo
+//
+//  Created by HL on 2022/3/15.
+//
+
+import SwiftUI
+
+@main
+struct ListViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
