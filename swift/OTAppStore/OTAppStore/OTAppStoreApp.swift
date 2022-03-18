8d9ac84416269b59fdf2b9b3107c236806e7b866
liuxiaojie <liuxiaojie@xiaomi.com>
2022-03-18 21:00:26 +0800

    [Init] init SwiftUI Project

---
+++ swift/OTAppStore/OTAppStore/OTAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTAppStoreApp.swift
+//  OTAppStore
+//
+//  Created by liuxj on 2022/3/18.
+//
+
+import SwiftUI
+
+@main
+struct OTAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
