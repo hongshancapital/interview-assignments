2e4b80be49043cdb2af211c80ec9ca059c227aa2
xiuyuan <xiuyuan@byai.com>
2022-03-21 23:03:38 +0800

    短链系统

--- short-link-center/src/main/java/com/sequoiacap/shortlinkcenter/common/exception/CommonException.java
+++ short-link-center/src/main/java/com/sequoiacap/shortlinkcenter/common/exception/CommonException.java
@@ -1,13 +1,3 @@
-/*
- *
- *  Copyright 2020 byai.com All right reserved. This software is the
- *  confidential and proprietary information of byai.com ("Confidential
- *  Information"). You shall not disclose such Confidential Information and shall
- *  use it only in accordance with the terms of the license agreement you entered
- *  into with byai.com.
- * /
- */
-
 package com.sequoiacap.shortlinkcenter.common.exception;
 
 /**
