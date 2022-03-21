d17fc544a9a462f2f57a593b88f9da96157b726d
liuxiaojie <liuxiaojie@xiaomi.com>
2022-03-21 11:02:54 +0800

    [Update] restore favorite state after refresh

--- swift/OTAppStore/OTAppStore/ViewModel/OTAppViewModel.swift
+++ swift/OTAppStore/OTAppStore/ViewModel/OTAppViewModel.swift
@@ -21,6 +21,7 @@ class OTAppViewModel: ObservableObject {
     @Published var hasMoreData: Bool = false
     var hasError: Bool = false
     var errorMessage: String?
+    var favoriteIds: [Int] = [Int]()
     
     private let requestPath = "https://itunes.apple.com/search"
     private var requestOffset = 0
@@ -48,25 +49,47 @@ class OTAppViewModel: ObservableObject {
         if let index = appModelList.firstIndex(where:  { $0.id == id }) {
             appModelList[index].isFavorite.toggle()
         }
+        
+        if let favoriteIndex = favoriteIds.firstIndex(where: { $0 == id }) {
+            favoriteIds.remove(at: favoriteIndex)
+        } else {
+            favoriteIds.append(id)
+        }
     }
     
     //MARK: Helper
-    //接口请求到的有重复数据，添加前需要先过滤
-    private func mergeAppModelList(with appendList: [AppModel]) {
-        let result = appendList.filter { item in
-            return appModelList.firstIndex(where: { $0.id == item.id }) == nil
+    //添加offset后接口请求到的有重复数据，添加前需要先过滤
+    private func mergeAppModelList(with appendList: [AppModel], isRefresh: Bool) {
+        var headList: [AppModel]
+        var tailList: [AppModel]
+        
+        //刷新时，原始list重置，新list不需要过滤
+        if isRefresh {
+            headList = [AppModel]()
+            tailList = appendList
+        } else {
+            headList = appModelList
+            tailList = appendList.filter { item in
+                return headList.firstIndex(where: { $0.id == item.id }) == nil
+            }
         }
-        appModelList.append(contentsOf: result)
+        
+        //恢复点赞状态
+        favoriteIds.forEach { id in
+            if let index = tailList.firstIndex(where: { $0.id == id }) {
+                tailList[index].isFavorite.toggle()
+            }
+        }
+        
+        headList.append(contentsOf: tailList)
+        //只赋值一次，避免多次外界view多次刷新
+        appModelList = headList
     }
     
     private func doRequest(with requestParams: OTNetworkParams, isRefresh: Bool = false) async {
         do {
             let appData: AppData = try await OTNetwork.shared.getData(from: requestPath, params: requestParams)
-            if isRefresh {
-                appModelList = appData.results
-            } else {
-                mergeAppModelList(with: appData.results)
-            }
+            mergeAppModelList(with: appData.results, isRefresh: isRefresh)
             requestOffset = appModelList.count
             //FIXME: 这里为了还原视频效果，手动限制加载数量
             hasMoreData = (requestOffset < appModelCountLimit)
