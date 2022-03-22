992385a3e081cd5e86cf66e765e249f310e3fe97
liupingwei <liupingwei@shizhuang-inc.com>
2022-03-22 20:48:14 +0800

    项目创建

---
+++ swift/App/App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by august on 2022/3/22.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
