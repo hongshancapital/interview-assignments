760e362ffd09401024d4e64c7ea18e712e684752
Fu Yin <fooying@qq.com>
2022-03-23 11:11:25 +0800

    frontend

--- fullstack/admin/src/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic"
-
-const ReactAdmin = dynamic(() => import("admin/index"), {
-  ssr: false,
-})
-
-const HomePage = () => <ReactAdmin />
-
-export default HomePage
--- fullstack/admin/src/utils/crypt.ts
+++
@@ -1,6 +0,0 @@
-export function cryptPwd(password: string): string {
-  const crypto = require('crypto');
-  const md5 = crypto.createHash('md5');
-  const res = md5.update(password).digest('hex')
-  return res;
-}
