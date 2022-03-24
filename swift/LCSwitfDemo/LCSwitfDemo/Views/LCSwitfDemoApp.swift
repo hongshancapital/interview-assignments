5f53430d60a5561888f57128a97d802ed781ee3e
liangjie <liang.jie@olaola.chat>
2022-03-24 11:17:48 +0800

    SwiftDemo-梁杰

---
+++ swift/LCSwitfDemo/LCSwitfDemo/Views/LCSwitfDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCSwitfDemoApp.swift
+//  LCSwitfDemo
+//
+//  Created by 梁杰 on 2022/3/23.
+//
+
+import SwiftUI
+
+@main
+struct LCSwitfDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LCHomeListView()
+        }
+    }
+}
