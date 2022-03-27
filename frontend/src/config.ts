43825ce48b1886e6fb198330da5a826be6f691b2
zhangnan <zhangnan@yunify.com>
2022-03-27 12:09:25 +0800

    feat: commit carousel code

---
+++ frontend/src/config.ts
@@ -0,0 +1,12 @@
+import banner1 from "./assets/airpods.png";
+import banner2 from "./assets/iphone.png";
+import banner3 from "./assets/tablet.png";
+import { ListProps } from "./interface";
+
+const list: ListProps[] = [
+  { url: banner1 },
+  { url: banner2 },
+  { url: banner3 },
+];
+
+export default list;
