cc7a59207a680d727f4a08e599c00243c78613d4
curryfu <curryfu@tencent.com>
2022-03-31 12:11:44 +0800

    performance:优化样式及ts

--- frontend/src/components/Slide/Slide.tsx
+++ frontend/src/components/Slide/Slide.tsx
@@ -1,61 +1,39 @@
 import React from "react";
 import "./Slide.css";
-import { CarouselBannerConfig } from "../../config/Carousel";
+import {CarouselBannerConfig, CarouselBannerConfigInter, ContentItem} from "../../config/Carousel";
+
 interface SlideProps {
     index?: number;
     transitionTime?: number;
 }
+
 const Indicator: React.FC<SlideProps> = (props: SlideProps) => {
-    const { index = 0, transitionTime = 1000 } = props;
+    const {index = 0, transitionTime = 1000} = props;
     return (
-        <div className="wrap" style={{ left: `-${100 * index}vw`, transition: `left ease ${transitionTime}ms` }}>
+        <div className="wrap" style={{left: `-${100 * index}vw`, transition: `left ease ${transitionTime}ms`}}>
             {
-                CarouselBannerConfig.map((value: any) => {
+                CarouselBannerConfig.map((value: CarouselBannerConfigInter) => {
                     return (
-                        <div className="banner" key={value.id}>
-                            <img src={value.imageUrl} alt="Tablet or xPhone"/>
+                        <div className="banner" key={value.id} style={{background: value.backgroundColor}}>
+                            {value.id === 'xPhone' ?
+                                <img src={value.imageUrl} alt="Tablet or xPhone" style={{objectFit: 'contain'}}/> :
+                                <img src={value.imageUrl} alt="Tablet or xPhone" style={{objectFit: 'cover'}}/>}
+
                             <div className="text">
-                            {
-                                value.content.map((value: any) => {
-                                    return (
-                                        <span style={{...value.style}}>
-                                            {value.name}
-                                        </span>
-                                    )
-                                })
-                            }
+                                {
+                                    value.content.map((value: ContentItem) => {
+                                        return (
+                                            <span style={{...value.style}}>
+                            {value.name}
+                                </span>
+                                        )
+                                    })
+                                }
                             </div>
                         </div>
                     )
                 })
             }
-            {/* <div className="start">
-                <span>
-                    xPhone
-                </span>
-                <span>
-                    Lots to love. Less to spend.
-                </span>
-                <span>
-                    Starting at $399.
-                </span>
-            </div>
-            <div className="middle">
-                <span>
-                    Tablet
-                </span>
-                <span>
-                    Just the right amount of everything
-                </span>
-            </div>
-            <div className="end">
-                <span>
-                    Buy a Tablet or xPhone for college.
-                </span>
-                <span>
-                    Get arPods.
-                </span>
-            </div> */}
         </div>
     )
 };
--- frontend/src/config/Carousel.ts
+++ frontend/src/config/Carousel.ts
@@ -1,13 +1,31 @@
 import airpods from "../assets/airpods.png";
 import iphone from "../assets/iphone.png";
 import tablet from "../assets/tablet.png";
-export const CarouselConfig = { // 用于配置轮播图
+
+interface CarouselConfigInter {
+    indicatorIsShow: boolean;
+    delay: 3000;
+    initialIndex: 0;
+    transitionTime: 1000;
+}
+
+export type ContentItem = { name: string } & { style: Record<string, string | Number> }
+
+export interface CarouselBannerConfigInter {
+    id: string;
+    content: ContentItem[];
+    imageUrl: string;
+    color: string;
+    backgroundColor: string;
+}
+
+export const CarouselConfig: CarouselConfigInter = { // 用于配置轮播图
     indicatorIsShow: true, // 指示器是否展示
     delay: 3000, // 轮播图展示时间 （单位毫秒）
     initialIndex: 0, //初始展示轮播图
     transitionTime: 1000 // 轮播图过渡耗时 （单位毫秒）
 }
-export const CarouselBannerConfig = [
+export const CarouselBannerConfig: CarouselBannerConfigInter[] = [
     {
         id: "xPhone",
         content: [
@@ -16,8 +34,9 @@ export const CarouselBannerConfig = [
                 style: {
                     color: '#fff',
                     textAlign: 'center',
-                    fontSize: '90',
-                    fontWeight: 800,
+                    fontSize: '90px',
+                    fontWeight: 500,
+                    marginTop: "20vh"
                 }
             },
             {
@@ -25,7 +44,7 @@ export const CarouselBannerConfig = [
                 style: {
                     color: '#fff',
                     textAlign: 'center',
-                    fontSize: '50px',
+                    fontSize: '40px',
                     fontWeight: 500,
                 },
             },
@@ -34,52 +53,66 @@ export const CarouselBannerConfig = [
                 style: {
                     color: '#fff',
                     textAlign: 'center',
-                    fontSize: '50px',
+                    fontSize: '40px',
                     fontWeight: 500,
                 }
             }
         ],
         imageUrl: iphone,
         color: "#fff",
-        backgroundColor: ""
+        backgroundColor: "#111111"
     }, {
         id: "Tablet",
         content: [
             {
                 name: "Tablet",
                 style: {
-
+                    color: '#000',
+                    textAlign: 'center',
+                    fontSize: '40px',
+                    fontWeight: 500,
+                    marginTop: "20vh"
                 }
             },
             {
                 name: "Just the right amount of everything",
                 style: {
-
+                    color: '#000',
+                    textAlign: 'center',
+                    fontSize: '20px',
+                    fontWeight: 500,
                 }
             }
         ],
         imageUrl: airpods,
         color: "#fff",
-        backgroundColor: ""
+        backgroundColor: "#F1F1F3"
     }, {
         id: "Tablet or xPhone",
         content: [
             {
                 name: "Buy a Tablet or xPhone for college.",
                 style: {
-
+                    color: '#000',
+                    textAlign: 'center',
+                    fontSize: '40px',
+                    fontWeight: 800,
+                    marginTop: "20vh"
                 }
             },
             {
                 name: "Get arPods.",
                 style: {
-
+                    color: '#000',
+                    textAlign: 'center',
+                    fontSize: '40px',
+                    fontWeight: 800,
                 }
             }
         ],
         imageUrl: tablet,
         color: "#fff",
-        backgroundColor: ""
+        backgroundColor: "#FAFAFA"
     },
 ]
 
