73ab125b64584bc3092d65d2bba98b369c8c46bb
Nanoo <g@harogo.fun>
2022-04-01 19:57:50 +0800

    update: carousel data format

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,33 +5,44 @@ import Carousel from './components/carousel';
 import airpods from './assets/airpods.png';
 import iphone from './assets/iphone.png';
 import tablet from './assets/tablet.png';
+import classNames from 'classnames';
+
+const data = [
+  {
+    title: 'xPhone',
+    description: 'Lots to love. Less to spend. Starting at $399',
+    image: iphone,
+    className: 'xphone',
+  },
+  {
+    title: 'Tablet',
+    description: `Just the right amount of everything.`,
+    image: tablet,
+    className: 'tablet',
+  },
+  {
+    title: 'Buy a Tablet or xPhone for college',
+    description: 'Get arPods.',
+    image: airpods,
+    className: 'airpods',
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <Carousel duration={10000} autoplay>
-        <div className="wrapper xphone">
-          <div className="text">
-            <h2>xPhone</h2>
-            <p>Lots to love. Less to spend</p>
-            <p>Starting at $399</p>
-          </div>
-          <div className="pic" style={{ backgroundImage: `url(${iphone})` }}></div>
-        </div>
-        <div className="wrapper tablet">
-          <div className="text">
-            <h2>Tablet</h2>
-            <p>Just the right amount of everything.</p>
-          </div>
-          <div className="pic" style={{ backgroundImage: `url(${tablet})` }}></div>
-        </div>
-        <div className="wrapper airpods">
-          <div className="text">
-            <h2>Buy a Tablet or xPhone for college</h2>
-            <h2>Get arPods.</h2>
-          </div>
-          <div className="pic" style={{ backgroundImage: `url(${airpods})` }}></div>
-        </div>
+        {data.map((d, i) => {
+          return (
+            <div key={d.title + i} className={classNames('wrapper', d.className)}>
+              <div className="text">
+                <h2>{d.title}</h2>
+                <p>{d.description}</p>
+              </div>
+              <div className="pic" style={{ backgroundImage: `url(${d.image})` }}></div>
+            </div>
+          );
+        })}
       </Carousel>
     </div>
   );
