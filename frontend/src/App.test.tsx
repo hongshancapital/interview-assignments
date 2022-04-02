a683471cd31ad3b16611def3ec5e2943f6f0d812
surui1 <surui1@xiaomi.com>
2022-04-02 15:46:11 +0800

    commit carousel and unittest

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render,screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import App from './App';
 
-test('renders learn react link', () => {
+test('test text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const xPhone = getByText("xPhone");
+  expect(xPhone).toBeInTheDocument();
+  const tablet = getByText("Tablet");
+  expect(tablet).toBeInTheDocument();
+  const airPods = getByText("Buy a Tablet");
+  expect(airPods).toBeInTheDocument();
 });
+
+test('test click', () => {
+  let {getByTestId} = render(<App />);
+  let action = getByTestId('action-item-1')
+  userEvent.click(action)
+  expect(action).toHaveClass('carousel-action-item')
+})
