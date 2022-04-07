27a4a5e82aa22cd5d6aa3a4e6b3d7e078e2095a8
chm1n9 <522151157@qq.com>
2022-04-07 14:35:42 +0800

    feat: Carousel demo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,36 @@
 import React from "react";
 import "./App.css";
+import GoodsCard, { GoodsCardProps } from "./components/GoodsCard";
+import Carousel from './components/Carousel'
+import iphone from './assets/iphone.png'
+import tablet from './assets/tablet.png'
+import airpods from './assets/airpods.png'
+
+const goodsList: GoodsCardProps[] = [{
+  headline: 'xPhone',
+  subhead: 'Lots to love. Less to spend.\nStarting at $399',
+  image: iphone,
+  backgroundColor: "#111",
+  color: "white",
+}, {
+  headline: 'Tablet',
+  subhead: 'Just the right amount of everything.',
+  image: tablet,
+  backgroundColor: "#FAFAFA",
+  color: "black",
+},{
+  headline: 'Bug a Tablet or xPhone for college.\nGet arPods',
+  image: airpods,
+  backgroundColor: "#F1F1F3",
+  color: "black",
+}]
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return <div className="App">
+    <Carousel auotPlay>{
+      goodsList.map(item => <GoodsCard key={item.headline} {...item} />)
+    }</Carousel>
+  </div>
 }
 
 export default App;
