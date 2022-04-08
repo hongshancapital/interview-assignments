0c8a996819a299fec82dbca3af21711181b6eee4
hulk4fe <benye.deng@outlook.com>
2022-04-08 09:32:20 +0800

    feat: app update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import SwiperView from "./views/swiper";
+import { SwiperAdapter } from './adapter/swiper';
+import get from 'lodash/get';
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  const [payload, setPayload] = useState({});
+  useEffect(() => {
+    console.log('##app mounted--->')
+    setPayload(SwiperAdapter.getPayload());
+  }, [])
+  return <div className="App">
+    {/* write your component here */}
+    <SwiperView 
+      autoPlay={get(payload, 'autoPlay', true)}
+      sliders={get(payload, 'sliders', [])}
+    ></SwiperView>
+  </div>;
 }
 
 export default App;
