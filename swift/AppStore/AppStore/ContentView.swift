bd79c4a1daa5d5d9eeda3471c503534c287179ef
liuxing <liuxing@mucang.cn>
2022-04-11 15:47:17 +0800

    项目初始化

---
+++ swift/AppStore/AppStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppStore
+//
+//  Created by liuxing on 2022/4/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
