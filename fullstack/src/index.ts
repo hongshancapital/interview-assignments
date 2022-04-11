95476d1d853cd4e6e4f75a2dea31ee19f835c151
李泉水 <lqs4314@163.com>
2022-04-11 22:23:38 +0800

    fullstack

---
+++ fullstack/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import shortRouter from './routes/short';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function (req, res) {
+    res.render('demo.html');
+});
+
+app.use('/', shortRouter);
+
+app.listen('3000', () => {
+    console.log('server start on http://localhost:3000')
+})
---
+++ fullstack/src/routes/short.ts
@@ -0,0 +1,66 @@
+import express from 'express';
+import { createClient } from 'redis';
+
+const router = express.Router();
+const client = createClient({
+    url: 'redis://localhost:6379'
+});
+
+let isRedisConnected = false;
+
+client.on('error', () => {
+    isRedisConnected = false;
+    console.log('redis 连接错误');
+});
+
+client.on('reconnecting', () => {
+    console.log('redis 正在重连...')
+});
+
+client.on('connect', () => {
+    isRedisConnected = true;
+    console.log('redis 连接成功');
+});
+
+client.connect();
+
+router.post('/long2short', async function (req, res, next) {
+    const long = req.body?.long as string;
+    if (!long) {
+        res.send({ success: false, msg: '缺少参数long' })
+        return;
+    }
+    try {
+        new URL(long)
+    } catch(e) {
+        res.send({ success: false, msg: '参数long不是正确的url格式' });
+        return;
+    }
+    if (!isRedisConnected) {
+        res.send({ success: false, msg: '系统错误' })
+        return;
+    }
+
+    let short: string;
+    do {
+        short = Math.random().toString(36).substr(-8);
+    } while (!await client.setNX(short, long));
+    
+    res.send({ success: true, data: short });
+});
+
+router.get('/*', async function (req, res, next) {
+    const short = req.url.substr(1);
+    if (!isRedisConnected) {
+        res.send('系统错误');
+        return;
+    }
+    const long = await client.get(short)
+    if (long) {
+        res.redirect(long);
+    } else {
+        next();
+    }
+});
+
+export default router;
