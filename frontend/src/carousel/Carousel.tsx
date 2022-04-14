9d0e700026cd54767791e523c9ccd26075fbc73c
chenyuxuan <530853620@qq.com>
2022-04-15 00:53:22 +0800

    test done

--- frontend/src/carousel/Carousel.tsx
+++ frontend/src/carousel/Carousel.tsx
@@ -44,11 +44,11 @@ function Carousel(props: any) {
         if (!props.open) {return;}
         if (config.current === config.next) {//判断是等切页还是等展示，next用于控制切页
             timeout.current = window.setTimeout(() => {
-                setConfig(Object.assign({}, config, {next: (config.current + 1 === props.children.length?0:(config.current + 1))}));
+                setConfig(Object.assign({}, config, {next: (config.current + 1 === props.children?.length?0:(config.current + 1))}));
             }, config.showDuration * 1000);
         } else {
             timeout.current = window.setTimeout(() => {
-                setConfig(Object.assign({}, config, {current: (config.current + 1 === props.children.length?0:(config.current + 1))}));
+                setConfig(Object.assign({}, config, {current: (config.current + 1 === props.children?.length?0:(config.current + 1))}));
             }, config.switchDuration * 1000);
         }
     }, [config, props.open, props.children]);
@@ -58,13 +58,13 @@ function Carousel(props: any) {
         setConfig(Object.assign({}, config, {current: index, next: index}));
     };
 
-    const tickList = Array.from({length: props.children.length}, (v: any, index: number) => {//生成按钮
+    const tickList = Array.from({length: (props.children || []).length}, (v: any, index: number) => {//生成按钮
         return <Tick index={index} parent={config} click={() => tickClick(index)} key={index} open={props.open}/>;
     });
 
-    const pageList = props.children.map((v: any, index: number) => {//生成页队列
+    const pageList = props.children?.map((v: any, index: number) => {//生成页队列
         return <div className="page" key={index}>{v}</div>;
-    });
+    }) || [];
 
     return <div className="Carousel">
         <div className={'page-box ' + (props.open?'switching':'')} style={{transitionDuration: props.open?(config.switchDuration + 's'):'unset', left: config.next === 0?0:(config.next * -100 + '%')}}>{pageList}</div>
