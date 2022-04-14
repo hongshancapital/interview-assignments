e3e0f04ef9905a0a84ffd841106a8c0a7ea120a8
zhanglin <a1107108799@gmail.com>
2022-04-14 15:54:43 +0800

    my homework

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./App.css";
+import AutoCarouselPage from "./page/AutoCarouselPage";
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return <div className="App">
+    <AutoCarouselPage />
+  </div>;
 }
 
 export default App;
