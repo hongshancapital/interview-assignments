127ee4e70236096dd4b1b968445cb2b80615a814
yangwanhong <yangwanhong@linklogis.com>
2022-04-15 15:17:09 +0800

    commit

---
+++ yangwanhong-exam/src/main/java/com/example/demo/YangwanhongEexamApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangwanhongEexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YangwanhongEexamApplication.class, args);
+	}
+
+}
---
+++ yangwanhong-exam/src/test/java/com/example/demo/YangwanhongEexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YangwanhongEexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
