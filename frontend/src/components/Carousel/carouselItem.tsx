71fe6f0409415c1824ed41636a19f63c12f03e76
yaowenjun <yaowenjun@baijiayun.com>
2022-04-23 13:07:29 +0800

    fix(frontend): 添加key

--- frontend/src/components/Carousel/carouselItem.tsx
+++ frontend/src/components/Carousel/carouselItem.tsx
@@ -9,8 +9,8 @@ export default function CarouselItem(props: ICarouselItem) {
     <div className='carousel-item'>
       <img className='carousel-item-image' src={src} alt={alt ?? ''} />
       <div className='carousel-item-wrapper' style={{ color }}>
-        {title.map(t => <p key={t} className='carousel-item-title'>{t}</p>)}
-        {desc?.map(d => <p key={d} className='carousel-item-desc'>{d}</p>)}
+        {title.map((t, index) => <p key={`${t}_${index}`} className='carousel-item-title'>{t}</p>)}
+        {desc?.map((d, index) => <p key={`${d}_${index}`} className='carousel-item-desc'>{d}</p>)}
       </div>
     </div>
   )
