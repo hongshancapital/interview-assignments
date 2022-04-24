8a4df0715815c485f558637391bc392d167e8b1f
xuzhao <twtyjvkg@foxmail.com>
2022-04-24 14:40:34 +0800

    轮播图

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,67 @@
 import React from "react";
 import "./App.css";
+import Carousel from './Carousel';
+import CarouselItem from './CarouselItem';
+
+const contentStyle: any = {
+    height: '100vh',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+};
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return (
+      <div className="App">
+        <Carousel autoplay>
+            <div>
+                <CarouselItem
+                    style={{ background: '#000', height: '100vh' }}
+                    title={(
+                        <div style={{ color: '#fff' }} >XPhone</div>
+                    )}
+                    description={(
+                        <div style={{ color: '#fff' }}>
+                            <div>Lots to love, less to spend.</div>
+                            <div>Starting at $399.</div>
+                        </div>
+                    )}
+                />
+            </div>
+            <div>
+                <CarouselItem
+                    style={{ background: '#fff', height: '100vh' }}
+                    title={(
+                        <div style={{ color: '#000' }} >
+                            Tablet
+                        </div>
+                    )}
+                    description={(
+                        <div style={{ color: '#000' }}>
+                            <div>Just the right amount of everything.</div>
+                        </div>
+                    )}
+                />
+            </div>
+            <div>
+                <CarouselItem
+                    style={{ background: '#fff', height: '100vh' }}
+                    title={(
+                        <div style={{ color: '#000' }} >
+                            Buy a Tablet or xPhone for college.
+                        </div>
+                    )}
+                    description={(
+                        <div style={{ color: '#000' }}>
+                            <div>Get arPods</div>
+                        </div>
+                    )}
+                />
+            </div>
+        </Carousel>
+      </div>
+  );
 }
 
 export default App;
