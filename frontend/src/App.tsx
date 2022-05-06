625fd943359b94c2c85bac88d079a1618ce05adc
fefuns <1321120469@qq.com>
2022-05-06 10:56:17 +0800

    feat: children改成数据化; 可否改用 className控制轮播

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,25 @@ import React from "react";
 import Carousel from 'components/Carousel';
 import "./App.scss";
 
+const items = [
+  {
+    title: 'xPhone',
+    subTitle: 'Lots to love. Less to spend. \n Starting at $399.',
+    bgImg: 'iphone'
+  },
+  {
+    title: 'Tablet',
+    subTitle: 'Just the right amount of everything',
+    bgImg: 'tablet'
+  },
+  {
+    title: 'Buy a tablet or xPhone for college. \n Get airPods',
+    bgImg: 'airpods'
+  }
+]
 function App() {
   return <div className="App">
-    <Carousel className='carousel-banner'>
-      <div className="carousel-item">
-        <h2 className="title">xPhone</h2>
-        <p className="sub-title">Lots to love. Less to spend <br />Starting at $399</p>
-      </div>
-      <div className="carousel-item">
-        <h2 className="title">Tablet</h2>
-        <p className="sub-title">Just the right amount of everything</p>
-      </div>
-      <div className="carousel-item">
-        <h2 className="title">Buy a Tablet or xPhone for college.<br />Get arPods</h2>
-      </div>
-    </Carousel>
+    <Carousel className='carousel-banner' items={items} />
   </div>;
 }
 
--- frontend/src/components/Carousel/index.tsx
+++ frontend/src/components/Carousel/index.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './style.scss';
-
+interface IItem {
+  title: string;
+  subTitle?: string;
+  bgImg: string;
+}
 interface Iprops {
-  // 切换的子元素 至少大于一项。考虑用这种形式是因为担心数据样式过多，例如可能第一屏中金额需要红色突出？并且像背景图这种样式不应该在组件中。组件重在功能最基础的样式，内容高度定义化交给使用者
-  children: JSX.Element[],
+  // 滑动数据
+  items: IItem[],
   // 样式名 多处使用时，可分别设置最外层盒子样式避免样式污染。可传多个并用空格分开
   className?: string,
   // 是否自动切换 默认值：true
@@ -13,17 +17,17 @@ interface Iprops {
   // 切换速度，即 slider自动滑动开始到结束的时间 单位: 毫秒，默认值: 300
   speed?: number;
 }
-function Carousel({children, className = '', autoPlay = true, delay = 3000, speed = 300 }: Iprops) {
+function Carousel({ items = [], className = '', autoPlay = true, delay = 3000, speed = 300 }: Iprops) {
   const [activeIndex, setActiveIndex] = useState(0);
   // 自动运行
   useEffect(() => {
     if (autoPlay) {
       let timer = setInterval(() => {
-        setActiveIndex(activeIndex < children.length -1  ? activeIndex + 1 : 0);
+        setActiveIndex(activeIndex < items.length - 1 ? activeIndex + 1 : 0);
       }, delay)
       return () => clearInterval(timer);
     }
-  }, [activeIndex, autoPlay, delay, children.length])
+  }, [activeIndex, autoPlay, delay, items.length])
 
   // 点击切换
   function changeActiveIndex(i: number) {
@@ -31,16 +35,24 @@ function Carousel({children, className = '', autoPlay = true, delay = 3000, spee
   }
 
   return <div className={`carousel-wrap ${className}`}>
-    <div className="carousel-list" style={{
-      width: `${children.length * 100}vw`,
+    {/*  style={{
+      width: `${items.length * 100}vw`,
       transitionDuration: `${speed}ms`,
       transform: `translate3d(-${activeIndex / 3 * 100}%, 0, 0)`
-    }}>
-      {children}
+    }} */}
+    <div className="carousel-list">
+      {items.map((item: IItem, index: number) => (
+        <div className={['carousel-item', index === activeIndex ? 'active' : '', index === activeIndex - 1 || index === activeIndex + items.length - 1 ? 'prev' : ''].join(' ')} key={index} style={{ transitionDuration: `${speed}ms` }}>
+          <div className="carousel-item-inner">
+            <h2 className="title">{item.title}</h2>
+            {item.subTitle && <p className="sub-title">{item.subTitle}</p>}
+          </div>
+        </div>
+      ))}
     </div>
     <div className="carousel-dots">
       <ul className="dot-list">
-        {children.map((item, index) => (
+        {items.map((item, index) => (
           <li key={index} className={activeIndex === index ? 'dot-item active' : 'dot-item'} onClick={() => changeActiveIndex(index)}>
             <i className="dot-progress" style={activeIndex === index && autoPlay ? { transitionDuration: `${delay}ms` } : {}}></i>
           </li>
