b70494e2606b1f2f1d3d75c374bf555d1593fa3b
yezijian <yezijian_721@aliyun.com>
2022-05-07 15:14:14 +0800

    shike-pr

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Carousel from "./pages/carouselIndex";
+
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return  <div className="App">
+          {/* write your component here */}
+            <Carousel/>
+          </div>;
 }
 
 export default App;
