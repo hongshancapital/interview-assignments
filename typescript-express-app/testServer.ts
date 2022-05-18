74b93ea184a9e26c83121432bd8809ccb23dadd7
FredII <FredII@users.noreply.github.com>
2022-05-18 23:48:05 +0800

    Delete typescript-express-app directory

--- typescript-express-app/testServer.ts
+++
@@ -1,6 +0,0 @@
-// 对api接口测试需要使用supertest，使用expert断言，supertest是一个非常好的适用于node的模拟http请求的库
-import request from 'supertest'
-// 引入产生请求的程序入口app.js
-import server from './app'
-
-export default request(server)
