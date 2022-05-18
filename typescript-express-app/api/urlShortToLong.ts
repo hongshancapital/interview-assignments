381f97d78745bb0feaf6f7d960705f91716aebdd
FredII <FredII@users.noreply.github.com>
2022-05-18 22:33:39 +0800

    Typescript 实现短域名服务 - lizilong
    完整提交

---
+++ typescript-express-app/api/urlShortToLong.ts
@@ -0,0 +1,28 @@
+import epxress from 'express'
+const router = epxress.Router()
+import Url from '../models/url'
+
+router.get('/:code', async (req, res, next) => {
+	try {
+		const urlCode = req.params.code
+		// 判断短域名在MongoDB数据库中是否存在
+		const url = await Url.findOne({ urlCode })
+		if (url) {
+			// 库中存在则取出后直接重定向至原链接
+			res.redirect(url.longUrl)
+		} else {
+			// 库中不存在则给出不存在提示
+			res.status(404).json({
+				status: '20001',
+				msg: 'No url found',
+			})
+		}
+	} catch (error) {
+		res.status(500).json({
+			status: '20002',
+			msg: 'Server error',
+		})
+	}
+})
+
+export default router
