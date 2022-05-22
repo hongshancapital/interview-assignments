4b388ac92ab542e72e1c3d5ccb4a1ce77c34f8a2
Jeffy <wonderjeffy@gmail.com>
2022-05-22 22:25:38 +0800

    mod: initial project

---
+++ swift/InterviewPreWork/InterviewPreWork/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InterviewPreWork
+//
+//  Created by jeffy on 2022/5/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift/InterviewPreWork/InterviewPreWork/InterviewPreWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewPreWorkApp.swift
+//  InterviewPreWork
+//
+//  Created by jeffy on 2022/5/12.
+//
+
+import SwiftUI
+
+@main
+struct InterviewPreWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
