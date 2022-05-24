7b39272ff7d8794269c545741bbbf3e9e035e919
liuzhe <snewartt@163.com>
2022-05-24 14:42:32 +0800

    add carousel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Carousel from './components/Carousel'
+import airpods from './assets/airpods.png';
+import iphone from './assets/iphone.png';
+import tablet from './assets/tablet.png';
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return <div className="App">
+    <Carousel list={[airpods, iphone, tablet]} />
+    {/* write your component here */}
+  </div>;
 }
 
 export default App;
