10b6db85c18869c2ca142318ec7a63f1e64cefc8
gricode <gricode@126.com>
2022-05-26 09:46:53 +0800

    init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return <div className="App">123{/* write your component here */}</div>;
 }
 
 export default App;
