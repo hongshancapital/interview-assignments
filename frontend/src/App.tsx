eacb9a638f1e3a64d1bb0e94be445e95efdf898b
qiubiao <qiubiao@dengdengfu.com>
2022-05-31 00:03:40 +0800

    feat: 数据化展示内容

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,24 +2,33 @@ import React from 'react';
 import Carousel from './components/carousel';
 
 import './App.css';
-import iphone from './assets/iphone.png';
-import tablet from './assets/tablet.png';
-import airpods from './assets/airpods.png';
 
 function App() {
+  const data = [
+    {
+      className: 'iphone',
+      title: 'xPhone',
+      text: <>Lots to love. less to spend.<br/>Starting at $399.</>
+    },
+    {
+      className: 'tablet',
+      title: 'Tablet',
+      text: 'Just the right amount of everything.'
+    },
+    {
+      className: 'airpods',
+      title: <>Buy a Tablet or xPhone for college.<br/>Get arPods.</>
+    }
+  ];
+
   return <div className="App">
     <Carousel>
-      <div className="iphone" style={{backgroundImage: `url(${iphone})`}}>
-        <div className="title">xPhone</div>
-        <div className="text">Lots to love. less to spend.<br/>Starting at $399.</div>
-      </div>
-      <div className="tablet" style={{backgroundImage: `url(${tablet})`}}>
-        <div className="title">Tablet</div>
-        <div className="text">Just the right amount of everything.</div>
-      </div>
-      <div className="airpods" style={{backgroundImage: `url(${airpods})`}}>
-        <div className="title">Buy a Tablet or xPhone for college.<br/>Get arPods.</div>
-      </div>
+      {
+        data.map((item, index) => <div key={index} className={item.className}>
+          <div className="title">{item.title}</div>
+          { item.text && <div className="text">{item.text}</div> }
+        </div>)
+      }
     </Carousel>
   </div>;
 }
