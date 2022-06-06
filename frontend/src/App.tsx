72a3998eb2a719789672ff76396cb20d937e515c
konrumi <konrumi@gmail.com>
2022-06-06 16:37:15 +0800

    :sparkles: Carousel 主功能实现
    + Carousel 功能及样式

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,43 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
 
-function App() {
-  return <div className="App">{/* write your component here */}</div>;
+import Carousel from './components/Carousel'
+
+import './App.css'
+
+function App () {
+  return <div className="App" style={{
+    width: '100vw',
+    height: '100vh',
+    margin: 'auto',
+    backgroundColor: '#333'
+  }}>
+    <Carousel
+      autoplayTime={2000}
+    >
+      <div className="item" style={{ backgroundColor: '#111', color: '#fff'}}>
+        <img className="item-img" src="assets/iphone.png" alt="img"/>
+        <div className="item-desc">
+          <h2 className="item-title">xPhone</h2>
+          <p>Lots to love. Less to spend.</p>
+          <p>Starting at $399</p>
+        </div>
+      </div>
+      <div className="item" style={{ backgroundColor: '#fafafa', color: '#000'}}>
+        <img className="item-img" src="assets/tablet.png" alt="img"/>
+        <div className="item-desc">
+          <h2 className="item-title">Tablet</h2>
+          <p>Just the right amount of everything.</p>
+        </div>
+      </div>
+      <div className="item" style={{ backgroundColor: '#f1f1f3', color: '#000'}}>
+        <img className="item-img" src="assets/airpods.png" alt="img"/>
+        <div className="item-desc">
+          <h2 className="item-title">Buy a Tablet or xPhone for college.</h2>
+          <h2 className="item-title">Get airPods.</h2>
+        </div>
+      </div>
+    </Carousel>
+  </div>
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Carousel.tsx
@@ -0,0 +1,156 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+import './Carousel.css'
+
+interface CarouselProps {
+  children?: React.ReactChild[],
+  autoplayTime?: number,
+  transitionTime?: number,
+  infinity?: boolean,
+  showArrows?: boolean,
+  showStatus?: boolean,
+  onClickItem?: (index: number) => void,
+  onClickThumb?: (index: number) => void,
+  onChange?: (index: number) => void,
+  defaultIndex?: number,
+}
+
+const noop: () => void = () => {}
+
+function Carousel ({
+  children,
+
+  autoplayTime = 2000,
+  transitionTime = 500,
+
+  showArrows = true,
+  showStatus = true,
+  infinity = true,
+
+  onChange = noop,
+
+  defaultIndex = 0
+}: CarouselProps) {
+  const [targetOffset, setTargetOffset] = useState(0)
+
+  const itemAmount: number = children?.length || 0
+
+  // refs
+  let containerRef = useRef<HTMLDivElement>(null)
+
+  let currentIndex = useRef<number>(defaultIndex)
+  let autoplayTimer = useRef<NodeJS.Timer>()
+
+  let itemList: Array<any> = []
+  let barList: Array<any> = []
+
+
+  /**
+   * Get carousel container current width
+   */
+  const getContainerWidth: () => number = function (): number {
+    return containerRef.current?.offsetWidth || 0
+  }
+
+  /**
+   * Swtich to target item, pass the item index
+   * @param targetIndex index of the target item
+   */
+  const switchTo: (targetIndex: number) => void = function (targetIndex: number): void {
+    if (infinity) {
+      if (targetIndex >= 0) {
+        targetIndex = targetIndex % itemAmount
+      } else {
+        targetIndex = itemAmount + (targetIndex % itemAmount)
+      }
+    }
+
+    currentIndex.current = targetIndex
+    setTargetOffset(currentIndex.current * getContainerWidth())
+
+    onChange(currentIndex.current)
+  }
+
+  /**
+   * Swtich to next item
+   */
+  const switchNext: () => void = function (): void {
+    let targetIndex: number = currentIndex.current + 1
+    if (infinity || targetIndex < itemAmount) {
+      switchTo(targetIndex)
+    }
+  }
+
+  /**
+   * Swtich to previous item
+   */
+  const switchPrev: () => void = function (): void {
+    let targetIndex: number = currentIndex.current - 1
+    if (infinity || targetIndex >= 0) {
+      switchTo(targetIndex)
+    }
+  }
+
+  useEffect(() => {
+    // reset time for manual switch condition
+    if (autoplayTimer.current) {
+      clearInterval(autoplayTimer.current)
+    }
+
+    // set autoplay timer
+    if (autoplayTime > 0) {
+      autoplayTimer.current = setInterval(() => {
+        switchNext()
+      }, autoplayTime + transitionTime)
+    }
+
+    return () => {
+      clearInterval(autoplayTimer.current)
+    }
+  })
+
+  children?.forEach((child, index) => {
+    // generate carousel items list
+    itemList.push(
+      <li className="carousel-item" key={index}>
+        {child}
+      </li>
+    )
+
+    // generate status bar list
+    barList.push(
+      index === currentIndex.current
+        ? <span className="carousel-status-bar carousel-current-bar" key={index}><i style={{ transition: `width ${autoplayTime}ms linear` }}></i>-</span>
+        : <span className="carousel-status-bar" key={index} onClick={() => {switchTo(index)}}><i></i>-</span>
+    )
+  })
+
+  return (
+    <div className="carousel-container" ref={containerRef}>
+      <div className="carousel-itemContainer">
+        <ol className="carousel-itemList" style={{ transform: `translateX(-${targetOffset}px)`, transition: `transform ${transitionTime}ms` }}>
+          {itemList}
+        </ol>
+      </div>
+
+      {
+        showArrows ? (
+          <div className="carousel-arrowContainer">
+            <div className="carousel-arrow carousel-arrow-prev" onClick={switchPrev}><i>&lt;</i></div>
+            <div className="carousel-arrow carousel-arrow-next" onClick={switchNext}><i>&gt;</i></div>
+          </div>
+        ) : null
+      }
+
+      {
+        showStatus ? (
+          <div className="carousel-statusContainer">
+            {barList}
+          </div>
+        ) : null
+      }
+    </div>
+  )
+}
+
+export default Carousel
