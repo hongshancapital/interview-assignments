8aac80859413f7c738b46c384513d05fb7cef675
zzz <neozl24@hotmail.com>
2022-06-09 03:41:03 +0800

    test: add some tests for App

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
 
-test('renders App Component', () => {
+test('App Component renders in document', () => {
   const { container } = render(<App />)
   expect(container).toBeInTheDocument()
-});
+})
+
+test('App Component renders expected presentation pages', () => {
+  const { getByText } = render(<App />)
+  expect(getByText('xPhone')).toBeInTheDocument()
+  expect(getByText('Lots to love. Less to spend.')).toBeVisible()
+
+  expect(getByText('Tablet')).toBeInTheDocument()
+  expect(getByText('Get arPods.')).toBeInTheDocument()
+})
