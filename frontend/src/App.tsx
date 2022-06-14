851996af97146045ee87cd3ebb568ca2f3a660f6
sky <1837809996@qq.com>
2022-06-14 17:37:09 +0800

    feat: add Carousel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Carousel } from "./components/Carousel";
+import iphoneImg from "./assets/iphone.png";
+import tabletImg from "./assets/tablet.png";
+import airpodsImg from "./assets/airpods.png";
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return (
+    <div className="App">
+      <Carousel className="carousel" autoplay>
+        <img key="iphoneImg" className="img" src={iphoneImg}></img>
+        <img key="tabletImg" className="img" src={tabletImg}></img>
+        <img key="airpodsImg" className="img" src={airpodsImg}></img>
+      </Carousel>
+    </div>
+  );
 }
 
 export default App;
