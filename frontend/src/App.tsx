9317709ec6ccb4aa29a3bb2a60fd6d4bdee739d8
hz9527 <981379316@qq.com>
2022-06-18 14:42:53 +0800

    feat(carousel): :lipstick: update style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,9 @@ const CarouselConfig = [
     subTitle: ['Lots to love. Less to spend.', 'Starting ar $399.'],
     alt: 'iPhone',
     img: IPhone,
-    color: 'white'
+    color: 'white',
+    backgroundColor: '#101010',
+    scale: 0.5
     // link
   },
   {
@@ -19,31 +21,40 @@ const CarouselConfig = [
     subTitle: ['Just the right amount of everything.'],
     alt: 'tablet',
     img: Tablet,
-    color: 'black'
+    backgroundColor: '#fafafa',
   },
   {
     title: ['Buy a Tablet or xPhone for college.', 'Get arPods.'],
-    subTitle: [],
     alt: 'airpods',
     img: Airpods,
-    color: 'black'
+    backgroundColor: '#f2f2f3',
+    scale: 1.3
   }
 ]
 
-const PR = 1 / window.devicePixelRatio
+function getScaleStyle(scale: number): React.CSSProperties {
+  return {
+    width: `${scale * 100}%`,
+    margin: `0 ${(1 - scale) / 2 * 100}%`
+  }
+}
 
 function App () {
   return (<div className="App">
     <Carousel>
       {
-        CarouselConfig.map(({ title, subTitle, alt, img, color }) => (
+        CarouselConfig.map(({ title = [], subTitle = [], alt, img, color, backgroundColor, scale }) => (
+          // 由于使用了背景色，这里没必要对图片做懒加载
           <CarouselItem key={alt}>
-            <div className="banner-container">
+            <div
+              className="banner-container"
+              style={{backgroundColor}}
+            >
               <div className="banner-text-con" style={{ color }}>
                 {title.map((str, i) => <div className="title" key={i}>{str}</div>)}
                 {subTitle.map((str, i) => <div className="text" key={i}>{str}</div>)}
               </div>
-              <img alt={alt} src={img} data-pr={PR}/>
+              <img alt={alt} style={scale ? getScaleStyle(scale) : {}} src={img} />
             </div>
           </CarouselItem>
         ))
