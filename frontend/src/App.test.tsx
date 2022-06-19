e3ace679de9c50f25eec4b55747dd42906f44906
Nice <naserdin@protonmail.com>
2022-06-19 13:56:39 +0800

    feat: app test added

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App, { slides } from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("app slide rendered", () => {
+  const { getByTestId } = render(<App />);
+
+  slides.forEach((slide) => {
+    const slideElm = getByTestId(slide.testId);
+    expect(slideElm).toBeVisible();
+    expect(slideElm.style.backgroundColor).toBe(slide.backgroundColor);
+    expect(slideElm.style.color).toBe(slide.color);
+
+    expect(slideElm.textContent).toContain(slide.title);
+    expect(slideElm.textContent).toContain(slide.text);
+  });
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,42 @@ import iphone from "./assets/iphone.png";
 import tablet from "./assets/tablet.png";
 import airpods from "./assets/airpods.png";
 
+type AppSlide = Array<{
+  imageSrc: string;
+  backgroundColor: string;
+  title: string;
+  text: string;
+  color: string;
+  testId: string;
+}>;
+
+export const slides: AppSlide = [
+  {
+    imageSrc: iphone,
+    backgroundColor: "rgb(17, 17, 17)",
+    title: "xPhone",
+    text: "Lots to love.Less to spend.\nStarting at $399",
+    color: "rgb(250, 250, 250)",
+    testId: "xPhone",
+  },
+  {
+    imageSrc: tablet,
+    backgroundColor: "rgb(250, 250, 250)",
+    title: "Tablet",
+    text: "Just the right amount of everything.",
+    color: "black",
+    testId: "Tablet",
+  },
+  {
+    imageSrc: airpods,
+    backgroundColor: "rgb(250, 250, 250)",
+    title: "Buy a Tablet or xPhone for college.\nGet arPods.",
+    text: "",
+    color: "black",
+    testId: "arPods",
+  },
+];
+
 function App() {
   const slideStyle = {
     width: "100%",
@@ -15,36 +51,14 @@ function App() {
     backgroundRepeat: "no-repeat",
   };
 
-  const slides = [
-    {
-      imageSrc: iphone,
-      backgroundColor: "#111111",
-      title: "xPhone",
-      text: "Lots to love.Less to spend.\nStarting at $399",
-      color: "#fafafa",
-    },
-    {
-      imageSrc: tablet,
-      backgroundColor: "#fafafa",
-      title: "Tablet",
-      text: "Just the right amount of everything.",
-      color: "black",
-    },
-    {
-      imageSrc: airpods,
-      backgroundColor: "#fafafa",
-      title: "Buy a Tablet or xPhone for college.\nGet arPods.",
-      color: "black",
-    },
-  ];
-
   return (
     <div className="App">
       <Carousel duration={3}>
         {slides.map((s) => {
           return (
-            <Slide>
+            <Slide key={s.title}>
               <div
+                data-testid={s.testId}
                 style={{
                   background: `no-repeat url(${s.imageSrc})`,
                   backgroundColor: s.backgroundColor,
