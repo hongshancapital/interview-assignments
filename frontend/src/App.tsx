95192c27eb7b322ba9f06bb99dc6ec6c9b7b7927
hehao <780152324@qq.com>
2022-06-20 19:04:59 +0800

    feat: Carousel组件-何豪

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "./App.css";
 
+import Carousel from "./components/carousel";
+
+import iphone from './assets/iphone.png';
+import tablet from './assets/tablet.png';
+import airpods from './assets/airpods.png';
+
+const content = [
+  {
+    title: 'xPhone',
+    text: 'Lots to love. Less to spend.\nStarting at $399.',
+    fontColor: 'white',
+    imgUrl: iphone,
+  },
+  {
+    title: 'Tablet',
+    text: 'Just the right amount of everything.',
+    fontColor: 'black',
+    imgUrl: tablet,
+  },
+  {
+    title: 'Buy a Tablet or xPhone for college.\nGet arPods.',
+    text: '',
+    fontColor: 'black',
+    imgUrl: airpods,
+  },
+]
+
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return <div className="App">
+    <Carousel content={content} switchTime={3000}></Carousel>
+  </div>;
 }
 
 export default App;
---
+++ frontend/src/components/carousel/index.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import './index.scss';
+
+interface CarouselProps {
+  content: Item[];
+  switchTime: number;
+}
+
+interface Item {
+  title: string;
+  text: string;
+  fontColor: string;
+  imgUrl: string;
+}
+
+function Carousel(props: CarouselProps) {
+  const { content, switchTime } = props
+  const [current, setCurrent] = useState(0)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if(current >= content.length - 1) {
+        setCurrent(0)
+      } else {
+        setCurrent(current + 1)
+      }
+    }, switchTime)
+    return () => clearInterval(timer)
+  }, [current, switchTime, content.length])
+
+  return (
+    <div className="carousel-container">
+      <div className="slides" style={{left: `${-1 * current * 100}%`}}>
+        {
+          content && content.map((item: Item, index: number) => (
+            <div className="slide-item" key={index}>
+              <p className="title" style={{color: `${item.fontColor}`}}>{item.title}</p>
+              <p className="text" style={{color: `${item.fontColor}`}}>{item.text}</p>
+              <img src={item.imgUrl} alt=""/>
+            </div>
+          ))
+        }
+      </div>
+      <div className="progress">
+        {
+          content && content.map((_, index: number) => (
+            <span className="outer" key={index}>
+              <span className="inner" style={index === current ? {width: '100%', transition: `width ${switchTime/1000}s linear`} : {}}></span>
+            </span>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Carousel;
