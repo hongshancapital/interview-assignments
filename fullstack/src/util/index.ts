282802567add795db747a45ecd954d6ae52f97a1
yaozeyuan001 <yaozeyuan001@ke.com>
2022-06-21 18:00:48 +0800

    fix: 补充默认返回msg信息

--- fullstack/src/util/index.ts
+++ fullstack/src/util/index.ts
@@ -2,14 +2,14 @@
  * 用类名区分工具库, 便于未来拆分
  */
 export class ResUtil {
-  static showError({ code = 10000, msg = "", data = {} }) {
+  static showError({ code = 10000, msg = "failed", data = {} }) {
     return ResUtil.showResult({
       code,
       msg,
       data,
     });
   }
-  static showResult({ code = 0, msg = "", data = {} }) {
+  static showResult({ code = 0, msg = "success", data = {} }) {
     return {
       code,
       msg,
