1497bcd6d9cf321eb5fa687dbfc6a1027602adc6
yaozeyuan001 <yaozeyuan001@ke.com>
2022-06-21 21:26:00 +0800

    feat: 合并到一个测试集中

--- fullstack/src/model/__tests__/index.ts
+++ fullstack/src/model/__tests__/index.ts
@@ -1,18 +1,20 @@
 import { Storage } from "../index";
 
-test("direct get url expect undefined", () => {
-  const randomUrl = `${Math.random() * 10000}`;
-  expect(Storage.get(randomUrl)).toBe(undefined);
-});
+describe("test Storage class", () => {
+  test("direct get url expect undefined", () => {
+    const randomUrl = `${Math.random() * 10000}`;
+    expect(Storage.get(randomUrl)).toBe(undefined);
+  });
 
-test("save same url expect same key", () => {
-  const randomUrl = `${Math.random() * 10000}`;
-  let key = Storage.add(randomUrl);
-  expect(Storage.add(randomUrl)).toBe(key);
-});
+  test("save same url expect same key", () => {
+    const randomUrl = `${Math.random() * 10000}`;
+    let key = Storage.add(randomUrl);
+    expect(Storage.add(randomUrl)).toBe(key);
+  });
 
-test("get url by key will success", () => {
-  const randomUrl = `${Math.random() * 10000}`;
-  let key = Storage.add(randomUrl);
-  expect(Storage.get(key)).toBe(randomUrl);
+  test("get url by key will success", () => {
+    const randomUrl = `${Math.random() * 10000}`;
+    let key = Storage.add(randomUrl);
+    expect(Storage.get(key)).toBe(randomUrl);
+  });
 });
