201fd5ab1e4ce237ee5ae3beee540782335e80ff
丁涛 <54212465+simon10481@users.noreply.github.com>
2022-06-21 11:29:48 +0800

    dingtao - lunbo

---
+++ frontend/src/CarouselSource.tsx
@@ -0,0 +1,32 @@
+import iphone from './assets/iphone.png'
+import tablet from './assets/tablet.png'
+import airpods from './assets/airpods.png'
+
+
+
+const carouselSource = [
+    {
+        title: ['xPhone'],
+        desc: ["Lots to love. Less to spend.", "Starting at $399"],
+        img: iphone,
+        backgroundColor: '#111111',
+        color: 'white',
+        id: 1,
+    }, {
+        title: ["Tablet"],
+        desc: ["Just the right amout of everything."],
+        img: tablet,
+        backgroundColor: '#fafafa',
+        color: 'black',
+        id: 2
+    },{
+        title: ['Buy a Tablet or xPhone for college.', 'Get airPods.'],
+        desc: [],
+        img: airpods,
+        backgroundColor: '#f1f1f1',
+        color: 'black',
+        id: 3
+    }
+]
+
+export default carouselSource
