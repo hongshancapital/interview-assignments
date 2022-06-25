251434e6f7b22c41c9faf5beb53d1dd1ac48b70b
lijie <lijie2@whrhkj.com>
2022-06-25 09:41:02 +0800

    优化

---
+++ swift/优化版本:新增图片缓存:状态管理/ListDemo/ListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListDemoApp.swift
+//  ListDemo
+//
+//  Created by renhe on 2022/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift/优化版本:新增图片缓存:状态管理/ListDemo/ViewModel/ContentViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  ContentViewModel.swift
+//  ListDemo
+//
+//  Created by renhe on 2022/6/24.
+//
+
+import Foundation
+
+class ContentViewModel : ObservableObject{
+    @Published var appInfos = [InfoModel]()
+    @Published var isLoading = false
+    @Published var hasMore = true
+    @Published var error: LoadError?
+    private var manager = ConfigManager.init()
+    
+    init()  {
+        Task{
+            await refresh()
+        }
+    }
+    @MainActor
+    func refresh() async{
+        isLoading = true
+        switch await manager.refresh() {
+        case .success((let result, let hasMore)):
+            self.appInfos = result
+            self.hasMore = hasMore
+        case .failure(let loadError):
+            self.error = loadError as? LoadError
+        }
+        isLoading = false
+
+    }
+    @MainActor
+    func loadMore() async{
+        guard !isLoading && error == nil && hasMore else {
+            return
+        }
+        isLoading = true
+        switch await manager.loadMore() {
+        case .success(let fetchResult):
+            appInfos += fetchResult.result
+            hasMore = fetchResult.hasMore
+        case .failure(let error):
+            self.error = error as? LoadError
+        }
+        isLoading = false
+
+    }
+    
+    func updateLikeByTrackId(byTrackId id: String){
+       var likeIds = UserDefaults.standard.stringArray(forKey: Config.keys) ?? []
+       if let index = likeIds.firstIndex(of: id) {
+           likeIds.remove(at: index)
+       }else{
+           likeIds.append(id)
+       }
+       UserDefaults.standard.set(likeIds, forKey: Config.keys)
+   }
+}
