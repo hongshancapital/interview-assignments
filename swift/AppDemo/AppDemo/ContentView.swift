c4f64ad0bd70ce6d6a006cabd18984421c89d520
魏人杰 <jeffreyweiok@163.com>
2022-06-27 16:02:18 +0800

    项目初始化

---
+++ swift/AppDemo/AppDemo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AppDemo
+//
+//  Created by Jeffrey Wei on 2022/6/27.
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift/AppDemo/AppDemoUITests/AppDemoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  AppDemoUITestsLaunchTests.swift
+//  AppDemoUITests
+//
+//  Created by Jeffrey Wei on 2022/6/27.
+//
+//
+
+import XCTest
+
+class AppDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
