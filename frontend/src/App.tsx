8dbafa69a4cb6ae1ef0cabf4663362466130f52b
yanyu <532942147@qq.com>
2022-07-07 22:18:51 +0800

    feat: 实现Carousel组件

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Carousel from './components/Carousel';
+import iphone from './assets/iphone.png';
+import tablet from './assets/tablet.png';
+import airpods from './assets/airpods.png';
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return (
+    <div className="App">
+      <Carousel>
+        <div className='page' style={{ backgroundImage: `url(${iphone})` }}>iPhone</div>
+        <div className='page' style={{ backgroundImage: `url(${tablet})` }}>tablet</div>
+        <div className='page' style={{ backgroundImage: `url(${airpods})` }}>airpods</div>
+      </Carousel>
+    </div>
+  );
 }
 
 export default App;
