5e9e868be9b679e42735e74cec547ded4c9ef9e2
arvin <arvinrenamed@gmail.com>
2022-07-15 16:35:23 +0800

    feat: 实现轮播效果

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,41 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+
+import { Carousel } from './component/Carousel/Carousel';
+import { IitemProps } from './component/Carousel/Item';
+const bannerListData: IitemProps[] = [
+  {
+    id: 0,
+    title: 'xPhone',
+    desc: 'Lots to love. Less to spend.\nStarting at $399.',
+    img: require('./assets/iphone.png'),
+    style: { color: 'white' },
+  },
+  {
+    id: 1,
+    title: 'Tablet',
+    desc: 'Just the right amount of everything.',
+    img: require('./assets/tablet.png'),
+  },
+  {
+    id: 2,
+    title: 'Buy a tablet or xPhone for college.\nGet airpods.',
+    img: require('./assets/airpods.png'),
+  },
+];
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return (
+    <div className="App">
+      <Carousel
+        delaySeconds={3}
+        itemList={bannerListData}
+        onChange={(index: number) => {
+          console.log(index);
+        }}
+      ></Carousel>
+    </div>
+  );
 }
 
 export default App;
