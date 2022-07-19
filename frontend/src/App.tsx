06ede1b59526a6ca2fd0e419e90b62b587c899a0
xiaoshihui <758676464@qq.com>
2022-07-19 19:56:01 +0800

    feat(carousel): 无限循环轮播功能首次提交

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,47 @@
+/**
+* @decsription 入口文件
+* @author xiaoshihui 
+* @Date 2022-07-19
+*/
+
 import React from "react";
+import Carousel from './components/carousel'
 import "./App.css";
+import xphone from "./assets/iphone.png";
+import tablet from "./assets/tablet.png";
+import airpod from "./assets/airpods.png";
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
-}
+  /** 内容可配置 */
+  const config = [
+    {
+      key: 0,
+      title: ['xPhone'],
+      subTitle: ['Lots to love.Less to speed.', 'Starting at $399'],
+      image: xphone,
+      backgroundColor: '#111111',
+      color: '#fff',
+    },
+    {
+      key: 1,
+      title: ['Tablet'],
+      subTitle: ['Just the right amount of everything'],
+      image: tablet,
+      backgroundColor: '#fafafa',
+    },
+    {
+      key: 2,
+      title: ['Buy a tablet or xPhone for college.', 'Get arPods'],
+      subTitle: [],
+      image: airpod,
+      backgroundColor: '#f1f1f3',
+    },
+  ]
 
+  return (
+    <div className="App">
+      <Carousel config={config} />
+    </div>
+  )
+}
 export default App;
