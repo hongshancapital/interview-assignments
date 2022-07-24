261ba5068c2edeae3e8d0e80628f78accff9e595
丁超 <dingchao@dingchaodeMacBook-Pro.local>
2022-07-24 11:59:26 +0800

    整理代码结构添加注释

--- frontend/src/components/carousel/index.tsx
+++ frontend/src/components/carousel/index.tsx
@@ -16,10 +16,10 @@ import Indicators, { SelectFunction } from './indicators';
 /**
  * children: slide的内容
  * height?: slide的高度，外部指定，可以使数字可以是字符串
- * defaultActiveIndex: 初始化的时候激活哪个index的slide, index 从0 开始
- * duration: slides切换的时候的间隔时间
- * interval: 多久触发一次切换
- * timingFunction: 切换的动画函数
+ * defaultActiveIndex: 初始化的时候激活哪个index的slide, index从0开始
+ * duration: slides切换的间隔时间
+ * interval: sldies过久触发一次切换
+ * timingFunction: slide切换的动画函数定义
  */
 interface CarouselProps {
   children: ReactNode,
@@ -65,44 +65,61 @@ const Carousel: FC<CarouselProps> = memo(({
   timingFunction=TimingFunction.easeInOut,
   interval = 3,
 }) => {
+  /************* 组件生命周期全局变量定义 *************/
+  // 自动切换timer的引用
   const timerRef = useRef<NodeJS.Timeout | null>(null);
-  // 初始化完毕(宽度计算)，初始化渲染的flag
-  const [entered, setEntered] = useState(false);
   // carousel根节点的引用
   const rootRef = useRef<HTMLDivElement>(null);
   // 获取根节点的宽度
   const rootSize = useSize(rootRef);
+  /************* 组件生命周期全局变量定义*************/
+
+
+  /************* 组件状态定义 *************/
+  // 初始化完毕(宽度计算)，初始化渲染的flag
+  const [entered, setEntered] = useState(false);
   // 宽度状态
   const [sizeState, setSizeState] = useState<Size>(initSize);
   // 当前激活的卡片index 从0开始
   const [activeIndex, setActiveIndex] = useState(defaultActiveIndex);
+  /************* 组件状态定义 *************/
+
+
+  /************* 组件衍生状态和回调函数定义 *************/
+  // slides的个数
+  const childCount = Children.count(children);
+  // 每个slide的高度和宽度
+  const carouselItemStyle = useMemo(() => ({ width: sizeState.width, height }), [height, sizeState.width]);
+  // 循环激活索引
+  const cyclicedActiveIndex = activeIndex % childCount;
+  // slide container的样式，定义了总宽度和滑动动画
+  const sliderContainerStyle = useMemo(() => ({
+    width:  childCount * sizeState.width,
+    transitionDuration: `${duration}s`,
+    transitionTimingFunction: timingFunction,
+    transform: `translateX(${ -sizeState.width * (cyclicedActiveIndex) }px)`,
+  }), [childCount, sizeState.width, duration, timingFunction, cyclicedActiveIndex]);
+  // 改变当前激活的slide的回调，用于indicator的点击，因为需要传入子组件，所以需要用useCallback包裹以便于pure render的生效
+  const onSelectCarouselItem = useCallback<SelectFunction>((index) => {
+    setActiveIndex(index);
+  }, []);
+  /************* 组件衍生状态和回调函数定义 *************/
+
+
+  /************* 组件副作用定义 *************/
+  // 初始化carousel的大小和首次渲染状态
   useEffect(() => {
-    // type guide
+    // ts类型守卫
     if (rootSize !== undefined) {
-      // 初始化carousel的大小状态，同时将entered设置为true表示宽度初始化完毕，可以渲染carousel的item了
       setSizeState(rootSize);
       setEntered(true);
     }
   }, [rootSize]);
 
-  // 算出child的个数以便计算整个slide滑动容器的总宽度
-  const childCount = Children.count(children);
-  // 每一个卡片的样式，包括了卡片的高度和宽度
-  const carouselItemStyle = useMemo(() => ({ width: sizeState.width, height }), [height, sizeState.width]);
-  // slider 滑动容器的总宽度和初始化激活哪个卡片的位置计算
-  const sliderContainerStyle = useMemo(() => ({
-    width:  childCount * sizeState.width,
-    transitionDuration: `${duration}s`,
-    transitionTimingFunction: timingFunction,
-    transform: `translateX(${ -sizeState.width * (activeIndex % childCount) }px)`,
-  }), [childCount, sizeState.width, activeIndex, duration, timingFunction]);
-
-  const onSelectCarouselItem = useCallback<SelectFunction>((index) => {
-    setActiveIndex(index);
-  },[]);
-
+  // slide自动切换的副作用定义
   useEffect(() => {
     timerRef.current = global.setTimeout(() => {
+      // 由于对activeIndex进行了循环取模处理, 所以这里只需要每次向切换即可
       onSelectCarouselItem(activeIndex + 1)
     }, interval * 1000 );
 
@@ -112,6 +129,8 @@ const Carousel: FC<CarouselProps> = memo(({
       }
     }
   }, [activeIndex, onSelectCarouselItem, interval]);
+  /************* 组件副作用定义 *************/
+
 
   return (
     <div className={style.CarouseRoot} ref={rootRef}>
@@ -129,7 +148,7 @@ const Carousel: FC<CarouselProps> = memo(({
           )
         }
         <Indicators
-          activeIndex={activeIndex % childCount}
+          activeIndex={cyclicedActiveIndex}
           count={childCount}
           progressDuration={interval}
           onSelect={onSelectCarouselItem}
