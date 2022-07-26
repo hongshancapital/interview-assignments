535e77cc6f302bf393bd2758d348f6d1af56e7d7
Tianxi Ku <kutianxi@outlook.com>
2022-07-26 20:54:12 +0800

    add class names to carousel component

--- frontend/src/carousel/carousel.test.tsx
+++ frontend/src/carousel/carousel.test.tsx
@@ -1,19 +1,53 @@
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import React from 'react';
 import { Carousel } from './carousel';
 
 describe('Carousel', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
   it('should render children', () => {
     const { getByText } = render(
-      <Carousel>
-        <div>firstChild</div>
-        <div>secondChild</div>
-        <div>thirdChild</div>
+      <Carousel slideDurationMs={100}>
+        <div>firstSlide</div>
+        <div>secondSlide</div>
+        <div>thirdSlide</div>
       </Carousel>
     );
 
-    expect(getByText("firstChild")).toBeTruthy();
-    expect(getByText("secondChild")).toBeTruthy();
-    expect(getByText("thirdChild")).toBeTruthy();
+    expect(getByText('firstSlide')).toBeTruthy();
+    expect(getByText('secondSlide')).toBeTruthy();
+    expect(getByText('thirdSlide')).toBeTruthy();
+  });
+
+  it('should render fist slide as active by default', () => {
+    const { getByTestId } = render(
+      <Carousel slideDurationMs={200}>
+        <div>firstSlide</div>
+        <div>secondSlide</div>
+      </Carousel>
+    );
+
+    expect(getByTestId('carousel-slide-0')).toHaveClass('carouselSlide__active');
+    expect(getByTestId('carousel-slide-1')).not.toHaveClass('carouselSlide__active');
+  });
+
+  it('should render second slide as active after duration ms', () => {
+    const slideDurationMs = 200;
+
+    const { getByTestId } = render(
+      <Carousel slideDurationMs={slideDurationMs}>
+        <div>firstSlide</div>
+        <div>secondSlide</div>
+      </Carousel>
+    );
+
+    act(() => {
+      jest.advanceTimersByTime(slideDurationMs);
+    });
+
+    expect(getByTestId('carousel-slide-0')).not.toHaveClass('carouselSlide__active');
+    expect(getByTestId('carousel-slide-1')).toHaveClass('carouselSlide__active');
   });
 });
--- frontend/src/carousel/carousel.tsx
+++ frontend/src/carousel/carousel.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import { useCarouselActiveIndex } from './useCarouselActiveIndex';
 
 interface ICarouselProps {
   children: JSX.Element | JSX.Element[];
+  slideDurationMs: number;
 }
 
 export function Carousel(props: ICarouselProps): JSX.Element {
+  const children = Array.isArray(props.children) ? props.children : [props.children];
+  if (children.length === 0) {
+    return <></>;
+  }
+
+  const activeIndex = useCarouselActiveIndex(children.length, props.slideDurationMs);
   return (
-    <div>
-      {props.children}
+    <div className='carouselContainer'>
+      <div className='carouselSlidesWrapper'>
+        {
+          children.map((child, index) => (
+            <div key={index} data-testid={`carousel-slide-${index}`}
+                 className={`carouselSlide ${index === activeIndex ? 'carouselSlide__active' : ''}`}
+            >
+              {child}
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
