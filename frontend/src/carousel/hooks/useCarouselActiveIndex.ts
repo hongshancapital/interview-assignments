564140588b6695fe44da79d041936d728faa962e
Tianxi Ku <kutianxi@outlook.com>
2022-07-26 23:51:39 +0800

    minor refactor

--- frontend/src/carousel/hooks/useCarouselActiveIndex.ts
+++ frontend/src/carousel/hooks/useCarouselActiveIndex.ts
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useTimeout } from 'usehooks-ts';
 
-export function useCarouselActiveIndex(count: number, durationMs: number, defaultActiveIndex?: number): number {
-  const [activeIndex, setActiveIndex] = React.useState(defaultActiveIndex || 0);
+export function useCarouselActiveIndex(count: number, durationMs: number, defaultActiveIndex: number = 0): number {
+  const [activeIndex, setActiveIndex] = React.useState(defaultActiveIndex);
 
   useTimeout(() => {
     if (count > 0) {
