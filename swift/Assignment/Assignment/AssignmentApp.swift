b1dcfe1e74656e600a919ba0fb5b49ceed1b320f
shinolr <wyqbeta+github@gmail.com>
2022-07-27 17:39:08 +0800

    chore: init assignment project

---
+++ swift/Assignment/Assignment/AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentApp.swift
+//  Assignment
+//
+//  Created by shinolr on 2022/7/27.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
