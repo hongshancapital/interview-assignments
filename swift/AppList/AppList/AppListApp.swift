11a15dbbcf05b5c366650c6484c1c5662608cdc2
zhongxunchao <374884069@qq.com>
2022-08-01 23:34:29 +0800

    feat: 初始化App

---
+++ swift/AppList/AppList/AppListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppListApp.swift
+//  AppList
+//
+//  Created by 钟逊超 on 2022/8/1.
+//
+
+import SwiftUI
+
+@main
+struct AppListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift/AppList/AppList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppList
+//
+//  Created by 钟逊超 on 2022/8/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
