75b69f7b0f97299af80f5302eccb0228eeb7f46f
zhongxunchao <374884069@qq.com>
2022-08-02 00:11:11 +0800

    feat: 可以展示列表

--- swift/AppList/AppList/Home/HomeView.swift
+++ swift/AppList/AppList/Home/HomeView.swift
@@ -11,9 +11,38 @@ struct HomeView: View {
     
     @ObservedObject var viewModel = HomeViewModel()
     
+    private var nextSeq: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).onAppear {
-            viewModel.printAppList()
+        if viewModel.isLoading {
+            ActivityIndicatorView(style: .medium).onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    self.viewModel.fetchSubsequentAppList()
+                }
+            }
+        } else {
+            ScrollView {
+                PullToRefreshView(header: RefreshDefaultHeader(), footer: RefreshDefaultFooter()) {
+                    ScrollView {
+                        ForEach(viewModel.appList) { appModel in
+                            AppCell(model: appModel)
+                        }
+                    }
+                }
+                .environmentObject(viewModel.listState)
+                
+                Spacer().frame(height: 60)
+            }
+            .background(Color(red: 241/255.0, green: 241/255.0, blue: 245/255.0))
+            .addPullToRefresh(isHeaderRefreshing: $viewModel.isHeaderRefreshing,
+                              onHeaderRefresh: {
+                viewModel.fetchPreviousAppList()
+            },
+                              isFooterRefreshing: $viewModel.isFooterRefreshing,
+                              onFooterRefresh: {
+                viewModel.fetchSubsequentAppList()
+            })
+            .navigationBarTitle("App")
         }
     }
 }
--- swift/AppList/AppList/Home/HomeViewModel.swift
+++ swift/AppList/AppList/Home/HomeViewModel.swift
@@ -10,12 +10,54 @@ import SwiftUI
 
 class HomeViewModel: ObservableObject {
     
+    private let APP_LIMIT_COUNT: Int = 20
+    private var seq: Int = 0
+    
     /// 获取原始数据
     private lazy var appSourceList: AppResultModel? = {
         return try? FileUtils.loadJson(fileName: "Apps")
     }()
     
-    func printAppList() {
-        print(appSourceList?.results.count)
+    @Published var appList: [AppModel] = []
+    @Published var isLoading: Bool = true
+    
+    @State var listState = ListState()
+    @State var isHeaderRefreshing: Bool = false
+    @State var isFooterRefreshing: Bool = false
+    
+    /// 异步获取应用列表
+    func fetchSubsequentAppList() {
+        defer {
+            isFooterRefreshing = false
+            isLoading = false
+        }
+        guard let sourceList = self.appSourceList?.results else {
+            // 抛出错误
+            return
+        }
+        
+        if seq < 0 {
+            // appList保持不变
+            return
+        }
+        
+        // 每次请求最多20个应用.
+        if sourceList.count - seq < APP_LIMIT_COUNT  {
+            let result = sourceList[seq...]
+            self.appList.append(contentsOf: result)
+            seq = -1
+            listState.setNoMore(true)
+        } else {
+            let lastSeq = seq+APP_LIMIT_COUNT-1
+            let result = sourceList[seq...lastSeq]
+            self.appList.append(contentsOf: result)
+            seq += APP_LIMIT_COUNT
+            listState.setNoMore(false)
+        }
+    }
+    
+    func fetchPreviousAppList() {
+        isLoading = false
+        isHeaderRefreshing = false
     }
 }
