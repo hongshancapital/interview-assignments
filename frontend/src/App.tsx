b4e0c8ce6c911abb09f2a84ca5e0700aa2f3cb80
zalatmza <zalatmza@gmail.com>
2022-08-04 22:27:22 +0800

    feat: init global css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { memo } from "react";
 import "./App.css";
+import { DURING, CONFIG } from "./constant";
+import { IConfig } from "./typings";
+
+const Card = memo(({
+  title = "",
+  content = [],
+  fontColor = "#000",
+  backgroundImage = "",
+}: IConfig) => {
+  console.log('backgroundImage', backgroundImage);
+
+  const style: React.CSSProperties = {
+    color: fontColor,
+  }
+
+  return (
+    <section style={style}>
+      <div className="title">{title}</div>
+      <div className="content">
+        {
+          content.map((text, index) => <p key={`${index}-${text}`} className="text">{text}</p>)
+        }
+      </div>
+    </section>
+  );
+});
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return (
+    <div className="App">
+      {
+        CONFIG.map((config, index) => {
+          const { title, content, fontColor, backgroundImage} = config;
+          return <Card
+            key={`${index}-${title}`}
+            title={title}
+            content={content}
+            fontColor={fontColor}
+            backgroundImage={backgroundImage}
+          />
+        })
+      }
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/constant.ts
@@ -0,0 +1,36 @@
+import airpods from "./assets/airpods.png";
+import iphone from "./assets/iphone.png";
+import tablet from "./assets/tablet.png";
+import { IConfig } from "./typings";
+
+export const DURING: number = 3000;
+
+export const CONFIG: IConfig[] = [
+  {
+    title: "231",
+    fontColor: "#fff",
+    backgroundImage: airpods,
+    content: [
+      "asdasd",
+      "asdasdasd",
+    ]
+  },
+  {
+    title: "231",
+    fontColor: "#fff",
+    backgroundImage: tablet,
+    content: [
+      "asdasd",
+      "asdasdasd",
+    ]
+  },
+  {
+    title: "231",
+    fontColor: "#fff",
+    backgroundImage: iphone,
+    content: [
+      "asdasd",
+      "asdasdasd",
+    ]
+  }
+]
