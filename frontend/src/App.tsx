a0a9eebe9d3e54c450bf22bc7225fffa64b2071f
zsirfs <zhenglfsir@gmail.com>
2022-08-14 14:41:01 +0800

    feat: 完善示例

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,31 +5,32 @@ import { FullScreen } from './components/FullScreen';
 
 const commonStyle = { fontSize: 32, fontWeight: 'bold', color: '#fff' };
 function App() {
-
   return (
     <div className="App">
-      <Carousel>
-        <Carousel.Item>
-          <FullScreen center style={{ ...commonStyle, backgroundColor: '#32bbeb' }}>
-            Hello World
-          </FullScreen>
-        </Carousel.Item>
-        <Carousel.Item>
-          <FullScreen center style={{ ...commonStyle, backgroundColor: '#45eebc' }}>
-            Thank you for viewing my work
-          </FullScreen>
-        </Carousel.Item>
-        <Carousel.Item>
-          <FullScreen center style={{ ...commonStyle, backgroundColor: '#bbddee' }}>
-            Destined to meet for thousands of miles
-          </FullScreen>
-        </Carousel.Item>
-        <Carousel.Item>
-          <FullScreen center style={{ ...commonStyle, backgroundColor: '#abadee' }}>
-            Bye~
-          </FullScreen>
-        </Carousel.Item>
-      </Carousel>
+      <FullScreen>
+        <Carousel>
+          <Carousel.Item>
+            <FullScreen center style={{ ...commonStyle, backgroundColor: '#32bbeb' }}>
+              Hello World
+            </FullScreen>
+          </Carousel.Item>
+          <Carousel.Item>
+            <FullScreen center style={{ ...commonStyle, backgroundColor: '#45eebc' }}>
+              Thank you for viewing my work
+            </FullScreen>
+          </Carousel.Item>
+          <Carousel.Item>
+            <FullScreen center style={{ ...commonStyle, backgroundColor: '#bbddee' }}>
+              Destined to meet for thousands of miles
+            </FullScreen>
+          </Carousel.Item>
+          <Carousel.Item>
+            <FullScreen center style={{ ...commonStyle, backgroundColor: '#abadee' }}>
+              Bye~
+            </FullScreen>
+          </Carousel.Item>
+        </Carousel>
+      </FullScreen>
     </div>
   );
 }
