3356ccb03acd1c08a3fab54a95f2656862c23297
lugy90 <naitianzong@outlook.com>
2022-08-15 00:18:04 +0800

    update

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen, within } from '@testing-library/react';
 import { KeyframeEffect, Animation } from './types'
 import App from '../App';
 
-test('how to test ?', async () => {
+test('test', async () => {
   window.KeyframeEffect = KeyframeEffect
   window.Animation = Animation
   render(<App />);
