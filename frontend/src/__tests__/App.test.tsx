6f624b1811fe5f71d6c68e8435082715d911a60c
lugy90 <naitianzong@outlook.com>
2022-08-15 02:17:14 +0800

    update: add test case

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { render, screen, within, cleanup } from '@testing-library/react';
 import { KeyframeEffect, Animation } from './types'
+import { initialData } from '../stores/AppContext'
 import App from '../App';
+
+beforeAll(() => {
+  window.KeyframeEffect = KeyframeEffect
+  window.Animation = Animation
+})
 
 beforeEach(cleanup)
 
 test('the number of poster and progress need to be the same', async () => {
-  window.KeyframeEffect = KeyframeEffect
-  window.Animation = Animation
   const { container } = render(<App />);
   const posters = container.getElementsByClassName('Poster');
   const progresses = container.getElementsByClassName('Progress');
   expect(posters.length).toBe(progresses.length);
 });
+
+test('check the poster info', async () => {
+  const { container } = render(<App />);
+  const posters = container.getElementsByClassName('Poster');
+  for(let i = 0; i < posters.length; i++) {
+    const pds = posters[i].getElementsByClassName('poster-des');
+    expect(pds[0].textContent).toBe(initialData.posters[i].descs[0].text)
+  }
+})
