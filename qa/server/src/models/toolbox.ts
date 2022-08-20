1ea36cff01b8413bb9dfc7137dfb248d53e8bf5e
‘zhandehuang’ <it_zdh@163.com307758534@qq.com>
2022-08-20 16:41:09 +0800

    dmm

--- qa/server/src/models/toolbox.ts
+++
@@ -1,5 +0,0 @@
-import { TOOLBOX_ENUM } from "../types";
-
-export const generateToolboxNames = () => {
-    return [TOOLBOX_ENUM.E2E, TOOLBOX_ENUM.HEADLESS_CHROME, TOOLBOX_ENUM.JEST, TOOLBOX_ENUM.SELENIUM];
-}
