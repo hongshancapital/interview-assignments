8228f7f70b35a48523e382319d1b2a41e59c90ed
zeyuyuan <yzy@pcomg.com>
2022-08-23 23:07:27 +0800

    feat: complete carousel

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const sleep = (time: number) => new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(null)
+  }, time)
+})
+
+test('test carousel step one', () => {
+  const {getByText} = render(<App/>);
+  const element = getByText(/Lots to love.Less to spend./i);
+  expect(element).toBeInTheDocument();
+});
+
+test('test carousel step two', async () => {
+  const {getByText} = render(<App/>);
+  await sleep(4000)
+  const element = getByText(/Just the right amount of everything./i);
+  expect(element).toBeInTheDocument();
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, {HTMLAttributes} from "react";
 import "./App.css";
+import Carousel from "./components/Carousel";
+import iphone from './assets/iphone.png'
+import tablet from './assets/tablet.png'
+import airpods from './assets/airpods.png'
+
+const getBgStyle = (url: string): HTMLAttributes<HTMLDivElement> => {
+  return {
+    style: {
+      backgroundImage: `url('${url}')`
+    }
+  }
+}
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return <div className="App">
+    <Carousel>
+      <div className="carousel-item carousel-item--iphone" {...getBgStyle(iphone)}>
+        <h2 className="title">xPhone</h2>
+        <p className="text">Lots to love.Less to spend.</p>
+        <p className="text">Starting at $399.</p>
+      </div>
+      <div className="carousel-item" {...getBgStyle(tablet)}>
+        <h2 className="title">Tablet</h2>
+        <p className="text">Just the right amount of everything.</p>
+      </div>
+      <div className="carousel-item" {...getBgStyle(airpods)}>
+        <h2 className="title">
+          <div>Buy a Tablet or xPhone for college.</div>
+          <div>Get arPods</div>
+        </h2>
+      </div>
+    </Carousel>
+  </div>;
 }
 
 export default App;
