f5e83a3cf3e6243b126d039f758ed9537fe2b1be
lsl52978 <2361042981@qq.com>
2022-08-26 21:23:59 +0800

    assignment accomplished

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
-
+import Carousel from './components/carousel'
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return <div className="App">
+    <Carousel interval={3000}>
+      <div className="iphone">
+        <h1 className="title">xPhone</h1>
+        <p className="info">Lots to love.Less to spend.</p>
+        <p className="info">Starting at $399.</p>
+      </div>
+      <div className="tablet">
+        <h1 className="title">Tablet</h1>
+        <p className="info">Just the right amount of everythins.</p>
+      </div>
+      <div className="airpods">
+        <h1 className="title">Buy a Tablet or xPhone for college.</h1>
+        <h1 className="title second-row">Get arPods.</h1>
+      </div>
+    </Carousel>
+  </div>;
 }
 
 export default App;
