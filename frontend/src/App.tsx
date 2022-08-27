a59e89b39c418514302a1f573a43fe8e05ee2541
zhoujj <1254883879@qq.com>
2022-08-27 23:24:53 +0800

    代码格式化

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,35 +3,35 @@ import "./App.css";
 import Carousel, {ImgInfo} from "./carousel";
 function App() {
   const items = useMemo(() => {
-    let items: ImgInfo[] =
-        [
-          {
-            key: 'iphone',
-            //图片地址
-            imgUrl: require("./assets/iphone.png"),
-            //图片文字描述
-            label: ['xPhone'],
-            note: ['Lots to love. Less to spend.', 'Starting at $399'],
-            color: 'white',
-          },
-          {
-            key: 'tablet',
-            //图片地址
-            imgUrl: require("./assets/tablet.png"),
-            //图片文字描述
-            label: ['Tablet'],
-            note: ['Just the right amount of everything.']
-          },
-          {
-            key: 'airpods',
-            //图片地址
-            imgUrl: require("./assets/airpods.png"),
-            //图片文字描述
-            label: ["Buy a Tablet or xPhone for college", "Get airPods."],
-          }
-        ]
-    ;
-    return items;
+      let items: ImgInfo[] =
+          [
+              {
+                  key: 'iphone',
+                  //图片地址
+                  imgUrl: require("./assets/iphone.png"),
+                    //图片文字描述
+                  label: ['xPhone'],
+                  note: ['Lots to love. Less to spend.', 'Starting at $399'],
+                  color: 'white',
+              },
+              {
+                  key: 'tablet',
+                  //图片地址
+                  imgUrl: require("./assets/tablet.png"),
+                  //图片文字描述
+                  label: ['Tablet'],
+                  note: ['Just the right amount of everything.']
+              },
+              {
+                  key: 'airpods',
+                  //图片地址
+                  imgUrl: require("./assets/airpods.png"),
+                  //图片文字描述
+                  label: ["Buy a Tablet or xPhone for college", "Get airPods."],
+              }
+          ]
+      ;
+      return items;
   }, []);
 
   return (
