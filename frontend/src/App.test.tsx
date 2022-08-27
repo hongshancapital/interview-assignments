9a9c3f4884ac1f33908021d605fabf1f7f24d3e3
zhoujj <1254883879@qq.com>
2022-08-27 23:19:09 +0800

    frontend job

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -3,7 +3,25 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByTestId } = render(<App />);
+  let duration = 3000;
+  //轮播容器
+  var carouselContainer = getByTestId('carousel-container');
+  //已经在DOM中
+  expect(carouselContainer).toBeInTheDocument();
+  //轮播时长每次3000ms
+  expect(carouselContainer.style).toContain(duration+'ms');
+  //当前应该是第一个
+  expect(carouselContainer.style).toContain('translateX(-0%)');
+  //当前轮播条
+  var step1 = getByTestId("iphone");
+  expect(step1).toBeInTheDocument();
+
+  //3秒之后再判断
+  setTimeout(function () {
+    carouselContainer = getByTestId('carousel-container');
+    //当前应该是第2个
+    expect(carouselContainer.style).toContain('translateX(-100%)');
+  }, duration)
+  
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,47 @@
-import React from "react";
+import React, { useMemo } from "react";
 import "./App.css";
-
+import Carousel, {ImgInfo} from "./carousel";
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  const items = useMemo(() => {
+    let items: ImgInfo[] =
+        [
+          {
+            key: 'iphone',
+            //图片地址
+            imgUrl: require("./assets/iphone.png"),
+            //图片文字描述
+            label: ['xPhone'],
+            note: ['Lots to love. Less to spend.', 'Starting at $399'],
+            color: 'white',
+          },
+          {
+            key: 'tablet',
+            //图片地址
+            imgUrl: require("./assets/tablet.png"),
+            //图片文字描述
+            label: ['Tablet'],
+            note: ['Just the right amount of everything.']
+          },
+          {
+            key: 'airpods',
+            //图片地址
+            imgUrl: require("./assets/airpods.png"),
+            //图片文字描述
+            label: ["Buy a Tablet or xPhone for college", "Get airPods."],
+          }
+        ]
+    ;
+    return items;
+  }, []);
+
+  return (
+      <div className="App">
+        <Carousel
+            duration={3000}
+            items={items}
+        ></Carousel>
+      </div>
+  );
 }
 
 export default App;
