0ed2f1450908b4c5267ff0327ed7cae5ee5359e7
takeItIzzy <q1367257497@gmail.com>
2022-09-02 00:53:54 +0800

    feat: 轮播功能开发完成

---
+++ frontend/src/components/Carousel/index.tsx
@@ -0,0 +1,5 @@
+import Carousel from "./Carousel";
+
+export default Carousel;
+export type { CarouselProps } from "./Carousel";
+export { default as Paper } from "./Paper";
---
+++ frontend/src/components/Carousel/types.ts
@@ -0,0 +1,6 @@
+import * as React from "react";
+
+export interface BaseComponent {
+  className?: string;
+  style?: React.CSSProperties;
+}
