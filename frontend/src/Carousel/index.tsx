832986edcad9dc3f082fd365662a2af06d88cc29
hardenzheng <2821084381@qq.com>
2022-09-04 20:42:49 +0800

    refactor: 代码重构。     1. 直接在父节点上监听动画结束；     2. 直接根据_curIndex的值，render时往目标progress上添加动画

--- frontend/src/Carousel/index.tsx
+++ frontend/src/Carousel/index.tsx
@@ -1,6 +1,6 @@
 
 import React, { ReactNode } from 'react'
-import { useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
 import './index.css'
 
 interface CarouselProps{
@@ -13,41 +13,11 @@ interface CarouselProps{
     children: ReactNode
 }
 function Carousel({stayDuration, switchDuration, size, children} : CarouselProps){
-    const refProgressList = useRef(null)
     const [_curIndex, setCurIndex] = useState(0)
+    const len = React.Children.count(children)
 
-    // 所有进度条添加动画监听 和 回收函数
-    useEffect(()=>{
-        const len = React.Children.count(children)
-
-        const progresses = getCurrent(refProgressList).childNodes
-        for(let i = 0; i < progresses.length; i++){
-            const current = progresses[i].childNodes[0]
-            current.onanimationend = ()=>{
-                current.className = "progress_inner"
-
-                setCurIndex((oldIndex)=> (oldIndex + 1) % len) 
-            }
-        }
-        return ()=>{
-            for(let i = 0; i < progresses.length; i++){
-                const current = progresses[i].childNodes[0]
-                current.onanimationend = null
-            }
-        }
-    },[children])
-
-    //给当前的progress添加动画
-    useEffect(()=>{
-        const len = React.Children.count(children)
-        if(_curIndex >= len)    return
-
-        const current = getCurrent(refProgressList).childNodes[_curIndex].childNodes[0]
-        current.className += " progress_change"
-    }, [_curIndex, children])
-
-    function getCurrent(refValue: any){
-        return refValue.current as any
+    const animationEndCallback = ()=>{
+        setCurIndex((oldIndex)=> (oldIndex + 1) % len) 
     }
 
     return (
@@ -57,11 +27,11 @@ function Carousel({stayDuration, switchDuration, size, children} : CarouselProps
             {children}
         </div>
 
-        <div ref={refProgressList} className="progress_list">
-            {React.Children.map(children, ()=>{
+        <div className="progress_list" onAnimationEnd={animationEndCallback}>
+            {React.Children.map(children, (child, index)=>{
                 return (
                 <div className="progress">
-                    <div className="progress_inner" style={{animationDuration: `${stayDuration / 1000}s`}}></div>
+                    <div className={`progress_inner ${_curIndex === index ? 'progress_change': ''}`} style={{animationDuration: `${stayDuration / 1000}s`}}></div>
                 </div>)
             })}
         </div>
