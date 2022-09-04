48d79ad829ad5389e63abb4fd1299d97684f22b8
takeItIzzy <q1367257497@gmail.com>
2022-09-04 20:14:28 +0800

    test: 为作业添加单元测试

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { act, render } from "@testing-library/react";
+import React from "react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("作业测试", function () {
+  it("三张图展示", async function () {
+    await act(async () => {
+      const { getByText } = render(<App />);
+
+      const xPhoneTitle = getByText("xPhone");
+      const xPhoneDesc = getByText(/Lots to love. Less to spend./);
+      const tabletTitle = getByText("Tablet");
+      const tabletDesc = getByText("Just the right amount of everything.");
+      const podsTitle = getByText(/Buy a Tablet or xPhone for college./);
+      expect(xPhoneTitle).toBeInTheDocument();
+      expect(xPhoneDesc).toBeInTheDocument();
+      expect(tabletTitle).toBeInTheDocument();
+      expect(tabletDesc).toBeInTheDocument();
+      expect(podsTitle).toBeInTheDocument();
+    });
+  });
 });
