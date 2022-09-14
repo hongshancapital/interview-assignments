be0278b7566867a79f3ea9f0ae43bf0927bbaec1
xiaoyue hu <xiaoyuehu@xiaoyuedeMacBook-Pro.local>
2022-09-14 18:50:28 +0800

    add slide

--- frontend/src/carousel/Carousel.tsx
+++ frontend/src/carousel/Carousel.tsx
@@ -1,16 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useAppSelector, useAppDispatch } from '../app/hooks';
-import {} from './carouselSlice';
+import { scrollTo } from './carouselSlice';
 import s from './Carousel.module.scss';
 
-const imgName = ["airpods.png","iphone.png","tablet.png"];
-
 export default function Carousel() {
+  const { active, imgInfo } = useAppSelector((state) => state.carousel)
   const dispatch = useAppDispatch();
+  let timer: number;
+  useEffect(() => {
+    const scrollExe = (activeIndex: number) => {
+
+      dispatch(scrollTo({ active: activeIndex }));
+
+      const next = (activeIndex + 1) % imgInfo.length;
+      timer = window.setTimeout(() => scrollExe(next), 10000);  
+    }
+    scrollExe(active);
+
+    return () => window.clearTimeout(timer);
+
+  }, []);
 
   return (
-    <div>
+    <div className={s.carousel}>
+      <div className={s['carousel-inner']}>
+        {imgInfo.map((info, index) => <div
+          key={index}
+          className={`${s['carousel-item']} ${s[info.position]}`}
+        >
+
+          {info.titles.map((title, tIndex) => <h1 key={tIndex}>{title}</h1>)}
+
+          {info.subtitles.map((subtitle, subIndex) => <h2 key={subIndex}>{subtitle}</h2>)}
+
+          <img src={require(`../assets/${info.imgName}`)} alt="" />
+
+        </div>)}
+      </div>
     </div>
   );
 }
--- frontend/src/carousel/carouselSlice.ts
+++ frontend/src/carousel/carouselSlice.ts
@@ -1,26 +1,91 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState, AppThunk } from '../app/store';
 
+export interface ImgInfo {
+  position: string;
+  name: string;
+  imgName: string;
+  titles: string[];
+  subtitles: string[]; 
+}
+
 export interface CarouselState {
-  value: number;
-  status: 'idle' | 'loading' | 'failed';
+  active: number;
+  imgInfo: ImgInfo[];
 }
 
 const initialState: CarouselState = {
-  value: 0,
-  status: 'idle',
+  active: 0,
+  imgInfo: [
+    {
+      position: 'active',
+      name: 'iphone',
+      imgName: 'iphone.png',
+      titles: ['xPhone'],
+      subtitles: ['Lots to love. Less to spend.', 'Starting at $399']
+    },
+    {
+      position: '',
+      name: 'tablet',
+      imgName: 'tablet.png',
+      titles: ['Tablet'],
+      subtitles: ['Just the right amount of everything.'] 
+    },
+    {
+      position: 'prev',
+      name: 'airpods',
+      imgName: 'airpods.png',
+      titles: ['Buy a Tablet or xPhone for college.', 'Get airpods'],
+      subtitles: [] 
+    }
+  ] 
 };
+
+const buildInfo = (imgInfo: ImgInfo[], active: number) => {
+  const prevIndex = (active - 1 + imgInfo.length) % imgInfo.length;
+  return imgInfo.map((info, index) => {
+    
+    if(index === active) {
+
+      return {
+        ...info,
+        position: 'active'
+      }
+    }
+
+    if(index === prevIndex) {
+
+      return {
+        ...info,
+        position: 'prev'
+      }
+    }
+
+    return {
+      ...info,
+      position: ''
+    }
+  });
+}
 
 export const carouselSlice = createSlice({
   name: 'carousel',
   initialState,
   reducers: {
-    increment: (state) => {
-        state.value += 1;
+
+    scrollTo: (state, action) => {
+      const { active } = action.payload;
+      console.log(active);
+
+      Object.assign(state, {
+        active,
+        imgInfo: buildInfo(state.imgInfo, active) 
+      });
+
     },
   },
 });
 
-export const { increment } = carouselSlice.actions;
+export const { scrollTo } = carouselSlice.actions;
 
 export default carouselSlice.reducer;
