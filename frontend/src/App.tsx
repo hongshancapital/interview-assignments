719e87b641504d9f7f8be164b26b4ea53ad30cd9
lirm15 <lirm15@chinaunicom.cn>
2022-09-27 18:52:37 +0800

    初次提交

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  return <div className="App">{
+    /* write your component here */
+    }</div>;
 }
 
 export default App;
