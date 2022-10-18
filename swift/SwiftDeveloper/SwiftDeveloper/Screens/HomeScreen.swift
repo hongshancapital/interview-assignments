df9a5dff74289b7e5a3ee3a596f2c6c7355d4221
wuzhe213 <wuzhe213@163.com>
2022-10-18 19:21:39 +0800

    init project

---
+++ swift/SwiftDeveloper/SwiftDeveloper/Screens/HomeScreen.swift
@@ -0,0 +1,8 @@
+//
+//  HomeScreen.swift
+//  SwiftDeveloper
+//
+//  Created by zhe wu on 2022/10/18.
+//
+
+import Foundation
---
+++ swift/SwiftDeveloper/SwiftDeveloper/SwiftDeveloperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDeveloperApp.swift
+//  SwiftDeveloper
+//
+//  Created by zhe wu on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDeveloperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
