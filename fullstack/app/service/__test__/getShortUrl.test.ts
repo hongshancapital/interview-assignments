7ebb5db50f844706b4cbd2e6632e469924e4f0a4
5percent <v@luzhe.info>
2022-10-25 20:31:08 +0800

    test: add ut

--- fullstack/app/service/__test__/getShortUrl.test.ts
+++ fullstack/app/service/__test__/getShortUrl.test.ts
@@ -1,20 +1,30 @@
 import { describe, expect, test, jest } from '@jest/globals';
-import { NOT_FOUND, DB_RUNTIME_ERROR, HASH_ERROR, PARAM_ERROR } from '../../common/errCode';
+import { DB_RUNTIME_ERROR, HASH_ERROR, BASE62_ERROR, PARAM_ERROR, SUCCESS } from '../../common/errCode';
 
 import { query, insert } from '../../database/db';
 import mmh from '../../common/mmh';
+import { encodeBase62 } from '../../common/base62';
 
 import { getShortUrl } from '../getShortUrl';
 import { DataModal } from '../..';
 
+const mockOriginUrl: string = 'https://www.baidu.com/';
+const mockHash: number = 307499014;
+const mockShortUrl: string = 'xNptn';
+
 const mockdata: Array<DataModal> = [{
-  short_url: 'xNptn',
-  origin_url: 'https://www.baidu.com/',
-  origin_hash: 307499014
+  short_url: mockShortUrl,
+  origin_url: mockOriginUrl,
+  origin_hash: mockHash
 }];
 
-const mockUrl: string = 'https://www.baidu.com/';
+const mockdata2: Array<DataModal> = [{
+  short_url: mockShortUrl,
+  origin_url: 'https://www.taobao.com',
+  origin_hash: mockHash
+}];
 
+const mockHash2: number = 307499015;
 const emptyMockdata: Array<DataModal> = [];
 
 jest.mock('../../database/db');
@@ -22,33 +32,59 @@ const mockedQuery = jest.mocked(query);
 const mockedInsert = jest.mocked(insert);
 
 jest.mock('../../common/mmh');
-const mockMmh = jest.mocked(mmh);
+const mockedMmh = jest.mocked(mmh);
+
+jest.mock('../../common/base62');
+const mockedBase62 = jest.mocked(encodeBase62);
 
 describe('getShortUrl function', () => {
   test('no shortUrl param', () => {
-    expect(getShortUrl('')).rejects.toBe(PARAM_ERROR);
+    return expect(getShortUrl('')).rejects.toBe(PARAM_ERROR);
   });
 
   test('hash failed', () => {
-    mockMmh.mockImplementation(() => NaN);
-    expect(getShortUrl(mockUrl)).rejects.toBe(HASH_ERROR);
+    mockedMmh.mockImplementation(() => NaN);
+    return expect(getShortUrl(mockOriginUrl)).rejects.toBe(HASH_ERROR);
   });
 
-  // test('error occurred while querying', () => {
-  // });
+  test('error occurred while querying', () => {
+    mockedMmh.mockImplementation(() => mockHash);
+    mockedQuery.mockImplementation(() => Promise.reject(DB_RUNTIME_ERROR));
+    return expect(getShortUrl(mockOriginUrl)).rejects.toBe(DB_RUNTIME_ERROR);
+  });
 
-  // test('could find short url', () => {
-  // });
+  test('could find short url', () => {
+    mockedQuery.mockImplementation(() => Promise.resolve(mockdata));
+    return expect(getShortUrl(mockOriginUrl)).resolves.toBe(mockShortUrl);
+  });
 
-  // test('could find short url but hash conflicts', () => {
-  // });
+  test('base62 failed', () => {
+    mockedQuery.mockImplementation(() => Promise.resolve(emptyMockdata));
+    mockedBase62.mockImplementation(() => '');
+    return expect(getShortUrl(mockOriginUrl)).rejects.toBe(BASE62_ERROR);
+  });
 
-  // test('base62 failed', () => {
-  // });
+  test('error occurred while inserting', () => {
+    mockedBase62.mockImplementation(() => mockShortUrl);
+    mockedInsert.mockImplementation(() => Promise.reject(DB_RUNTIME_ERROR));
+    return expect(getShortUrl(mockOriginUrl)).rejects.toBe(DB_RUNTIME_ERROR);
+  });
 
-  // test('error occurred while querying', () => {
-  // });
+  test('everything is ok', () => {
+    mockedInsert.mockImplementation(() => Promise.resolve(SUCCESS));
+    return expect(getShortUrl(mockOriginUrl)).resolves.toBe(mockShortUrl);
+  });
 
-  // test('everything is ok', () => {
-  // });
+  test('could find short url but hash conflicts', () => {
+    mockedMmh
+      .mockImplementationOnce(() => mockHash)
+      .mockImplementationOnce(() => mockHash2)
+    ;
+    mockedQuery
+      .mockImplementationOnce(() => Promise.resolve(mockdata2))
+      .mockImplementationOnce(() => Promise.resolve(emptyMockdata))
+    ;
+
+    return expect(getShortUrl(mockOriginUrl)).resolves.toBe(mockShortUrl);
+  });
 });
