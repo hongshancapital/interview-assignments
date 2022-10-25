b89aa2a1c56ebd5e76c695610020d82cdc0cc75c
nobody <2274871074@qq.com>
2022-10-25 09:35:10 +0800

    feat: add unit test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -3,8 +3,13 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders App', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Lots to love/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByText, queryAllByRole } = render(<App />);
+  const textElement = getByText(/Lots to love/i);
+  expect(textElement).toBeInTheDocument();
+
+  const imgElements = queryAllByRole('img')
+
+  expect(imgElements).toHaveLength(3)
+  expect(imgElements[0]).toHaveClass('item-image')
 });
 
