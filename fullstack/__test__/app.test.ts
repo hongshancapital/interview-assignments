fa226b1ff58c558e3ee13a56e753a4cd104fd1be
5percent <v@luzhe.info>
2022-10-26 00:49:59 +0800

    test: add API IT cases & upload coverage

--- fullstack/__test__/app.test.ts
+++ fullstack/__test__/app.test.ts
@@ -1,9 +1,12 @@
-import { describe, expect, test, afterAll } from '@jest/globals';
+import { describe, expect, test, afterAll, jest } from '@jest/globals';
 import request from 'supertest';
 import { app, server } from '../app/app';
 
 import { SHORT_DOMAIN } from '../app/common/url';
-import { SUCCESS, NOT_FOUND } from '../app/common/errCode';
+import { SUCCESS, NOT_FOUND, DB_RUNTIME_ERROR, PARAM_ERROR } from '../app/common/errCode';
+
+import * as db from '../app/database/db';
+
 
 const fetchAPI = async (url: string, method: string) => {
   let res: any;
@@ -22,14 +25,31 @@ const fetchAPI = async (url: string, method: string) => {
 afterAll(() => { server.close() });
 
 describe('API IT', () => {
+  test('get by a empty url', async () => {
+    const resData = await fetchAPI('', 'get');
+
+    expect(resData.errCode).toBe(PARAM_ERROR);
+  });
+
   test('get by a null url', async () => {
     const resData = await fetchAPI(SHORT_DOMAIN + 'abcde', 'get');
 
     expect(resData.errCode).toBe(NOT_FOUND);
   });
 
+
   let shortUrl: string = '';
   const originUrl = 'https://www.baidu.com/s?' + Date.now();
+
+  jest.mock('../app/database/db');
+  jest.spyOn(db, 'insert').mockResolvedValueOnce(DB_RUNTIME_ERROR);
+  test('error occured while posting', async () => {
+
+    const resData = await fetchAPI(originUrl, 'post');
+
+    expect(resData.errCode).toBe(DB_RUNTIME_ERROR);
+  });
+
   test('post a new url', async () => {
     const resData = await fetchAPI(originUrl, 'post');
 
