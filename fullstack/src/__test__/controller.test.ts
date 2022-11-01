b0cfb77edfca145fbcdad03030779cf2c65d0cf4
lizhaoguang <mx1700@gmail.com>
2022-11-01 21:23:30 +0800

    优化单元测试

--- fullstack/src/__test__/controller.test.ts
+++ fullstack/src/__test__/controller.test.ts
@@ -11,21 +11,19 @@ const IdTranslatorMock = IdTranslator as jest.MockedClass<typeof IdTranslator>;
 const CacheMock = Cache as jest.MockedClass<typeof Cache>;
 
 describe("controller test", () => {
-  const dbAccess = new DbAccessMock({} as any);
-  const cache = new CacheMock({} as any);
-  const idTranslator = new IdTranslatorMock();
+  const dbAccess = jest.mocked(new DbAccess({} as any));
+  const cache = jest.mocked(new Cache({} as any));
+  const idTranslator = jest.mocked(new IdTranslator());
   const controller = new Controller(dbAccess, cache, idTranslator);
 
   beforeEach(() => {
-    // Clear all instances and calls to constructor and all methods:
     DbAccessMock.mockClear();
     CacheMock.mockClear();
     IdTranslatorMock.mockClear();
-
   });
 
   test('getUrl decode return null', async () => {
-    (idTranslator.decode as jest.MockedFunction<typeof idTranslator.decode>).mockReturnValue(null);
+    idTranslator.decode.mockReturnValue(null);
     const v = await controller.getUrl('test');
     expect(v).toBeNull();
     expect(idTranslator.decode).toBeCalledWith('test');
@@ -33,8 +31,8 @@ describe("controller test", () => {
 
   test('getUrl has cache', async () => {
     const e = 'https://baidu.com';
-    (idTranslator.decode as jest.MockedFunction<typeof idTranslator.decode>).mockReturnValue(1);
-    (cache.get as jest.MockedFunction<typeof cache.get>).mockResolvedValue(e);
+    idTranslator.decode.mockReturnValue(1);
+    cache.get.mockResolvedValue(e);
 
     const v = await controller.getUrl('test');
     expect(v).toBe(e);
@@ -43,9 +41,9 @@ describe("controller test", () => {
   });
 
   test('getUrl no cache, no db result', async () => {
-    (idTranslator.decode as jest.MockedFunction<typeof idTranslator.decode>).mockReturnValue(1);
-    (cache.get as jest.MockedFunction<typeof cache.get>).mockResolvedValue(null);
-    (dbAccess.getUrlById as jest.MockedFunction<typeof dbAccess.getUrlById>).mockResolvedValue(null);
+    idTranslator.decode.mockReturnValue(1);
+    cache.get.mockResolvedValue(null);
+    dbAccess.getUrlById.mockResolvedValue(null);
 
     const v = await controller.getUrl('test');
     expect(v).toBe(null);
@@ -57,9 +55,9 @@ describe("controller test", () => {
 
   test('getUrl no cache, has result', async () => {
     const e = 'https://baidu.com';
-    (idTranslator.decode as jest.MockedFunction<typeof idTranslator.decode>).mockReturnValue(1);
-    (cache.get as jest.MockedFunction<typeof cache.get>).mockResolvedValue(null);
-    (dbAccess.getUrlById as jest.MockedFunction<typeof dbAccess.getUrlById>).mockResolvedValue(e);
+    idTranslator.decode.mockReturnValue(1);
+    cache.get.mockResolvedValue(null);
+    dbAccess.getUrlById.mockResolvedValue(e);
 
     const v = await controller.getUrl('test');
     expect(v).toBe(e);
@@ -72,8 +70,8 @@ describe("controller test", () => {
   test('saveUrl exists', async () => {
     const url = 'https://baidu.com';
     const shortUrl = "abcde";
-    (dbAccess.getIdByUrl as jest.MockedFunction<typeof dbAccess.getIdByUrl>).mockResolvedValue(1);
-    (idTranslator.encode as jest.MockedFunction<typeof idTranslator.encode>).mockReturnValue(shortUrl);
+    dbAccess.getIdByUrl.mockResolvedValue(1);
+    idTranslator.encode.mockReturnValue(shortUrl);
 
     const v = await controller.saveUrl(url);
     expect(v).toBe(shortUrl);
@@ -84,9 +82,9 @@ describe("controller test", () => {
   test('saveUrl not exists', async () => {
     const url = 'https://baidu.com';
     const shortUrl = "abcde";
-    (dbAccess.getIdByUrl as jest.MockedFunction<typeof dbAccess.getIdByUrl>).mockResolvedValue(null);
-    (idTranslator.encode as jest.MockedFunction<typeof idTranslator.encode>).mockReturnValue(shortUrl);
-    (dbAccess.save as jest.MockedFunction<typeof dbAccess.save>).mockResolvedValue(1);
+    dbAccess.getIdByUrl.mockResolvedValue(null);
+    idTranslator.encode.mockReturnValue(shortUrl);
+    dbAccess.save.mockResolvedValue(1);
 
     const v = await controller.saveUrl(url);
     expect(v).toBe(shortUrl);
@@ -94,5 +92,4 @@ describe("controller test", () => {
     expect(idTranslator.encode).toBeCalledWith(1);
     expect(dbAccess.save).toBeCalledWith(url);
   })
-
 });
