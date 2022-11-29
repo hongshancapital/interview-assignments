ee6fb97e2ae2722fb84db7bfcceee9c4b6afb3ab
jalyfeng <jalyfeng@gmail.com>
2022-11-29 14:54:49 +0800

    feat: 初始化工程

---
+++ swift/AppDemo/AppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppDemo
+//
+//  Created by jaly on 2022/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
