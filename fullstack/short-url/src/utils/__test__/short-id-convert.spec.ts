707a13feb3ac4644c1bd281f5be8c9732a60e226
Weili Y <yi@weiliy.net>
2023-01-05 16:57:45 +0800

    rename type Id to NumberId and ensure result must be number

--- fullstack/short-url/src/utils/__test__/short-id-convert.spec.ts
+++ fullstack/short-url/src/utils/__test__/short-id-convert.spec.ts
@@ -1,26 +1,32 @@
-import * as conv from '../short-id-converter'
+import * as conv from '../short-id-converter';
 
 describe('short id convert', () => {
   it('should return fixed uniq 8 length short id when encode give any int number <= 194754273880', async () => {
-    const hashIdMin = conv.encode(0)
-    const hashIdMax = conv.encode(194754273880)
-    const hashIdOver = conv.encode(194754273881)
+    const hashIdMin = conv.encode(0);
+    const hashIdMax = conv.encode(194754273880);
+    const hashIdOver = conv.encode(194754273881);
 
-    expect(hashIdMin).toHaveLength(8)
-    expect(hashIdMax).toHaveLength(8)
+    expect(hashIdMin).toHaveLength(8);
+    expect(hashIdMax).toHaveLength(8);
 
-    expect(hashIdMin).toMatchSnapshot()
-    expect(hashIdMax).toMatchSnapshot()
+    expect(hashIdMin).toMatchSnapshot();
+    expect(hashIdMax).toMatchSnapshot();
 
-    expect(hashIdMin).not.toEqual(hashIdMax)
-    expect(hashIdOver).toHaveLength(9)
+    expect(hashIdMin).not.toEqual(hashIdMax);
+    expect(hashIdOver).toHaveLength(9);
   });
 
   it('should return original number id when decode give short id', async () => {
-    const hashIdOne = conv.encode(3)
-    const hashIdTwo = conv.encode(4)
+    const hashIdOne = conv.encode(3);
+    const hashIdTwo = conv.encode(4);
+    const hashIdThree = conv.encode(194754273880);
 
-    expect(conv.decode(hashIdOne)).toEqual(3)
-    expect(conv.decode(hashIdTwo)).toEqual(4)
+    expect(conv.decode(hashIdOne)).toEqual(3);
+    expect(conv.decode(hashIdTwo)).toEqual(4);
+    expect(conv.decode(hashIdThree)).toEqual(194754273880);
+  });
+
+  it('should throw error when decode did not have number result', async () => {
+    expect(() => conv.decode('ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ')).toThrowError();
   });
 });
--- fullstack/short-url/src/utils/short-id-converter.ts
+++ fullstack/short-url/src/utils/short-id-converter.ts
@@ -1,11 +1,22 @@
-import Hashids from 'hashids'
+import Hashids from 'hashids';
 
-export type Id = number
-export type HashId = string
+export type NumberId = number;
+export type HashId = string;
 
-const base58alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+const base58alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
 
-const hashids = new Hashids("short-url", 8, base58alphabet);
+const hashids = new Hashids('short-url', 8, base58alphabet);
 
-export const encode = (id: Id) : HashId => hashids.encode(id)
-export const decode = (hashId: HashId) => hashids.decode(hashId)[0]
+export const encode = (id: NumberId): HashId => hashids.encode(id);
+export const decode = (hashId: HashId): NumberId => {
+  const result = hashids.decode(hashId);
+  if (result.length !== 1) {
+    throw new Error('No Result');
+  }
+  const numberId = result[0];
+  if (typeof numberId !== 'number') {
+    throw new Error('Result Not Number');
+  }
+
+  return numberId;
+};
