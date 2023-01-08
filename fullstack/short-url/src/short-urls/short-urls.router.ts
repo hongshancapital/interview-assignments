cdfcd93a8c8b26b0800c2f1a818c8d642c3f6a86
Weili Y <yi@weiliy.net>
2023-01-09 00:39:44 +0800

    catch async error

--- fullstack/short-url/src/short-urls/short-urls.router.ts
+++ fullstack/short-url/src/short-urls/short-urls.router.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 
 import { getContext } from '@/utils/context';
 import { ShortUrlCreate } from './short-url.interface';
@@ -6,31 +7,37 @@ import * as shortUrlsService from './short-urls.service';
 
 export const shortUrlsRouter = express.Router();
 
-shortUrlsRouter.get('/:shortId', async (req, res) => {
-  const shortId = req.params.shortId;
-  const ctx = getContext(res);
-  const shortUrl = await shortUrlsService.find(shortId, ctx);
-  res.format({
-    html: () => {
-      if (shortUrl) {
-        res.redirect(shortUrl.url);
-      } else {
-        res.status(404).send('<p>not found</p>');
-      }
-    },
-    default: () => {
-      if (shortUrl) {
-        res.json(shortUrl);
-      } else {
-        res.status(404).send('not found');
-      }
-    },
-  });
-});
+shortUrlsRouter.get(
+  '/:shortId',
+  asyncHandler(async (req, res) => {
+    const shortId = req.params.shortId;
+    const ctx = getContext(res);
+    const shortUrl = await shortUrlsService.find(shortId, ctx);
+    res.format({
+      html: () => {
+        if (shortUrl) {
+          res.redirect(shortUrl.url);
+        } else {
+          res.status(404).send('<p>not found</p>');
+        }
+      },
+      default: () => {
+        if (shortUrl) {
+          res.json(shortUrl);
+        } else {
+          res.status(404).send('not found');
+        }
+      },
+    });
+  })
+);
 
-shortUrlsRouter.post('/', async (req, res) => {
-  const ctx = getContext(res);
-  const shortUrlCreate: ShortUrlCreate = req.body;
-  const shortUrl = await shortUrlsService.create(shortUrlCreate, ctx);
-  res.json(shortUrl);
-});
+shortUrlsRouter.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    const ctx = getContext(res);
+    const shortUrlCreate: ShortUrlCreate = req.body;
+    const shortUrl = await shortUrlsService.create(shortUrlCreate, ctx);
+    res.json(shortUrl);
+  })
+);
