67ec65964b11ce09e56b0aafaca7787834b26697
jay-yu-w <ywj828@gmail.com>
2023-01-09 16:13:08 +0800

    fix: remove useless async

--- frontend/src/components/Carousel/Carousel.test.tsx
+++ frontend/src/components/Carousel/Carousel.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import Carousel, { carouselMockData } from '.';
 
 describe('<Carousel />', () => {
-  it('should display the first slide when first rendered', async () => {
+  it('should display the first slide when first rendered', () => {
     const { getByTestId, container } = render(
       <Carousel contents={carouselMockData} />
     );
