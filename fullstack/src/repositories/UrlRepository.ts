e9f467318abc5f1d2f05f6602673fce8c1911b4e
qizf <qizf@outlook.com>
2023-01-10 22:09:50 +0800

    fix: short path  colision

--- fullstack/src/repositories/UrlRepository.ts
+++ fullstack/src/repositories/UrlRepository.ts
@@ -1,14 +1,35 @@
 import { BASE_URL } from '@base/env';
 import IUrlRepository from './IUrlRepository';
 import UrlSchema from '../schemas/UrlSchema';
+import BizError from '@error/BizError';
 
 class UrlRepository implements IUrlRepository {
+
+  static RETRY_TIMES = 3;
+
   public async createShortUrl(originalUrl: string): Promise<string> {
     const url = await UrlSchema.findOne({ originalUrl });
 
     if (url) return BASE_URL + url.shortPath;
 
-    const shortPath = Math.random().toString(36).substring(2, 8);
+    let shortPath = '';
+    let tryTime = 0;
+
+    // 防碰撞处理
+    while(tryTime < UrlRepository.RETRY_TIMES) {
+      const random = Math.random().toString(36).substring(2, 8);
+      const findPath = await UrlSchema.findOne({ shortPath: random  })
+      if (!findPath) {
+        shortPath = random
+        break;
+      }
+      tryTime += 1;
+    }
+
+    if (!shortPath) {
+      throw new BizError('Url generate failed', 500);
+    }
+
     await UrlSchema.create({ originalUrl, shortPath });
 
     return BASE_URL + shortPath;
