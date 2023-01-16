88b3a610a449db3d7b511d444ddfc89160dee3e9
Mengchentony <mengchentony@gmail.com>
2023-01-16 14:23:36 +0800

    fix: sql query

--- fullstack/src/repos/url-repo.ts
+++ fullstack/src/repos/url-repo.ts
@@ -22,6 +22,33 @@ async function getOne(urlHash: string): Promise<IUrl | null> {
  */
 async function getOneByShortUrl(shortUrl: string): Promise<IUrl | null> {
   const db = await orm.openDb();
+  /** 如果需要改写为sql query，取决于orm框架，
+  // 这里以squelize为例，假设模型为Url
+  const url = await Url.findOne({ where: { shortUrl } });
+  return url
+
+  //如果是mongoos，写法也相同，
+  // 如果是typeORM，需要创建实体，并注册到connection连接中
+  import { Entity, PrimaryColumn } from "typeorm";
+
+  @Entity(“short_url_map”)
+  export class Url {
+      @PrimaryColumn()
+      id: number;
+
+      lastName: string;
+  }
+  import { getConnection } from "typeorm";
+
+  const url = await getConnection()
+    .createQueryBuilder()
+    .select("url")
+    .from(Url, "url")
+    .where("url.short_url = :shortUrl", { shortUrl })
+    .getOne();
+
+  中午时间短暂关系，这里只进行了mock处理，我想您要求改写的sqlquery就在上面
+  */
   for (const url of db.urls) {
     if (url.shortUrl === shortUrl) {
       return url;
