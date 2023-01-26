cb047a03c4606f7dfcfc44140cd507e41a4a648d
cyrilluce <clue@163.com>
2023-01-26 16:47:53 +0800

    feat: 实现轮播

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,38 @@
 import './App.css';
+import Carousel from './Carousel';
 
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  return <div className='App'>
+    {/* write your component here */}
+    <Carousel list={[
+      {
+        className: 'card card-1',
+        content: <>
+          <span style={{ color: '#fff' }}>
+            <h1 className='title'>xPhone</h1>
+            <p className="text">Lots to love. Less to spend</p>
+            <p className="text">Starting at $399.</p>
+          </span>
+
+          <div className='img img-1'></div>
+        </>
+      },
+      {
+        className: 'card card-2',
+        content: <>
+          <h1 className='title' >Tablet</h1>
+          <p className="text">Just the right amount to every thing.</p>
+        </>
+      },
+      {
+        className: 'card card-3',
+        content: <>
+          <h1 className='title' >Buy a Tablet or xPhone for college.</h1>
+          <h1 className='title' >Get arPods.</h1>
+        </>
+      }
+    ]} duration={3000}/>
+  </div>;
 }
 
 export default App;
---
+++ frontend/src/Carousel/ProgressBar.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+import './ProgressBar.css'
+
+export interface ProgressBarProps{
+    size: number
+    index: number
+    duration: number
+}
+export default function ProgressBar(props: ProgressBarProps){
+    const {size = 0, index = 0, duration} = props
+    useEffect(()=>{}, [index, size])
+    return <div className="_progress-bar">
+        {(new Array(size).fill(1).map((_, i) => <div key={i} className="_item">
+            {i === index && <Progress key={Math.random()} duration={duration} />}
+        </div>))}
+    </div>
+}
+
+function Progress(props: {duration: number}){
+    const [started, setStarted] = useState(false)
+    // 渲染后开始变形动画
+    useEffect(()=>{
+        const timer = requestAnimationFrame(()=>setStarted(true))
+        return ()=>cancelAnimationFrame(timer)
+    }, [])
+    return <div className="_progress" style={{
+        width: started ? '100%' : 0,
+        transitionDuration: `${props.duration}ms`
+    }}></div>
+}
