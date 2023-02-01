1c10d4fbcc514d68baa1591cee098e3724d043b5
coffee <704471294@qq.com>
2023-02-01 23:39:43 +0800

    fix: app test cannot find element

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { container } = render(<App />);
+  const rootElement = container.querySelector('.App');
+  expect(rootElement).toBeInTheDocument();
 });
