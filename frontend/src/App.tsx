9561181c9fcfe3bc9a6d6c4a9802bc8eeb93c5ce
fortune_cookie <767077147@qq.com>
2023-02-02 11:31:13 +0800

    feat: carousel demo is done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,45 @@
-import './App.css';
+import styles from './app.module.scss'
+import Carousel from './components/Carousel'
+import airpodsImg from './assets/airpods.png'
+import iphoneImg from './assets/iphone.png'
+import tabletImg from './assets/tablet.png'
+import CarouselItem from './components/CarouselItem'
+import { CSSProperties } from 'react'
+import classnames from 'classnames'
 
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  const carouselItemStyle: CSSProperties = {
+    width: '1000px',
+    height: '700px'
+  }
+
+  return (
+    <div className={styles.app}>
+      <Carousel autoplay>
+        <CarouselItem img={iphoneImg} style={carouselItemStyle}>
+          <section className={styles.content}>
+            <h2 className={classnames(styles.title, styles.white)}>xPhone</h2>
+            <p className={classnames(styles.description, styles.white)}>
+              Lots to love. Less to spend.
+            </p>
+            <p className={classnames(styles.description, styles.white)}>Staring at $399.</p>
+          </section>
+        </CarouselItem>
+        <CarouselItem img={tabletImg} style={carouselItemStyle}>
+          <section className={styles.content}>
+            <h2 className={styles.title}>Tablet</h2>
+            <p className={styles.description}>Just the right amount of everything</p>
+          </section>
+        </CarouselItem>
+        <CarouselItem img={airpodsImg} style={carouselItemStyle}>
+          <section className={styles.content}>
+            <h2 className={styles.title}>Buy a Tablet or xPhone for college.</h2>
+            <h2 className={styles.title}>Get arPods.</h2>
+          </section>
+        </CarouselItem>
+      </Carousel>
+    </div>
+  )
 }
 
-export default App;
+export default App
