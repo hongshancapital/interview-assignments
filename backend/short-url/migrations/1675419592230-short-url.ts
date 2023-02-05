4dfac2300c8f03fdc7414f9a42f07743199be53c
gaocong <gaocong@authing.cn>
2023-02-05 22:03:33 +0800

    feat: db init

--- backend/short-url/migrations/1675419592230-short-url.ts
+++ backend/short-url/migrations/1675419592230-short-url.ts
@@ -1,5 +1,8 @@
 import { MigrationInterface, QueryRunner, Table, TableIndex } from "typeorm";
 
+
+// "CREATE TABLE `shorturls` (`id` int NOT NULL, `short_url` varchar(8) NOT NULL, `long_url` varchar(4096) NOT NULL,`created_at` timestamp(32) NOT NULL,`updated_at` timestamp(32) NOT NULL, UNIQUE INDEX `IDX_short_url_long_url` (`short_url,long_url`), PRIMARY KEY (`id`)) ENGINE=InnoDB",
+
 const TABLE_NAME = 'shorturls';
 const ShortUrlIndex = 'IDX_short_url_long_url';
 export class shortUrl1675419592230 implements MigrationInterface {
@@ -25,15 +28,15 @@ export class shortUrl1675419592230 implements MigrationInterface {
               },
               {
                 name: 'created_at',
-                type: 'bigint',
+                type: 'timestamp',
+                length: '32',
                 comment: '创建时间',
-                default: Date.now(),
               },
               {
                 name: 'updated_at',
-                type: 'bigint',
+                type: 'timestamp',
+                length: '32',
                 comment: '更新时间',
-                default: Date.now(),
               },
             ]
           }),
