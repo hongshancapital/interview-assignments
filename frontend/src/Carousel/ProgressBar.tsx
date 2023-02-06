bf20cdcf4adc397f1caff7fba48a1a4a47e62baa
cyrilluce <clue@163.com>
2023-02-06 16:58:29 +0800

    fix: 学习了一下css3 animation

--- frontend/src/Carousel/ProgressBar.tsx
+++ frontend/src/Carousel/ProgressBar.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react"
 import './ProgressBar.css'
 
 export interface ProgressBarProps {
@@ -15,24 +14,13 @@ export default function ProgressBar(props: ProgressBarProps) {
 
     return <div className="_progress-bar" data-testid="progress-bar">
         {(new Array(size).fill(1).map((_, i) => <div key={i} className="_item" data-testid="progress-item">
-            {i === index && <Progress key={Math.random()} duration={duration} />}
+            {i === index && <div
+                className="_progress"
+                style={{
+                    transitionDuration: `${duration}ms`
+                }}
+                data-testid="progress"
+            ></div>}
         </div>))}
     </div>
-}
-
-function Progress(props: { duration: number }) {
-    const [started, setStarted] = useState(false)
-    // 渲染后开始变形动画
-    useEffect(() => {
-        const timer = requestAnimationFrame(() => setStarted(true))
-        return () => cancelAnimationFrame(timer)
-    }, [])
-    return <div
-        className="_progress"
-        style={{
-            width: started ? '100%' : 0,
-            transitionDuration: `${props.duration}ms`
-        }}
-        data-testid="progress"
-    ></div>
 }
