92d2dd1f0007a940b88775ec37721a00fc3e04a6
MickleWen <30724282+MickleWen@users.noreply.github.com>
2023-02-06 23:08:08 +0800

    Update site.ts
    增加校验

--- backend/server/app/validator/site.ts
+++ backend/server/app/validator/site.ts
@@ -1,12 +1,12 @@
 import { Application } from 'egg';
 
-// const urlPattern = new RegExp('(http(|s)://){0,1}([\w-]+\.)+[\w-]+(/)?')
+const urlPattern = new RegExp('(http(|s)://){0,1}([\w-]+\.)+[\w-]+(/)?')
 
 module.exports = (app: Application) => {
     const Joi = app.Joi;
     return {
         encodeUrl: Joi.object().keys({
-            url: Joi.string(), // Joi.string().regex(urlPattern),
+            url: Joi.string().regex(urlPattern),
         })
     };
 }
