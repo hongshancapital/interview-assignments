1da8c9fe812a4efa6aec57d339311faa86609829
xudong <xudong@xueqiu.com>
2023-02-07 17:41:13 +0800

    Initial Commit

---
+++ RedwoodTestProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RedwoodTestProject
+//
+//  Created by 徐栋 on 2023/2/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
