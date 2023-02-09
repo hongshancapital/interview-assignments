f3fe9f2f028a4addd9b9d381a19c1e0963b1bac3
fan <vincefanz@gmail.com>
2023-02-09 10:26:52 +0800

    Carousel

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,18 @@
+/** @format */
+
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
 test('renders learn react link', () => {
+  window.ResizeObserver = ResizeObserver;
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText('xPhone');
   expect(linkElement).toBeInTheDocument();
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,46 @@
+/** @format */
+
+import React from 'react';
+import Carousel from './components/Carousel';
 import './App.css';
 
+interface CardInfo {
+  title: string;
+  content: string;
+  name: string;
+}
+
+const cardList: Array<CardInfo> = [
+  {
+    title: 'xPhone',
+    content: 'Lots to love. Less to spend. <br/> Starting at $399.',
+    name: 'img-iphone',
+  },
+  {
+    title: 'Tablet',
+    content: 'Just the right amount of everything.',
+    name: 'img-tablet',
+  },
+  {
+    title: 'Buy a Tablet or xPhone for college. <br/> Get airPods.',
+    content: '',
+    name: 'img-airpods',
+  },
+];
+
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  return (
+    <div className="App">
+      <Carousel>
+        {cardList.map((info, index) => (
+          <div className={info.name} key={index}>
+            <div className="title" dangerouslySetInnerHTML={{__html: info.title}}></div>
+            <div className="text" dangerouslySetInnerHTML={{__html: info.content}}></div>
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
 }
 
 export default App;
