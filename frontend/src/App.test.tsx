200a4d7e7952575e2a2025f6fbd6f5334a252897
厉文强 <vincent.liwq@gmail.com>
2023-02-09 15:42:09 +0800

    第一次提交

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+    const { getByText } = render(<App />);
+    const ele1 = getByText(/Lots to love. Less to spend/i);
+    expect(ele1).toBeInTheDocument();
+    const ele2 = getByText(/Just the right amount of everything./i);
+    expect(ele2).toBeInTheDocument();
+    const ele3 = getByText(/Buy a Tablet or xPhone for college./i);
+    expect(ele3).toBeInTheDocument();
 });
