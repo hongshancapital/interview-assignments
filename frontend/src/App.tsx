06688ee4211590d06733d002430a88e55ae50810
xzp1234 <2431768798@qq.com>
2023-02-14 21:37:37 +0800

    轮播组件

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,46 @@
 import './App.css';
+import Carousel from './carousel';
+import { ReactNode } from 'react';
 
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+
+  let CarouselList: Array<ReactNode> = Array(3).fill(null);
+  // 自定义轮播图结构
+  CarouselList[0] = (
+    <div className="bg-container bg-iphone" key="bg-iphone">
+      <div className="carousel-item-title">
+        xPhone
+      </div>
+      <div className="carousel-item-subtitle">
+        Lots to love. Less to spend.
+        <br/>Starting at $399
+      </div>
+    </div>
+  );
+  CarouselList[1] = (
+    <div className="bg-container bg-airpods" key="bg-airpods">
+      <div className="carousel-item-title">
+        Tablet
+      </div>
+      <div className="carousel-item-subtitle">
+        Just the right amount of everything.
+      </div>
+    </div>
+  );
+  CarouselList[2] = (
+    <div className="bg-container bg-tablet" key="bg-tablet">
+      <div className="carousel-item-title">
+        Buy a Tablet or xPhone for college.
+        <br/>Get arPods.
+      </div>
+    </div>
+  );
+  
+  return (
+    <div className='App'>
+      <Carousel children={ CarouselList } width="100%" height="100%"></Carousel>
+    </div>
+  );
 }
 
 export default App;
