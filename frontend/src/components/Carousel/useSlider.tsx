8d7a56168d84bdb3700fc74d553611843eaa88d9
skipper <ivugogo@gmail.com>
2023-02-25 13:57:41 +0800

    refactor: animation for slide and button progress

--- frontend/src/components/Carousel/useSlider.tsx
+++ frontend/src/components/Carousel/useSlider.tsx
@@ -1,5 +1,30 @@
 import { useState } from "react";
+import useInterval from "use-interval";
 
-export default function useSlider() {
-  const [] = useState(0);
+const frame = 10;
+
+/**
+ * @param sliderCnt total number of slider
+ * @param interval 3000ms default
+ */
+export default function useSlider(sliderCnt: number, interval = 3000): [number, number] {
+  // progress of current slider: 0 <= & <= 100
+  const [progress, updateProgress] = useState(0);
+
+  // current slider: 0 <= & < sliderCnt
+  const [current, switchTo] = useState(0);
+
+  // or animationend
+  useInterval(() => {
+    const newProgress = progress + 100 / frame;
+    updateProgress(newProgress > 100 ? 0 : newProgress);
+    if (newProgress >= 100) {
+      switchTo((current) => {
+        const next = current + 1;
+        return next > sliderCnt ? 0 : next;
+      });
+    }
+  }, interval / frame);
+
+  return [current, progress];
 }
