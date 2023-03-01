8f82518eed4a21dfdbc747aa587265b9e472ddc6
xiaoshuangLi <qq1157502759@gmail.com>
2023-03-01 14:03:07 +0800

    feat: 🎸 complete Carousel

---
+++ frontend/src/components/Carousel/Carousel.tsx
@@ -0,0 +1,150 @@
+import React, {
+  useRef,
+  useMemo,
+  useState,
+  useCallback,
+} from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+type Item = {
+  img: string,
+  title?: string,
+  text?: string,
+};
+
+type CarouselProps = {
+  className?: string,
+  list: Item[] | string[],
+  switchDuration?: number,
+  previewDuration?: number,
+};
+
+const useEventCallback = <T,>(fn: T) => {
+  const ref = useRef<T>(fn);
+
+  ref.current = fn;
+
+  return useCallback(() => {
+    typeof ref.current === 'function' && ref.current();
+  }, [ref]) as T;
+};
+
+const Carousel = React.forwardRef<HTMLDivElement, CarouselProps>((props, ref) => {
+  const {
+    className,
+    list: propsList,
+    switchDuration = 0.5,
+    previewDuration = 2,
+  } = props;
+
+  const cls = classnames(
+    className,
+    'components-carousel-render'
+  );
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const list = useMemo(() => {
+    if (!propsList) {
+      return [];
+    }
+
+    return propsList.map((item) => {
+      return typeof item === 'string'
+        ? { img: item }
+        : item;
+    });
+  }, [propsList]);
+
+  const style = useMemo(() => {
+    return {
+      '--switch-duration': `${switchDuration}s`,
+      '--preview-duration': `${previewDuration}s`,
+    } as React.CSSProperties;
+  }, [switchDuration, previewDuration]);
+
+  const onTransitionEnd = useEventCallback(() => {
+    setCurrentIndex((prevCurrentIndex) => {
+      const { length } = list;
+
+      return (prevCurrentIndex + 0.5) % length;
+    });
+  });
+
+  const onAnimationEnd = useEventCallback(() => {
+    setCurrentIndex((prevCurrentIndex) => {
+      const { length } = list;
+
+      return (prevCurrentIndex + 0.5) % length;
+    });
+  });
+
+  const renderImgs = () => {
+    const count = Math.ceil(currentIndex) > list.length - 1
+      ? 0
+      : Math.ceil(currentIndex);
+
+    const imgsStyle = {
+      transform: `translateX(${-100 * count}%)`,
+    };
+
+    const items = list.map((item, index) => {
+      const { img, title = '', text = '' } = item;
+
+      const imgStyle = { backgroundImage: `url(${img})` };
+
+      return (
+        <div className="imgs-item" key={index} style={imgStyle}>
+          <div className="info">
+            <div className="title">{ title }</div>
+            <div className="text">{ text }</div>
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <div className="carousel-imgs" onTransitionEnd={onTransitionEnd} style={imgsStyle}>
+        { items }
+      </div>
+    );
+  };
+
+  const renderAnchors = () => {
+    const items = list.map((a, index) => {
+      const itemCls = classnames(
+        'anchors-item',
+        index === Math.ceil(currentIndex) ? 'animated' : '',
+      );
+
+      const onClickItem = () => {
+        setCurrentIndex(index - 0.5);
+      };
+
+      return (
+        <div
+          key={index}
+          className={itemCls}
+          onClick={onClickItem}
+          onAnimationEnd={onAnimationEnd}
+        />
+      );
+    });
+
+    return (
+      <div className="carousel-anchors">
+        { items }
+      </div>
+    );
+  }
+
+  return (
+    <div ref={ref} className={cls} style={style}>
+      { renderImgs() }
+      { renderAnchors() }
+    </div>
+  );
+});
+
+export default Carousel;
