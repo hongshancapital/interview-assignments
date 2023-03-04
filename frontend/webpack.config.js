ebd5203729b5b9d865c1f6bfae67f881d5f03b35
jiushang <jiushang@qunhemail.com>
2023-03-04 15:14:21 +0800

    init webpack for ts+scss development environment

---
+++ frontend/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "main.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x)?$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|jpeg|ttf)$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebPackPlugin({
+            template: `./public/index.html`,
+            filename: `index.html`,
+            scriptLoading: 'defer',
+            minify: {
+                collapseWhitespace: true,
+                keepClosingSlash: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            }
+
+        })
+    ],
+    devServer: {
+        hot: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    devtool: 'source-map',
+}
