0041ec5f0f9f813b99e69dc2e39cbdee47bfac9b
Danny <liudan@growingio.com>
2023-03-05 23:08:11 +0800

    feat: 修改createsql -> schema

--- fullstack/server/schema.sql
+++ fullstack/server/schema.sql
@@ -2,6 +2,7 @@ CREATE TABLE if not exists link_map_table(
   id serial primary key,
   short_link text NOT NULL,
   long_link text NOT NULL,
+  create_time timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE UNIQUE INDEX link_index ON link_map_table(short_link);
