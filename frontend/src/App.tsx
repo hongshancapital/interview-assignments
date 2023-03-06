a6115caf50c6dd655aac63b553a0aa44affd93d0
Cpaster <wangmengdong@bytedance.com>
2023-03-06 11:25:16 +0800

    feat: 修改图片引用

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,27 +2,21 @@ import React from "react";
 import "./App.css";
 
 import Carousel from './components/Carousel';
-import iphoneImg from './assets/iphone.png';
-import tabletImg from './assets/tablet.png';
-import airpodsImg from './assets/airpods.png';
 
 const carouselItems = [
   {
     title: "xPhone",
     describe: "Lots to love. Less to spend.\n Starting at $399.",
-    image: iphoneImg,
     className: 'carousel-iphone'
   },
   {
     title: "Tablet",
     describe: "Just the right amount of everything.",
-    image: tabletImg,
     className: 'carousel-tablet'
   },
   {
     title: "Buy a tablet or xPhone for college.\n Get airPods",
     describe: "",
-    image: airpodsImg,
     className: 'carousel-airpods'
   }
 ];
