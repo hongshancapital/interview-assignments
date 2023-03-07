a486e6579152965f364fa68edec670623992e663
heshi <heshi.net288@hotmail.com>
2023-03-07 19:14:15 +0800

    feat: carousel组件
    Signed-off-by: heshi <heshi.net288@hotmail.com>

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,70 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, render, screen, fireEvent } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
+import useActiveIndex from './carousel/useActiveIndex';
+import Dots from './carousel/dots';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.useFakeTimers();
+
+describe('carousel component unit testing', () => {
+  test('snapshot', () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+  test('useActiveIndex hooks', () => {
+    const { result } = renderHook(() => useActiveIndex({ count: 3 }));
+    expect(result.current.curIndex).toBe(0);
+
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
+    expect(result.current.curIndex).toBe(1);
+
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
+    expect(result.current.curIndex).toBe(2);
+
+    act(() => {
+      jest.runOnlyPendingTimers();
+    });
+    expect(result.current.curIndex).toBe(0);
+
+    jest.clearAllTimers();
+  });
+
+  test('dot component', async () => {
+    const { result } = renderHook(() => useActiveIndex({ count: 3 }));
+
+    const props = {
+      count: 3,
+      duration: 3000,
+      curIndex: 0,
+      setCurIndex: result.current.setCurIndex,
+    };
+
+    act(() => {
+      render(<Dots {...props} />);
+    });
+
+    const dot = document.querySelector('.dotContainer');
+    expect(dot).toBeInTheDocument();
+
+    const dotBoxs = document.querySelectorAll('.dotBox');
+    expect(dotBoxs.length).toBe(3);
+
+    const dotCenter = document.querySelectorAll('.dotCenter.active');
+    expect(dotCenter.length).toBe(1);
+  });
+
+  test('dot click', async () => {
+    render(<App />);
+
+    const carousel = await screen.findByTestId('carousel');
+    expect(carousel.classList.contains('active-0')).toBe(true);
+    const dot1 = await screen.findByTestId('dot_1');
+    fireEvent.click(dot1);
+    expect(carousel.classList.contains('active-1')).toBe(true);
+  });
 });
