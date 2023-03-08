b9fe2799d4b0d91bcc78307c62b35d8fe5eb290f
王鹏鹏 <1784611522@qq.com>
2023-03-08 09:58:03 +0800

    完善单元测试

--- frontend/src/Carousel/index.test.tsx
+++ frontend/src/Carousel/index.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Carousel from './index';
 
-test('only one child', () => {
-  const { container } = render(<Carousel><div>1</div></Carousel>);
-  const dots = container.querySelector('.dots');
-  expect(dots).toBeNull();
+test('Carousel', () => {
+  const { container } = render(<Carousel></Carousel>);
+  const img = container.querySelector('.img');
+  expect(img).toBeInstanceOf(HTMLElement);
 });
