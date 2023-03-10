c22a9bd1072961ff7005328aaef2c135f3f47608
mingo <zzmingo@qq.com>
2023-03-10 14:37:44 +0800

    feat: service tests

--- fullstack/server/tests/units/service.test.ts
+++ fullstack/server/tests/units/service.test.ts
@@ -5,6 +5,22 @@ import { createService } from '../../src/service'
 
 describe('service', () => {
   it('createOrQueryShort', async () => {
-    var service = createService(createMemoryRepository())
+    const service = createService(await createMemoryRepository())
+    const url = "https://example.com/abc";
+    const urlData = await service.createOrQueryShort(url)
+    expect(urlData.id).not.toBeUndefined()
+    expect(urlData.url).toEqual(url)
+    expect(urlData.short.length).toEqual(8)
+    expect(urlData.createTime).not.toBeUndefined()
+    expect(urlData.refreshTime).not.toBeUndefined()
+  })
+
+  it('queryByShort', async () => {
+    const service = createService(await createMemoryRepository())
+    const url = "https://example.com/abc";
+    const urlData = await service.createOrQueryShort(url)
+    const urlData2 = await service.queryByShort(urlData.short)
+    expect(urlData2).toMatchObject(urlData)
+
   })
 })
