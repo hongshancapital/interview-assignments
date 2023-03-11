90fcd7e257c419338156a42462b6158cfe4c21b6
Tianliang <tianliang.zhao@compass.com>
2023-03-11 12:50:39 +0800

    finished the interview task

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App, {
+  Carousel,
+  CarouselItem,
+  CarouselPagination,
+  CarouselPaginationItem,
+} from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("App should render normally", () => {
+  const { container } = render(<App />);
+
+  expect(container.querySelector(".carousel")).toBeInTheDocument();
+});
+test("Carousel Item should render normally", () => {
+  const { getByText } = render(<CarouselItem>content</CarouselItem>);
+
+  expect(getByText("content")).toBeInTheDocument();
+});
+
+test("CarouselPaginationItem should render normally", () => {
+  const { container } = render(
+    <CarouselPaginationItem show={true} onFinished={jest.fn()} />
+  );
+
+  expect(container.querySelector(".running")).toBeInTheDocument();
+});
+
+test("CarouselPagination should render normally", () => {
+  const { container } = render(
+    <CarouselPagination total={5} currentIndex={0} onFinished={jest.fn()} />
+  );
+  const allItems = container.querySelectorAll(
+    ".carouselPaginationItemAnimation"
+  );
+
+  expect(allItems.length).toBe(5);
+  expect(allItems[0]).toHaveClass("running");
+});
+
+test("Carousel should render normally", () => {
+  const data = [1, 2, 3, 4];
+
+  const { container } = render(
+    <Carousel>
+      {data.map((item) => {
+        return <CarouselItem key={item}>{item}</CarouselItem>;
+      })}
+    </Carousel>
+  );
+
+  expect(container.querySelector(".carousel")).toBeInTheDocument();
+  expect(container.querySelector(".carouselItemWrapper")).toBeInTheDocument();
+  expect(container.querySelector(".carouselPagination")).toBeInTheDocument();
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,136 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
+export const data = [
+  {
+    title: "xPhone",
+    color: "#fff",
+    desc: "Lots to love. Less to spend.\nStarting at $399",
+    image: require("./assets/iphone.png"),
+  },
+  {
+    title: "Tablet",
+    desc: "Just the right amount of everything.",
+    image: require("./assets/tablet.png"),
+  },
+  {
+    title: "Buy a Tablet or XPhone for college.\nGet arPods",
+    image: require("./assets/airpods.png"),
+  },
+];
+
+type CarouselItemType = { children: React.ReactNode };
+export function CarouselItem(props: CarouselItemType) {
+  const { children } = props;
+
+  return <div className="carouselItem">{children}</div>;
+}
+
+type carouselPaginationItemType = {
+  show: boolean;
+  onFinished: () => void;
+};
+export function CarouselPaginationItem({
+  show,
+  onFinished,
+}: carouselPaginationItemType) {
+  const [display, setDisplay] = useState(false);
+
+  useEffect(() => {
+    setDisplay(show);
+  }, [show]);
+
+  return (
+    <div className="carouselPaginationItem">
+      <div
+        className={
+          `carouselPaginationItemAnimation` + (display ? " running" : "")
+        }
+        onTransitionEnd={onFinished}
+      />
+    </div>
+  );
+}
+
+type CarouselPaginationType = {
+  currentIndex: number;
+  total: number;
+  onFinished: () => void;
+};
+export function CarouselPagination(props: CarouselPaginationType) {
+  const { currentIndex, total, onFinished } = props;
+
+  return (
+    <div className="carouselPagination">
+      {new Array(total).fill(1).map((v, index) => {
+        return (
+          <CarouselPaginationItem
+            show={currentIndex === index}
+            onFinished={onFinished}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+type CarouselType = {
+  children: React.ReactNode;
+  // data: { title: string; desc?: string; image: string; color?: string }[];
+};
+
+export function Carousel({ children }: CarouselType) {
+  const [index, setIndex] = useState(0);
+  const total = React.Children.toArray(children).length;
+  const onFinished = () => {
+    setIndex(index === total - 1 ? 0 : index + 1);
+  };
+
+  if (!children) {
+    return null;
+  }
+
+  return (
+    <div className="carousel">
+      <div
+        className="carouselItemWrapper"
+        style={{
+          transform: `translateX(-${index * 100}%)`,
+        }}
+      >
+        {children}
+      </div>
+      <CarouselPagination
+        total={total}
+        currentIndex={index}
+        onFinished={onFinished}
+      />
+    </div>
+  );
+}
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  return (
+    <div className="App">
+      <Carousel>
+        {data.map(({ title, desc, image, color }) => {
+          return (
+            <CarouselItem key={title}>
+              <div
+                className="carouselItemInternalWrapper"
+                style={{
+                  backgroundImage: `url(${image})`,
+                  ...(color ? { color } : {}),
+                }}
+              >
+                <div className="carouselPaginationItemTitle">{title}</div>
+                {desc && <div>{desc}</div>}
+              </div>
+            </CarouselItem>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
 }
 
 export default App;
