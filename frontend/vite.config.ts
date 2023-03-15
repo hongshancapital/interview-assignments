586cc892525421bc8a6f61521f44ca345a73217c
Wangzuo <qingjiu17@icloud.com>
2023-03-15 18:30:44 +0800

    :wrench: (Vite) 修改配置文件

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import path from "path"
-
-const resolvePath = (value: string) => path.resolve(__dirname, value)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  resolve: {
-    alias: {
-      "@": resolvePath("src"),
-    },
-  },
   plugins: [react()],
 })
