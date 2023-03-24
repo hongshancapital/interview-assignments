befec5335977e1518ededc482bdd577976c8ad5a
daihaodong.121 <daihaodong.121@bytedance.com>
2023-03-24 14:09:19 +0800

    fix: add unique id hook

--- frontend/src/components/CarouselItem/index.tsx
+++ frontend/src/components/CarouselItem/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import { BLACK_COLOR } from '../../constants/color';
-import { nanoid } from 'nanoid';
+import { useUniqueId } from '../../hooks';
 import styles from './index.module.css';
 
 interface CarouselItemProps {
@@ -13,10 +13,12 @@ interface CarouselItemProps {
 }
 
 const CarouselItem: FC<CarouselItemProps> = ({title, backgroundImage, description = [], fontColor = BLACK_COLOR, backgroundColor}) => {
+    const titleKeys = useUniqueId(title.length)
+    const descriptionKeys = useUniqueId(description.length)
     return <div className={styles.carouselItem} style={{backgroundImage: `url(${backgroundImage})`, color: fontColor, backgroundColor}}>
         <div className={styles.textContainer}>
-            <div className={styles.title}>{title.map(curTitle => <div key={nanoid()}>{curTitle}</div>)}</div>
-            {description.length ? <div className={styles.description}>{description.map(curDescription => <div key={nanoid()}>{curDescription}</div>)}</div> : null}
+            <div className={styles.title}>{title.map((curTitle, index) => <div key={titleKeys[index]}>{curTitle}</div>)}</div>
+            {description.length ? <div className={styles.description}>{description.map((curDescription, index) => <div key={descriptionKeys[index]}>{curDescription}</div>)}</div> : null}
         </div>
     </div>
 }
