a19ab93632cd04710c3121f49c67e3d845f4da18
zhanghairui <hr.zhang@mayibida.com>
2023-03-25 14:32:15 +0800

    zhanghairui push demo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,30 @@
-import './App.css';
-
+import './App.css'
+import Carousel from './Carousel/index'
+import iphoneImg from './assets/iphone.png'
+import tabletImg from './assets/tablet.png'
+import airpodsImg from './assets/airpods.png'
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  const swiperList = [
+    {
+      title: 'xPhone',
+      text: 'Lots to spend.\nStarting at $399.',
+      img: iphoneImg,
+    },
+    {
+      title: 'Buy a Tablet or xPhone for college.\nGet ar Pods.',
+      img: airpodsImg,
+    },
+    {
+      title: 'Tablet',
+      text: 'Just the right amount of everything.',
+      img: tabletImg,
+    },
+  ]
+  return (
+    <div className="App">
+      <Carousel swiperList={swiperList} />
+    </div>
+  )
 }
 
-export default App;
+export default App
