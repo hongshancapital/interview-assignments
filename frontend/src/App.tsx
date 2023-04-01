902188fee21aa1d22dde3db07bd35c9e2966a0d2
shhhplus <shhhplus@msn.com>
2023-04-01 23:22:00 +0800

    feat: add carousel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,49 @@
+import Carousel from './components/Carousel';
+import Layout from './components/Layout';
 import './App.css';
 
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  return (
+    <div className="app">
+      <Carousel autoplay>
+        <Carousel.Item>
+          <Layout
+            theme="dark"
+            title="xPhone"
+            desc={
+              <>
+                Lots to love. Less to spend.
+                <br />
+                Starting at $399.
+              </>
+            }
+            bg={require('./assets/iphone_icon.png')}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Layout
+            theme="light"
+            title="Tablet"
+            desc="Just the right amount of everything."
+            bg={require('./assets/tablet_icon.png')}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Layout
+            theme="gray"
+            title={
+              <>
+                Buy a Tablet or Phone for college.
+                <br />
+                Get airPods.
+              </>
+            }
+            bg={require('./assets/airpods_icon.png')}
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 }
 
 export default App;
