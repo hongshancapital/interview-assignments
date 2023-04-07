6eaca82744764ae1e1fd92eabc4e1d1f8c0a9ed9
李永飞 <liyongfei@lightdark.tech>
2023-04-07 09:28:39 +0800

    test(url-shortener): read short url tests

--- backend/url-shortener/test/index.test.ts
+++ backend/url-shortener/test/index.test.ts
@@ -2,6 +2,7 @@ import { app } from '../src/app';
 import supertest from 'supertest';
 import { nanoid } from 'nanoid';
 import {
+    IReadShortUrlResult,
     IShortUrlParam,
     IShortUrlResult,
     SHORT_CODE_MAX_LENGTH,
@@ -23,6 +24,29 @@ async function postShortUrl(
 ): Promise<IShortUrlResult> {
     const res = await request.post('/shortUrl').send(shortUrlParam);
     return res.body as IShortUrlResult;
+}
+
+/**
+ * 请求读取短域名对应的信息
+ * @param shortCode 短域名唯一编码
+ * @returns 短域名相关信息
+ */
+async function readShortUrl(shortCode: string): Promise<IReadShortUrlResult> {
+    const res = await request.get(`/${shortCode}`);
+    return res.body as IReadShortUrlResult;
+}
+
+/**
+ * 获取一个符合规范且与传入的短码不同的短码
+ * @param shortCode 短域名唯一编码
+ * @returns 不同于传入参数的另一个短码
+ */
+function getDiffShortCode(shortCode: string): string {
+    const diffShortCode = nanoid(SHORT_CODE_MAX_LENGTH);
+    if (diffShortCode != shortCode) {
+        return diffShortCode;
+    }
+    return getDiffShortCode(shortCode);
 }
 
 describe('url shortener api', () => {
@@ -106,6 +130,36 @@ describe('url shortener api', () => {
             const result = await postShortUrl(shortUrlParam);
             expect(result.code).toBe(StatusCode.Error);
             expect(result.shortUrl).toBeUndefined();
+            expect(result.msg).toBeDefined();
+        });
+    });
+
+    describe('read short url', () => {
+        // TODO  need create first
+        it('exist shortCode', async () => {
+            const shortCode = nanoid(Math.random() * SHORT_CODE_MAX_LENGTH);
+            await postShortUrl({ shortCode, longUrl: testLongUrl });
+            const result = await readShortUrl(shortCode);
+            expect(result.code).toBe(StatusCode.Success);
+            expect(result.longUrl).toBe(testLongUrl);
+        });
+
+        it('no exist shortCode', async () => {
+            const shortCode = nanoid(Math.random() * SHORT_CODE_MAX_LENGTH);
+            await postShortUrl({ shortCode, longUrl: testLongUrl });
+
+            const result = await readShortUrl(getDiffShortCode(shortCode));
+            expect(result.code).toBe(StatusCode.Error);
+            expect(result.longUrl).toBeUndefined();
+            expect(result.msg).toBeDefined();
+        });
+
+        it('incorrect shortCode - overlong', async () => {
+            // overlong shortCode
+            const shortCode = nanoid(SHORT_CODE_MAX_LENGTH + 1);
+            const result = await readShortUrl(shortCode);
+            expect(result.code).toBe(StatusCode.Error);
+            expect(result.longUrl).toBeUndefined();
             expect(result.msg).toBeDefined();
         });
     });
