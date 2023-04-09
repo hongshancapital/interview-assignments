7f1b98ab77b86544b4c5b559f604a13356800628
missop <keepisdom@sina.com>
2023-04-09 22:07:00 +0800

    feat: 更新单元测试

--- frontend/src/__tests__/Carousel.test.tsx
+++ frontend/src/__tests__/Carousel.test.tsx
@@ -1,26 +1,75 @@
 import { render,act } from '@testing-library/react';
 import Carousel from '../components/Carousel';
 
-const TEST_DATA = [123,456,789];
+const SLIDE_DATA = [
+  {
+    title: 'xPhone',
+    texts: ['Lots to love.Less to spend.', 'Starting at $399.'],
+    className: 'App__slide__phone',
+  },
+  {
+    title: 'Tablet',
+    texts: ['Just the right amount of everything'],
+    className: 'App__slide__tablet',
+  },
+  {
+    title: (
+      <>
+        Buy a Tablet or xPhone for college .<br />
+        Get arPods.
+      </>
+    ),
+    texts: [],
+    className: 'App__slide__arpods',
+  },
+];
+const COUNT = SLIDE_DATA.length;
+const Component = (
+  <Carousel count={COUNT} speed={300} delay={3000}>
+    {SLIDE_DATA.map((el, idx) => (
+      <Carousel.Slide
+        className={`App__slide ${el.className || ''}`}
+        key={idx}
+      >
+        <h2 className="App__title">{el.title}</h2>
+        {el.texts.map((text, idx2) => (
+          <p className="App__text" key={idx2}>
+            {text}
+          </p>
+        ))}
+      </Carousel.Slide>
+    ))}
+</Carousel>
+);
+
 describe('Carousel', () => {
-  it('should render and autoPlay', () => {
-    jest.useFakeTimers();
+  it('should render', () => {
     const { getAllByTestId } = render(
       <div data-testid="root">
-        <Carousel delay={3000} speed={300} count={3}>
-          <div data-testid="slide">
-            {TEST_DATA.map(el => <Carousel.Slide key={el}>{el}</Carousel.Slide>)}
-          </div>
-        </Carousel>
-      </div>,
+        {Component}
+      </div>
     );
-    act(() => {
-      jest.runOnlyPendingTimers();
-    });
 
     const root = getAllByTestId('root');
-    const slide = getAllByTestId('slide');
-    expect(root[0]).toContainHTML(`<div class="carousel__slide ">123</div>`);
-    expect(root[0]).toContainElement(slide[0]);
+    const paginations = root[0].querySelectorAll('.carousel__pagination__item');
+    expect(paginations.length).toBe(3);
+  });
+
+  it('slide after click pagination',() => {
+    const { getAllByTestId } = render(
+      <div data-testid="root">
+        {Component}
+      </div>
+    );
+
+    const root = getAllByTestId('root');
+    const paginations = root[0].querySelectorAll('.carousel__pagination__item');
+
+    act(()=>{
+      (paginations[1] as HTMLElement)?.click?.();
+    });
+
+    expect(paginations[0].children.length).toBe(0);
+    expect(paginations[1].children.length).toBe(1);
   });
 });
