f95c2f036ffd2005824b98b74e2ba2f4a9308a6d
yangzb <yangzb@winhong.com>
2023-04-10 17:51:47 +0800

    Carousel组件提交

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,23 @@
 import './App.css';
-
+import Carousel from './components/Carousel'
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  let img = [
+    {
+        name:'第一张幻灯片',
+        img:''
+    },
+    {
+        name:'第二张幻灯片',
+        img:''
+    },
+    {
+        name:'第三张幻灯片',
+        img:''
+    },
+]
+  return <div className='App'>{/* write your component here */}
+        <Carousel img={img} delay={2000}></Carousel>
+  </div>;
 }
 
 export default App;
