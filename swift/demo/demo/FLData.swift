a11c655b48f8fd20f732b0fe9ea3d38e6a6282ca
AlanZhangShuai <1484498126@qq.com>
2023-04-10 20:00:40 +0800

    @swiftUI demo

---
+++ swift/demo/demo/FLData.swift
@@ -0,0 +1,34 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Helpers for loading images and data.
+*/
+
+import Foundation
+import UIKit
+import SwiftUI
+
+let usersForJsonData: [FLUser] = load("userData.json")
+
+func load<T: Decodable>(_ filename: String, as type: T.Type = T.self) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
+
