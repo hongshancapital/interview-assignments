228bbcbebfe4f03c4ce8bf08a622ffbda2aa4de1
yanjian <yanjian92@126.com>
2023-04-11 08:58:18 +0800

    feat: 完成走马灯组件

---
+++ frontend/src/components/Carousel/index.spec.tsx
@@ -0,0 +1,40 @@
+import { render, RenderResult, fireEvent } from "@testing-library/react"
+import Carousel from "./index"
+
+let wrapper: RenderResult
+
+describe("test Carousel component", () => {
+  beforeEach(() => {
+    wrapper = render(
+      <Carousel autoPlay>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </Carousel>
+    )
+  })
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+  it("should render to document", () => {
+    let div = wrapper.queryByText("1")
+    expect(div).toBeInTheDocument()
+    div = wrapper.queryByText("2")
+    expect(div).toBeInTheDocument()
+  })
+  it("dot element should in the page", () => {
+    const dotElement = wrapper.container.querySelector(".carousel__dot")
+    expect(dotElement).toBeInTheDocument()
+    const items = dotElement?.querySelectorAll(".carousel__item")
+    expect(items?.length).toBe(3)
+    expect(items?.[0]).toHaveClass("carousel__item carousel__item--active")
+  })
+
+  it("click dot element", () => {
+    const items = wrapper.container.querySelectorAll(".carousel__item")
+    fireEvent.click(items[1])
+    expect(items[1]).toHaveClass("carousel__item carousel__item--active")
+    fireEvent.click(items[2])
+    expect(items[2]).toHaveClass("carousel__item carousel__item--active")
+  })
+})
