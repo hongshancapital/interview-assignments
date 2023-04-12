fa031da6a1e49496203413fafba8c7200d1524d7
lishu <175449333@qq.com>
2023-04-12 09:35:41 +0800

    [feature] init project

---
+++ swift/AppList/AppList/AppListApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct AppListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift/AppList/AppList/ContentView.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardListView(cardList: [])
+    }
+}
