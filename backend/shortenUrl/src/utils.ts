fabd3ddf78d624f976d940c7a4d11f5374af73d6
ligolas <tqligolas@gmail.com>
2023-04-13 00:25:06 +0800

    mv files

--- backend/shortenUrl/src/utils.ts
+++ backend/shortenUrl/src/utils.ts
@@ -1,6 +1,12 @@
 const REGEX_HTTP_PROTOCOL = /^https?:\/\//i
 
-export const isValidHttpUrl = (url:string):boolean =>{
+ /**
+ *
+ * 判断是否是合法的http（https） 协议下的url
+ * @param {string} url
+ * @return {*}  {boolean}
+ */
+ export const isValidHttpUrl = (url:string):boolean =>{
     try{
         const {href} = new URL(url)  
         return REGEX_HTTP_PROTOCOL.test(href) && !!href 
