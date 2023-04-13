a94384204879a02f4cb5278159f21cce63df4f58
Jerry <18800102517@163.com>
2023-04-13 23:34:24 +0800

    URL 逻辑增加时间戳&逻辑删

--- fullstack/src/server/model/Url.ts
+++ fullstack/src/server/model/Url.ts
@@ -4,14 +4,19 @@ import mongoose, {Model, Document} from "mongoose";
 export interface UrlDocument extends Document {
     longUrl: string;
     shortUrl: string;
+    createdAt: string;
+    updatedAt: string;
+    delete: boolean;
+}
+
+const urlSchema = new mongoose.Schema({
+    longUrl: String,
+    shortUrl: String,
     createdAt: {type: Date, default: Date.now},
     updatedAt: {type: Date, default: Date.now},
     delete: {type: Boolean, default: false},
-}
- const urlSchema = new mongoose.Schema({
-    longUrl: String,
-    shortUrl: String
 });
-const UrlModel:Model<UrlDocument> = mongoose.models.UrlModel || mongoose.model<UrlDocument>("UrlModel", urlSchema);
+
+const UrlModel: Model<UrlDocument> = mongoose.models.UrlModel || mongoose.model("UrlModel", urlSchema);
 
 export default UrlModel;
