ccf570bd90abddfc011a9f68cbef85ee7417fb77
安一腾 <wb-ayt773841@alibaba-inc.com>
2023-04-14 17:01:40 +0800

    feat: 前端面试-万宝锐旗外包

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,37 @@
+import Carousel from './components/Carousel'
+import Iphone from './assets/iphone.png'
+import Tablet from './assets/tablet.png'
+import Airpods from './assets/airpods.png'
+import IPhonePage from './pages/IPhonePage'
+import TabletPage from './pages/TabletPage'
+import AirpodsPage from './pages/AirpodsPage'
 import './App.css';
 
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  const imgList = [
+    { text: 'xPhone', bg: Iphone, content: <IPhonePage /> },
+    { text: 'Tablet', bg: Tablet, content: <TabletPage /> },
+    { text: 'Buy', bg: Airpods, content: <AirpodsPage /> },
+  ]
+
+  return (
+    <div className='App'>
+      <Carousel className="carousel-wrapper">
+        {
+          imgList.map((item, index) => {
+            return (
+              <div key={index}>
+                <img className="img" src={item.bg} alt="" />
+                <div>
+                  {item.content}
+                </div>
+              </div>
+            )
+          })
+        }
+      </Carousel>
+    </div>
+  )
 }
 
 export default App;
---
+++ frontend/src/pages/TabletPage.tsx
@@ -0,0 +1,10 @@
+import './TabletPage.css'
+
+export default function TabletPage() {
+  return (
+    <div className="tablet-page-wrapper">
+      <div className="tablet-title">Tablet</div>
+      <div className="tablet-content">Just the right amount of everything.</div>
+    </div>
+  )
+}
