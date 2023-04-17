eac38e60514db5afc8798234f3cf1490de6f78ee
李经纶 <281965992@qq.com>
2023-04-17 17:40:35 +0800

    添加Carousel组件

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,78 @@
+// 导入React模块和useState，useEffect，useRef函数
+import React, { useState, useEffect, useRef } from "react";
 import './App.css';
 
-function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+// 声明一个CarouselItem接口，表示每个项目的数据结构
+interface CarouselItem {
+  id: number; // 项目的唯一标识
+  image: string; // 项目的图片链接
+  title: string; // 项目的标题
+  description: string; // 项目的描述
 }
 
-export default App;
+// 声明一个Carousel组件，接受一个items数组作为属性，表示要显示的项目列表
+const Carousel = ({ items }: { items: CarouselItem[] }) => {
+  // 使用useState来声明一个current变量，表示当前显示的项目的索引，初始值为0
+  const [current, setCurrent] = useState(0);
+
+  // 使用useRef来声明一个timer变量，表示自动播放的定时器
+  const timer = useRef<NodeJS.Timeout | null>(null);
+
+  // 使用useEffect来设置和清除定时器，实现自动播放的逻辑
+  useEffect(() => {
+    // 设置定时器，每隔3秒钟切换到下一个项目
+    timer.current = setInterval(() => {
+      setCurrent((prev) => (prev + 1) % items.length);
+    }, 3000);
+
+    // 返回一个清除函数，在组件卸载时清除定时器
+    return () => {
+      if (timer.current) {
+        clearInterval(timer.current);
+      }
+    };
+  }, [items.length]);
+
+  // 定义一个handlePrev函数，用来处理点击左箭头的事件，切换到上一个项目
+  const handlePrev = () => {
+    setCurrent((prev) => (prev - 1 + items.length) % items.length);
+  };
+
+  // 定义一个handleNext函数，用来处理点击右箭头的事件，切换到下一个项目
+  const handleNext = () => {
+    setCurrent((prev) => (prev + 1) % items.length);
+  };
+
+  // 时间滚动条
+  const actionItem = items.map((i, index) => {
+    return (
+      <div className='actionItemStyle' key={index} onClick={() => setCurrent(index)}>
+        <div className='actionLineStyle' style={{ backgroundColor: current === index ? '#fff' : '#0000001a' , animationDuration: index === current ? `3s` : undefined,
+}} ></div>
+      </div>
+    )
+  })
+  // 返回一个JSX元素，表示Carousel组件的UI结构
+  return (
+    <div className="carousel">
+      {/* 显示当前项目的图片 */}
+      <img src={items[current].image} alt={items[current].title} />
+      {/* 显示当前项目的标题和描述 */}
+      <div className="carousel-info">
+        <h3>{items[current].title}</h3>
+        <p>{items[current].description}</p>
+      </div>
+      {/* 显示左右箭头，用来切换项目 */}
+      <div className="carousel-controls">
+        <button onClick={handlePrev}>&lt;</button>
+        <button onClick={handleNext}>&gt;</button>
+      </div>
+      <div className='actionStyle'>
+          {actionItem}
+        </div>
+    </div>
+  );
+};
+
+// 导出Carousel组件
+export default Carousel;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,45 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import MyCarousel from './App';
+import iphone from './assets/iphone.png';
+import tablet from './assets/tablet.png';
+import airpods from './assets/airpods.png';
+import Carousel from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const items = [
+  {
+    id: 1,
+    image:iphone,
+    title: "iPhone",
+    description:
+      "The sun rises over the horizon, painting the sky with warm colors.",
+    prices:'Starting at $399.'
+  },
+  {
+    id: 2,
+    image:tablet,
+    title: "Tablet",
+    description:
+      "KKKKKK ises over the horizon, painting the sky with warm colors.",
+    prices:'Starting at $499.'
+  },
+  {
+    id: 3,
+    image:airpods,
+    title: "airpods",
+    description:
+      "KKKKKK ises over the horizon, painting the sky with warm colors.",
+    prices:'Starting at $599.'
+  }
+]
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Carousel items={items}/>
   </React.StrictMode>
 );
