224137e16f8a2053b5cf562963fe96955d07ec4d
liangpin <1287418206@qq.com>
2023-04-21 14:06:47 +0800

    Delete rounte.ts

--- backend/shortDomain/rounte.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createShortUrl, getLongUrl } from "./controllers/shortUrlController";
-
-const routes = Router();
-
-routes.post("/shorturl", createShortUrl);
-routes.get("/shorturl/:code", getLongUrl);
-
-export default routes;
