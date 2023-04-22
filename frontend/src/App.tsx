52537d7e4b337c7e4b1de4b40aa11e2fd58b76ee
jr_mengpin <jr_mengpin@yonyou.com>
2023-04-22 15:26:53 +0800

    孟品-前端工程师

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,33 @@
+import Carousel from './components/Carousel'
+import iPhone from './assets/iphone.png'
+import tablet from './assets/tablet.png'
+import airPods from './assets/airpods.png'
 import './App.css';
 
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  const data = [
+    {
+      serviceUrl: iPhone, // 图片url
+      title: ['xPhone'], // 标题
+      desc: ['Lots to love.Less to spend.','Starting at $399.'],
+      color: "#ffffff"
+    },
+    {
+      serviceUrl: tablet, // 图片url
+      title: ['Tablet'], // 标题
+      desc: ['Just the right amount of everything.'],
+      color: "#000000"
+    },
+    {
+      serviceUrl: airPods, // 图片url
+      title: ['Buy a Tablet or xPhone for college Get.','Get arPods.'], // 标题
+      color: "#000000"
+    },
+  ]
+  return <div className='App'>
+      {/* write your component here */}
+      <Carousel data={data}/>
+    </div>;
 }
 
 export default App;
