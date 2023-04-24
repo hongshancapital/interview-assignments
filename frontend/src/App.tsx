31d5e24fffa245fde434a405b3973a0aa383f6b0
sweettina <1119436057@qq.com>
2023-04-24 21:32:56 +0800

    轮播图

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
-import './App.css';
-
+import "./App.css";
+import Carousel from "./Carousel/Carousel";
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  return (
+    <div className="App">
+      {/* write your component here */}
+      <Carousel />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/Carousel/Carousel.tsx
@@ -0,0 +1,81 @@
+import { useRef, useState, useEffect } from "react";
+import "./Carousel.less";
+import classnames from "classnames";
+import classNames from "classnames";
+
+function Carousel() {
+  const intervalRef = useRef<NodeJS.Timeout>();
+  const [count, setCount] = useState<number>(0);
+  const imgList = [
+    {
+      url: "iphone",
+      textDom: (
+        <div className="textBox">
+          <div className="bigTitle colorWhite">xPhone</div>
+          <div className="smallTitle colorWhite">
+            Lots to love. Less to spend
+          </div>
+          <div className="smallTitle colorWhite">Starting at $399</div>
+        </div>
+      ),
+    },
+    {
+      url: "tablet",
+      textDom: (
+        <div className="textBox">
+          <div className="bigTitle colorBlack">Tablet</div>
+          <div className="smallTitle colorBlack">
+            Just the right amount of everything
+          </div>
+        </div>
+      ),
+    },
+    {
+      url: "airpods",
+      textDom: (
+        <div className="textBox">
+          <div className="bigTitle colorBlack">
+            Buy a Tablet or xPhone for college
+          </div>
+          <div className="bigTitle colorBlack">Get arPods</div>
+        </div>
+      ),
+    },
+  ];
+  const screenWidth = window.screen.width;
+
+  useEffect(() => {
+    const timer: NodeJS.Timeout = setInterval(() => {
+      setCount((count) => (count >= 2 ? 0 : count + 1));
+    }, 2000);
+    intervalRef.current = timer;
+    console.log("timer", timer);
+    return () => clearInterval(intervalRef.current);
+  }, []);
+
+  return (
+    <div className="viewBox">
+      <div className="innerBox" style={{ left: `-${count * screenWidth}px` }}>
+        {imgList.map((item) => (
+          <div key={item.url} className={`imgItem ${item.url}Item`}>
+            {item.textDom}
+          </div>
+        ))}
+      </div>
+      <div className="btnBox">
+        {imgList.map((item, index) => (
+          <div
+            key={item.url}
+            className={classNames(
+              "btnItem",
+              count === index ? "activeBtn" : null
+            )}
+          >
+            <span className="inner"></span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default Carousel;
