2c1995951511ec644ac38abcb6c7490e7e935223
ming chen <mingchen@bj-dl-022.autox.sz>
2023-04-27 16:26:44 +0800

    feat: Carousel component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
 import './App.css';
+import Carousel from './components/Carousel';
+import airpods from './assets/airpods.png';
+import iphone from './assets/iphone.png';
+import tablet from './assets/tablet.png';
 
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  const images = [
+		{ url: airpods, description: ["Buy a Tablet or xPhone for college.", "Get airpods."] },
+		{ url: iphone, description: ["xPhone", "Lots to love.Less to spend", "Starting at $399."] },
+		{ url: tablet, description: ["Tablet", "Just the right amount of everything."] },
+	];
+	return (
+		<div className="App">
+			<Carousel images={images} />
+		</div>
+	);
 }
 
 export default App;
