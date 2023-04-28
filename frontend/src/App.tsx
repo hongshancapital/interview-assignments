f5db7d9ac3db95203c2b628e115698b88dc948d8
Willwillam <475294039@qq.com>
2023-04-28 09:54:32 +0800

    feat:carousel component and unit test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,27 @@
 import './App.css';
-
+import Carousel from './Carousel'
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  interface sliders {
+    imgUrl: string;
+    text: string
+  }
+  const slidesArr:sliders[] = [
+    {
+      imgUrl: require('./assets/iphone.png'),
+      text: 'xPhone | Lots to love. Less to spend. | Starting at $399.' 
+    },
+    {
+      imgUrl: require('./assets/tablet.png'),
+      text: 'Tablet | Just the right amount of everything.' 
+    },
+    {
+      imgUrl: require('./assets/airpods.png'),
+      text: 'Buy a Tablet or Phone for college. | Get arPods.' 
+    }]
+
+  return <div className='App'>
+    <Carousel slides={slidesArr} />
+  </div>;
 }
 
 export default App;
