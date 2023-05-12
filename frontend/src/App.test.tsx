deab2a5f6fbaa62af93906322d24f1a95a65604c
Arichy <arichyx@qq.com>
2023-05-12 13:34:57 +0800

    test: add test case

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { act } from 'react-dom/test-utils';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders learn react link', async () => {
+  jest.useFakeTimers();
+
+  const { getByText, getByTestId, getAllByTestId } = render(<App />);
+  expect(getByText('xPhone')).toBeInTheDocument();
+  const sliderProgressArr = getAllByTestId('carousel-slider-progress');
+  expect(sliderProgressArr).toHaveLength(3);
+
+  const scrollContianer = getByTestId('carousel-scroll-container');
+
+  expect(scrollContianer.style.transform).toBe('translateX(0px)');
+  act(() => {
+    jest.advanceTimersByTime(4000);
+  });
+
+  expect(sliderProgressArr[0]).toHaveStyle({ width: '0px' });
+  expect(sliderProgressArr[1]).toHaveStyle({ width: '40px' });
+  expect(sliderProgressArr[2]).toHaveStyle({ width: '0px' });
 });
