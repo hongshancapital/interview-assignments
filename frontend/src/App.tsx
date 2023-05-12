da090f40f637207900b1cce3b00956d299c0285d
Arichy <arichyx@qq.com>
2023-05-12 13:34:53 +0800

    feat: use in `App.tsx`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,37 @@
 import './App.css';
+import Carousel from './components/Carousel';
+import styles from './App.module.scss';
 
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  return (
+    <div className="App" style={{ width: 600 }}>
+      <Carousel interval={3000}>
+        <div
+          className={styles.page}
+          style={{ backgroundColor: 'black', color: 'white' }}
+        >
+          <h1>xPhone</h1>
+          <div>Lots to love. Less to spend.</div>
+          <div>Starting at $399.</div>
+        </div>
+
+        <div className={styles.page} style={{ backgroundColor: 'white' }}>
+          <h1>Tablet</h1>
+          <div>Just the right amout of everything.</div>
+          <div>Starting at $399.</div>
+        </div>
+
+        <div
+          className={styles.page}
+          style={{ backgroundColor: 'rgb(239,239,241)' }}
+        >
+          <h1>Buy a Tablet or xPhone for college</h1>
+          <div>Get arPods</div>
+          <div>Starting at $399.</div>
+        </div>
+      </Carousel>
+    </div>
+  );
 }
 
 export default App;
