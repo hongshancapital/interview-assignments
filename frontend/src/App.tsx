8046981acd14cc7ae6b72f8dcdf86816e2785ca3
Arichy <arichyx@qq.com>
2023-05-12 13:53:38 +0800

    feat: background-image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,22 +7,25 @@ function App() {
     <div className="App" style={{ width: 600 }}>
       <Carousel interval={3000}>
         <div
-          className={styles.page}
-          style={{ backgroundColor: 'black', color: 'white' }}
+          className={`${styles.page} ${styles.page1}`}
+          style={{ color: 'white' }}
         >
           <h1>xPhone</h1>
           <div>Lots to love. Less to spend.</div>
           <div>Starting at $399.</div>
         </div>
 
-        <div className={styles.page} style={{ backgroundColor: 'white' }}>
+        <div
+          className={`${styles.page} ${styles.page2}`}
+          style={{ backgroundColor: 'white' }}
+        >
           <h1>Tablet</h1>
           <div>Just the right amout of everything.</div>
           <div>Starting at $399.</div>
         </div>
 
         <div
-          className={styles.page}
+          className={`${styles.page} ${styles.page3}`}
           style={{ backgroundColor: 'rgb(239,239,241)' }}
         >
           <h1>Buy a Tablet or xPhone for college</h1>
