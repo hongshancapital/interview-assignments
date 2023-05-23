e7355ee4ea4014c96745d6fbe43af4b2f7bfaab4
zhang shijie <tmyk104117@126.com>
2023-05-24 07:58:59 +0800

    init project

---
+++ swift/AppHunter/AppHunter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AppHunter
+//
+//  Created by zhang shijie on 2023/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
