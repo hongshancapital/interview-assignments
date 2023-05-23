3edf4da586b7b46829aabdc725f63eae3a81ceb4
jiaqidegege <31435549+jiaqidegege@users.noreply.github.com>
2023-05-24 00:59:38 +0800

    feat(component): add carousel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
-import './App.css';
+import "./App.css";
+import Carousel from "./components/carousel/Carousel";
 
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  return (
+    <div className="App">
+      <Carousel interval={1600}>
+        <div className="page">page-1</div>
+        <div className="page">page-2</div>
+        <div className="page">page-3</div>
+        <div className="page">page-4</div>
+      </Carousel>
+    </div>
+  );
 }
 
 export default App;
