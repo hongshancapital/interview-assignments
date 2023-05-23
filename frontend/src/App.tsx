f72e14be0e8a932903e90816579fb9d132ba33c5
叶家伟 <jiaweiye.yjw@alibaba-inc.com>
2023-05-23 21:00:56 +0800

    feat: 叶家伟的提交

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,40 @@
-import './App.css';
+import Slider from "./comps";
+import "./App.css";
 
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  return (
+    <div className="App">
+      <Slider
+        len={3}
+        content={
+          <>
+            <div className="part">
+              <div className="text">
+                <p className="title">xPhone</p>
+                <p className="subtitle">Lots to love.Less to spend.</p>
+                <p className="subtitle">Starting at $399.</p>
+              </div>
+              <div className="img"></div>
+            </div>
+            <div className="part">
+              <div className="text">
+                <p className="title">Tablet</p>
+                <p className="subtitle">Just the right amount of everything.</p>
+              </div>
+              <div className="img"></div>
+            </div>
+            <div className="part">
+              <div className="text">
+                <p className="title">Buy a Tablet Or xPhone for college.</p>
+                <p className="title">Get arPods.</p>
+              </div>
+              <div className="img"></div>
+            </div>
+          </>
+        }
+      ></Slider>
+    </div>
+  );
 }
 
 export default App;
