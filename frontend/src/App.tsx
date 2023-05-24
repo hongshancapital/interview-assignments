1c1cf8b0643aa9dd112cbb124427d6726a1a44ee
shuzhiyu <1916354612@qq.com>
2023-05-24 20:00:38 +0800

    feat: 添加Carousel组件

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
-import './App.css';
+import Carousel from './components/Carousel'
+import airpods from './assets/airpods.png'
+import iphone from './assets/iphone.png'
+import tablet from './assets/tablet.png'
+import './App.css'
 
 function App() {
-  return <div className='App'>{/* write your component here */}</div>;
+  return (
+    <div className='App'>
+      <div className='carousel-area'>
+        <Carousel>
+          <img width='100%' height='100%' src={iphone} alt='' />
+          <img width='100%' height='100%' src={tablet} alt='' />
+          <img width='100%' height='100%' src={airpods} alt='' />
+        </Carousel>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
