a605c2482ad7231425657403a11ddd24e7f5eb98
dalmatian1231 <gzstation@126.com>
2023-07-08 11:11:48 +0800

    upload python

---
+++ it-support-engineer/Staff_IT_Engineer-GUOZHEN.py
@@ -0,0 +1,78 @@
+# Staff IT Engineer, Beijing - Owner:Guo Zhen
+# Date July, 5, 2023
+# After running this script, it will post resulte to server in json format
+# meanwhile 2 files 'ascendingfrequenterror.csv'和‘jsondatadump.txt’ will be generated in local path 
+
+import json
+import csv
+from collections import Counter
+import pandas as pd
+from requests.packages import urllib3
+import requests
+
+
+file_name = "./interview_data_set"
+file = open(file_name, "r")
+datachart = []
+
+for line in file.readlines():
+	if line.startswith('\t'):
+		pass
+	else:
+#fetch 4 values in each line and added tuple to list
+		description = line.split(':',3)[-1].strip('\n').strip()
+		details = []
+		pieces=line.split()
+		deviceName=pieces[3]
+		raw_processName=pieces[4]
+		processName=raw_processName[0:raw_processName.rfind('[')]
+		raw_processId=raw_processName[raw_processName.rfind('['):raw_processName.rfind(']')]
+		processId=raw_processId.strip('[')
+		rawtime=pieces[2].split(':')[0]
+		starthour="{}00".format(str(int(rawtime)).zfill(2))
+		nexthour="{}00".format((str(int(rawtime)+1)).zfill(2))
+		timeWindow="{}-{}".format(starthour, nexthour)
+		datachart.append((deviceName, processName, processId, description, timeWindow))
+
+
+#counter accumulates amounts, merges 5th value into previous 4 values, zip together to list
+
+counter = Counter(datachart)
+final_chart = []
+final_dict = {}
+key_index = ("deviceName", "processName", "processId", "description", "timeWindow", "numberOfOccurrence")
+for key, raw_Occurrence in counter.items():
+    deviceName, processId, processName, description, timeWindow = key
+    numberOfOccurrence = raw_Occurrence
+    final_dict = dict(zip(key_index,(*key, numberOfOccurrence)))
+    final_chart.append(final_dict)
+
+
+
+#Print out Top 50 frequent error list and generate a csv report 
+df=pd.DataFrame(final_chart)
+pd.set_option('display.max_rows',None)
+#pd.set_option('display.max_colwidth',None)
+#most_frequent_alert=df.sort_values(by='numberOfOccurrence',ascending=False).head(50)
+most_frequent_alert=df.sort_values(by='numberOfOccurrence',ascending=False)
+#print(most_frequent_alert)
+most_frequent_alert.to_csv('ascendingfrequenterror.csv', index=False)
+
+
+requests.packages.urllib3.disable_warnings()  
+
+#Upload json to server
+headers = {
+    "Content-type": "application/json; charset=UTF-8"
+}
+json_data=json.dumps(final_chart,indent = 2)
+response = requests.post(url='https://foo.com/bar', headers=headers, verify=False, data=json_data)
+with open('./jsondatadump.txt','wt') as p:
+	print (json_data,file=p) 
+
+print("脚本运行成功，请分别查看生成的两个文件'ascendingfrequenterror.csv'和‘jsondatadump.txt’")
+
+#if response.status_code == 200:
+#	print('Submit successed')
+#else:
+#	print('Submit failed')
