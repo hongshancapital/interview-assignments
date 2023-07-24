58c6da32ea79ef1a83739b64852714ebaf322ef5
lilifan <lilifan@lixiang.com>
2023-07-24 16:34:26 +0800

    短域名转换初始化提交

---
+++ backend/src/config/db.ts
@@ -0,0 +1,12 @@
+export const dbConfig = {
+  dev: {
+    host: 'localhost',
+    user: 'root',
+    database: 'short_url',
+    password: '111',
+    port: 3306,
+    // 给sequelize用
+    username: 'root',
+    dialect: 'mysql',
+  },
+}
