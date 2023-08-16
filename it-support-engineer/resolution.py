1521518b89cdb3e76b1edf724eb8539408bccb59
vissible <vissible@gmail.com>
2023-08-16 10:29:08 +0800

    vissible's answer

---
+++ it-support-engineer/resolution.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import gzip
+import re
+import requests
+import json
+
+def parse_log():
+    """parse log and return payload"""
+    with gzip.open("interview_data_set.gz", "rt") as f:
+        lines = []
+        for line in f:
+            if not line.startswith('May'):
+                lines[-1] += line
+            elif '--- last message repeated' in line:
+                lines.append(' '.join(line.split()[:3]+lines[-1].split()[3:]))
+            else:
+                lines.append(line)
+
+    pattern = r'^(?P<date>[a-zA-Z]{3} [0-9]{1,2}) (?P<time>\d{2}):\d{2}:\d{2} (?P<device>\S+) (?P<pname>[a-zA-Z.]+)\[(?P<pid>\d+).*[\]\)]: (?P<desc>.*)$'
+    result = {}
+    for line in lines:
+        matched_search = re.search(pattern, line)
+        if matched_search is not None:
+            result_key = matched_search.groups()
+            result[result_key] = result.get(result_key, 0) + 1
+
+    payload = []
+    for key in result.keys():
+        _, timeWindow, deviceName, processName, processId, description = key
+        payload.append({
+            "deviceName": deviceName,
+            "processId": processId,
+            "processName": processName,
+            "description": description,
+            "timeWindow": timeWindow,
+            "numberOfOccurrence": str(result[key])
+            })
+    return payload
+
+if __name__ == '__main__':
+    headers = {'Content-Type': 'application/json'}
+    payload=json.dumps(parse_log())
+    # url = "https://httpbin.org/post"
+    url = 'https://foo.com/bar'
+    resp = requests.post(url=url, headers=headers, data=payload)
+    if resp.status_code == 200:
+        print("successfully uploaded!")
+    else:
+        print("upload failed, status_code: {}".format(resp.status_code))
