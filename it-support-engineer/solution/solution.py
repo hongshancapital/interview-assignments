4009f89fb4c2af0d5e7457fab007ffae1d366646
llinan <believelinan@aliyun.com>
2023-08-16 10:15:50 +0800

    fix

--- it-support-engineer/solution/solution.py
+++ it-support-engineer/solution/solution.py
@@ -1,53 +1,47 @@
 import re
 import requests
-import csv
 from collections import OrderedDict
+from requests.packages import urllib3 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class LogParser(object):
     def __init__(self, filename):
         self.f = open(filename, 'rt')
-        self.csvfile = open('upload.csv', 'w')
-        self.csvwriter = csv.writer(self.csvfile)
-        # container of required data, which is ready to be uploaded
-        # self.data_all = {}
-        # enhancement: compatible with python version prior to 3.6
         self.data_all = OrderedDict()
-        # regular expression pattern, with named groups
-        self.pattern_log = r'^(?P<date>[a-zA-Z]{3} [0-9]{1,2}) (?P<logtime>\d{2}:\d{2}:\d{2}) (?P<device>\S+) (?P<processname>[a-zA-Z.]+)\[(?P<processid>\d+).*[\]\)]: (?P<description>.*)$'
-        self.timeWindow = '00'
+        self.pattern_log = r'^(?P<date>[a-zA-Z]{3} [0-9]{1,2}) (?P<logtime>\d{2}:\d{2}:\d{2}) (?P<device>\S+) (?P<processname>[a-zA-Z_. ]+)\[(?P<processid>\d+).*[\]\)]: (?P<description>.*)$'
+        self.pattern_log_repeat = r'.*last message repeated 1 time.*'
+        self.timeWindow = None
         self.upload_flag = False
-        self.data_item_ready = None
+        self.data_item_temp = None
+        self.count = 0
     def __del__(self):
         self.f.close()
-        self.csvfile.close()
-        print('**result reference: upload.csv**')
 
     # upload
     def upload(self):
+        payloads = []
         for value in self.data_all.values():
-            # payload = {
-            #     'deviceName': value.get('deviceName'),
-            #     'processId': value.get('processId'),
-            #     'processName': value.get('processName'),
-            #     'description': value.get('description'),
-            #     'timeWindow': value.get('timeWindow'),
-            #     'numberOfOccurrence':value.get('numberOfOccurrence')
-            # }
-            # foo.com is not reachable
-            # requests.request('post', 'https://foo.com/bar', headers={}, data=payload)
-            # write data to local csv file to mimic post upload
-            self.csvwriter.writerow([value.get('deviceName'), value.get('processId'), value.get('processName'), value.get('description'), value.get('timeWindow'), value.get('numberOfOccurrence')])
-        print('==mimic upload(write to csv)==')
+            payload = {
+                'deviceName': value.get('deviceName'),
+                'processId': value.get('processId'),
+                'processName': value.get('processName'),
+                'description': value.get('description'),
+                'timeWindow': value.get('timeWindow'),
+                'numberOfOccurrence':value.get('numberOfOccurrence')
+            }
+            payloads.append(payload)
+            self.count += 1
+        requests.request('post', 'https://foo.com/bar', headers={}, data=payloads, verify=False)
         self.data_all.clear()
+        if self.data_item_temp:
+            self.data_all.update(self.data_item_temp)
         self.upload_flag = False
+        self.data_item_temp = None
     
-    # parse log raw line text
-    def parse_line(self):
-        raw = self.f.readline()
-        if not raw:
-            raise EOFError()
+    # parse raw log. Possibly combination of multi-lines
+    def parse_line(self, raw):
         mysearch = re.search(self.pattern_log, raw, re.I)
-        # matched
+        # if match
         if mysearch is not None:
             search_dict = mysearch.groupdict()
             deviceName = search_dict.get('device')
@@ -66,28 +60,58 @@ class LogParser(object):
             data_item_with_occurrence = data_item_without_occurrence
             data_item = {key:data_item_with_occurrence}
 
-            if self.timeWindow == timeWindow:
+            if self.timeWindow == None:
+                self.timeWindow = timeWindow
+                self.data_all.update(data_item)
+            elif self.timeWindow == timeWindow:
                 self.data_all.update(data_item)
             # if timeWindow come to another hour range:
-            # 1. set upload_flag. it will upload local saved data_all and clean up it, this comes from the concern of running out of memory, expecially the log file is huge.
-            # 2. temporily save the parsed current log line text, and add to data_all after upload and data_all cleaning up. Because this item belongs to next timeWindow
+            # 1. set upload_flag. it will indicate parse_and_upload() to upload local saved data_all and then clean up it. This design comes from the concern of running out of memory, expecially the log file is huge.
+            # 2. temporily save the parsed current log line text, and add to data_all after upload and data_all cleaning up. Because this item belongs to next timeWindow.
             else:
                 self.timeWindow = timeWindow
                 self.upload_flag = True
                 self.data_item_temp = data_item
-        # not matched
-        # do enhancement is needed.
+        # if not match
         else:
-            print('==not match pattern==')
+            # **Warning**
+            # this part is not supposed to be arrived. If so, this code should be updated.
+            print('==[debug] {} not match pattern=='.format(raw))
+
+    def parse_lines(self):
+        while True:
+            self.line_cur = self.line_next
+            self.line_next = self.f.readline()
+            # EOF
+            if not self.line_next:
+                self.parse_line(self.line_cur)
+                raise EOFError()
+                break
+            # in case of one single-line log. This is the most cases in fact.
+            if re.compile(self.pattern_log).match(self.line_next):
+                self.parse_line(self.line_cur)
+                break
+            # in case like "May 13 12:11:51 --- last message repeated 1 time ---"
+            elif re.compile(self.pattern_log_repeat).match(self.line_next):
+                self.parse_line(self.line_cur)
+                self.line_next = self.line_cur
+                break
+            # in case of single log item with multi-lines
+            else:
+                self.line_next = self.line_cur.rstrip('\r\n') + ' ' + self.line_next.strip()
+            
 
     def parse_and_upload(self):
+        self.line_cur = None
+        self.line_next = self.f.readline()
         while True:
             try:
-                self.parse_line()
+                self.parse_lines()
                 if self.upload_flag:
                     self.upload()
-                    self.data_all.update(self.data_item_temp)
             except EOFError:
+                if self.data_item_temp:
+                    self.data_all.update(self.data_item_temp)
                 self.upload()
                 break
         
@@ -96,9 +120,11 @@ class LogParser(object):
 if __name__ == '__main__':
 
     try:
-        log = LogParser('./../interview_data_set')
+        log = LogParser('../interview_data_set')
         log.parse_and_upload()
 
     except Exception as e:
         print(e)
+    else:
+        print('uploaded {} logs'.format(log.count))
 
