180905b7843ecb4beb20efe62820798a49881267
HocoHe <hoco.he@outlook.com>
2023-11-21 08:17:30 +0000

    hoco answer update

--- it-support-engineer/hoco-homework-Powershell.ps1
+++ it-support-engineer/hoco-homework-Powershell.ps1
@@ -1,15 +1,11 @@
 ﻿$ErrorActionPreference = "Stop"
-
-# 设置服务器 URL
 $url = 'https://foo.com/bar'
+$logfile = '.\interview_data_set.gz'
 
-# 日志文件路径
-$logfile = 'C:\Users\admincn\Desktop\interview_data_set'
 
-# 初始化存储日志条目的数组
 $lineObjectArray = @()
 
-# 逐行处理日志内容
+
 foreach ($line in Get-Content -Path $logfile) {
     if ($line -match ':') {
         $lineArray = $line -split ' '
@@ -26,10 +22,10 @@ foreach ($line in Get-Content -Path $logfile) {
     }
 }
 
-# 初始化哈希表用于存储统计信息
+
 $groupCounter = @{}
 
-# 对日志条目进行统计
+
 $lineObjectArray | ForEach-Object {
     $key = $_.deviceName + $_.processId + $_.processName + $_.description + $_.hour
     if ($groupCounter.Contains($key)) {
@@ -48,15 +44,14 @@ $lineObjectArray | ForEach-Object {
     }
 }
 
-# 将统计信息按照 timeWindow 排序，并生成 JSON
+
 $sortedOutput = $groupCounter.Values | Sort-Object -Property timeWindow
 $jsonPayload = $sortedOutput | ConvertTo-Json
 
-# 输出 JSON 数据
+
 Write-Output $jsonPayload
 
-# 将 JSON 数据保存到文件
-$jsonPayload | Set-Content -Path "C:\Users\admincn\Desktop\output-2.json"
+$jsonPayload | Set-Content -Path ".\output-2.json"
 
-# 发送 JSON 数据到服务器
-#Invoke-RestMethod -Uri $url -TimeoutSec 5 -Method Post -InFile ".\output-1.json"
+
+Invoke-RestMethod -Uri $url -TimeoutSec 5 -Method Post -InFile ".\output-2.json"
